ebac8f862575e4899e4f75b04e56465a283bde1c
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2000-04-14 17:31:28 +0000

    Added copyright notices.

--- src/cli.c
+++ src/cli.c
@@ -1,3 +1,23 @@
+/*
+ *      Copyright (C) 2000 Nikos Mavroyanopoulos
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 #include <stdio.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -19,8 +39,8 @@ int main()
 	GNUTLS_STATE state;
 	char buffer[100];
 
-//	signal(SIGPIPE, SIG_IGN);
-	
+//      signal(SIGPIPE, SIG_IGN);
+
 	sd = socket(AF_INET, SOCK_STREAM, 0);
 	ERR(sd, "socket");
 
@@ -34,8 +54,8 @@ int main()
 
 	gnutls_init(&state, GNUTLS_CLIENT);
 	ret = gnutls_handshake(sd, state);
-	
-	if (ret<0) {
+
+	if (ret < 0) {
 		fprintf(stderr, "Handshake has failed\n");
 		gnutls_perror(ret);
 		return 1;
@@ -45,25 +65,29 @@ int main()
 
 	bzero(buffer, sizeof(buffer));
 	ret = gnutls_recv(sd, state, buffer, 5);
-	if (gnutls_is_fatal_error(ret)==1) {
+	if (gnutls_is_fatal_error(ret) == 1) {
 		if (ret == GNUTLS_E_CLOSURE_ALERT_RECEIVED) {
-			fprintf(stderr, "Peer has closed the GNUTLS connection\n");
+			fprintf(stderr,
+				"Peer has closed the GNUTLS connection\n");
 			return 0;
 		} else {
-			fprintf(stderr, "Received corrupted data(%d)\n", ret);
+			fprintf(stderr, "Received corrupted data(%d)\n",
+				ret);
 			return 1;
 		}
 	} else {
 		fprintf(stdout, "Received: %s\n", buffer);
 	}
-	
+
 	ret = gnutls_recv(sd, state, buffer, 10);
-	if (gnutls_is_fatal_error(ret)==1) {
+	if (gnutls_is_fatal_error(ret) == 1) {
 		if (ret == GNUTLS_E_CLOSURE_ALERT_RECEIVED) {
-			fprintf(stderr, "Peer has closed the GNUTLS connection\n");
+			fprintf(stderr,
+				"Peer has closed the GNUTLS connection\n");
 			return 0;
 		} else {
-			fprintf(stderr, "Received corrupted data(%d)\n", ret);
+			fprintf(stderr, "Received corrupted data(%d)\n",
+				ret);
 			return 1;
 		}
 	} else {
--- src/serv.c
+++ src/serv.c
@@ -1,3 +1,23 @@
+/*
+ *      Copyright (C) 2000 Nikos Mavroyanopoulos
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 #include <stdio.h>
 #include <errno.h>
 #include <sys/types.h>
@@ -49,7 +69,7 @@ int main()
 
 
 		ret = gnutls_handshake(sd, state);
-		if (ret<0) {
+		if (ret < 0) {
 			close(sd);
 			gnutls_deinit(&state);
 			fprintf(stderr, "Handshake has failed\n", ret);
@@ -58,8 +78,10 @@ int main()
 		}
 		fprintf(stderr, "Handshake was completed\n");
 
-		ret = gnutls_send(sd, state, "hello world\n", sizeof("hello world\n"));
-		if (ret<0) {
+		ret =
+		    gnutls_send(sd, state, "hello world\n",
+				sizeof("hello world\n"));
+		if (ret < 0) {
 			close(sd);
 			gnutls_deinit(&state);
 			gnutls_perror(ret);
