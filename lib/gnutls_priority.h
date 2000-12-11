d6dce67e7e5b5a8d728f2afd70231c2ee32445bb
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2000-12-11 23:45:18 +0000

    added missing file

---
+++ lib/gnutls_priority.h
@@ -0,0 +1,4 @@
+void gnutls_set_cipher_priority( GNUTLS_STATE state, int num, ...);
+void gnutls_set_kx_priority( GNUTLS_STATE state, int num, ...); 
+void gnutls_set_mac_priority( GNUTLS_STATE state, int num, ...);
+void gnutls_set_compression_priority( GNUTLS_STATE state, int num, ...);
