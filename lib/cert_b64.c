6fd19aae0172ff000b143036f0c40f2cf4cea58e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2001-01-19 14:14:50 +0000

    corrected license

--- lib/cert_b64.c
+++ lib/cert_b64.c
@@ -1,14 +1,14 @@
 /*
  * Copyright (C) 2000 Nikos Mavroyanopoulos <nmav@hellug.gr>
  *
- * This file is part of GNUTLS Certificate API.
+ * This file is part of GNUTLS.
  *
  * GNUTLS is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * GNUTLS Certificate API is distributed in the hope that it will be useful,
+ * GNUTLS is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
