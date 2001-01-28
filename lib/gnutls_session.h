5a4251ff234d5ebd8d451a0406d29fc4f1e896af
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2001-01-28 23:49:04 +0000

    added copyright notice

--- lib/gnutls_session.h
+++ lib/gnutls_session.h
@@ -1,2 +1,22 @@
+/*
+ *      Copyright (C) 2000 Nikos Mavroyanopoulos
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
 int gnutls_set_current_session( GNUTLS_STATE state, void* session, int session_size);
 int gnutls_get_current_session( GNUTLS_STATE state, void* session, int *session_size);
