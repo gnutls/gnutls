7fdf862deaf7c0f0a7bece11f4e8e12d55814e7f
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2001-09-23 18:20:56 +0000

    Preliminary support for DHE_RSA

---
+++ lib/auth_dhe_rsa.c
@@ -0,0 +1,275 @@
+/*
+ *      Copyright (C) 2000,2001 Nikos Mavroyanopoulos
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+/* DHE_RSA is not really working. It is used as a template 
+ * (it may work BUT it does not check certificates)
+ */
+
+#include "gnutls_int.h"
+#include "gnutls_auth_int.h"
+#include "gnutls_errors.h"
+#include "gnutls_dh.h"
+#include "gnutls_num.h"
+#include <auth_x509.h>
+
+static int gen_dhe_rsa_server_kx( GNUTLS_STATE , opaque**);
+static int gen_dhe_rsa_client_kx( GNUTLS_STATE , opaque**);
+static int proc_dhe_rsa_server_kx( GNUTLS_STATE , opaque*, int);
+static int proc_dhe_rsa_client_kx( GNUTLS_STATE , opaque*, int);
+
+MOD_AUTH_STRUCT dhe_rsa_auth_struct = {
+	"DHE_RSA",
+	_gnutls_gen_x509_server_certificate,
+	_gnutls_gen_x509_client_certificate,
+	gen_dhe_rsa_server_kx,
+	NULL,
+	NULL,
+	gen_dhe_rsa_client_kx,
+	_gnutls_gen_x509_client_cert_vrfy, /* gen client cert vrfy */
+	_gnutls_gen_x509_server_cert_req, /* server cert request */
+
+	_gnutls_proc_x509_server_certificate,
+	_gnutls_proc_x509_client_certificate,
+	proc_dhe_rsa_server_kx,
+	NULL,
+	NULL,
+	proc_dhe_rsa_client_kx,
+	_gnutls_proc_x509_client_cert_vrfy, /* proc client cert vrfy */
+	_gnutls_proc_x509_cert_req	/* proc server cert request */
+};
+
+static int gen_dhe_rsa_server_kx( GNUTLS_STATE state, opaque** data) {
+	MPI x, X, g, p;
+	size_t n_X, n_g, n_p;
+	uint8 *data_p;
+	uint8 *data_g;
+	uint8 *data_X;
+	int ret = 0;
+	const X509PKI_CREDENTIALS cred;
+	int bits;
+
+	cred = _gnutls_get_kx_cred( state->gnutls_key, GNUTLS_X509PKI, NULL);
+	if (cred==NULL) {
+		bits = DEFAULT_BITS; /* default */
+	} else {
+		bits = cred->dh_bits;
+	}
+
+	g = gnutls_get_dh_params(&p, bits);
+	if (g==NULL) {
+		gnutls_assert();
+		return GNUTLS_E_MEMORY_ERROR;
+	}
+
+	state->gnutls_key->auth_info = gnutls_malloc(sizeof(X509PKI_SERVER_AUTH_INFO_INT));
+	if (state->gnutls_key->auth_info==NULL) return GNUTLS_E_MEMORY_ERROR;
+
+	((X509PKI_SERVER_AUTH_INFO)state->gnutls_key->auth_info)->dh_bits = gcry_mpi_get_nbits(p);
+	state->gnutls_key->auth_info_size = sizeof(X509PKI_SERVER_AUTH_INFO);
+
+	X = gnutls_calc_dh_secret(&x, g, p);
+	if (X==NULL) {
+		_gnutls_mpi_release( &g);
+		_gnutls_mpi_release( &p);
+		gnutls_assert();
+		return GNUTLS_E_MEMORY_ERROR;
+	}
+	state->gnutls_key->dh_secret = x;
+
+	gcry_mpi_print(GCRYMPI_FMT_USG, NULL, &n_g, g);
+	gcry_mpi_print(GCRYMPI_FMT_USG, NULL, &n_p, p);
+	gcry_mpi_print(GCRYMPI_FMT_USG, NULL, &n_X, X);
+	(*data) = gnutls_malloc(n_g + n_p + n_X + 6);
+	if (*data==NULL) {
+		_gnutls_mpi_release( &X);
+		_gnutls_mpi_release( &g);
+		_gnutls_mpi_release( &p);
+		return GNUTLS_E_MEMORY_ERROR;
+	}
+
+	data_p = &(*data)[0];
+	gcry_mpi_print(GCRYMPI_FMT_USG, &data_p[2], &n_p, p);
+	_gnutls_mpi_release(&p);
+
+	WRITEuint16( n_p, data_p);
+
+	data_g = &data_p[2 + n_p];
+	gcry_mpi_print(GCRYMPI_FMT_USG, &data_g[2], &n_g, g);
+	_gnutls_mpi_release(&g);
+
+	WRITEuint16( n_g, data_g);
+
+	data_X = &data_g[2 + n_g];
+	gcry_mpi_print(GCRYMPI_FMT_USG, &data_X[2], &n_X, X);
+	_gnutls_mpi_release(&X);
+
+	WRITEuint16( n_X, data_X);
+
+	ret = n_p+n_g+n_X+6;
+
+	return ret;
+}
+
+static int gen_dhe_rsa_client_kx( GNUTLS_STATE state, opaque** data) {
+MPI x, X;
+size_t n_X;
+
+	X =  gnutls_calc_dh_secret(&x, state->gnutls_key->client_g,
+		   state->gnutls_key->client_p);
+		   
+	gcry_mpi_print(GCRYMPI_FMT_USG, NULL, &n_X, X);
+	(*data) = gnutls_malloc(n_X + 2);
+	if (*data==NULL)
+		return GNUTLS_E_MEMORY_ERROR;
+
+	gcry_mpi_print(GCRYMPI_FMT_USG, &(*data)[2], &n_X, X);
+	(*data)[0] = 1;	/* extern - explicit since we do not have
+				   certificate */
+	_gnutls_mpi_release(&X);
+	
+	WRITEuint16( n_X, &(*data)[0]);    
+	
+	/* calculate the key after calculating the message */
+	state->gnutls_key->KEY = gnutls_calc_dh_key(state->gnutls_key->client_Y, x, state->gnutls_key->client_p);
+
+	/* THESE SHOULD BE DISCARDED */
+	_gnutls_mpi_release(&state->gnutls_key->client_Y);
+	_gnutls_mpi_release(&state->gnutls_key->client_p);
+	_gnutls_mpi_release(&state->gnutls_key->client_g);
+	state->gnutls_key->client_Y = NULL;
+	state->gnutls_key->client_p = NULL;
+	state->gnutls_key->client_g = NULL;
+
+	return n_X+2;
+}
+
+static int proc_dhe_rsa_server_kx( GNUTLS_STATE state, opaque* data, int data_size) {
+	uint16 n_Y, n_g, n_p;
+	size_t _n_Y, _n_g, _n_p;
+	uint8 *data_p;
+	uint8 *data_g;
+	uint8 *data_Y;
+	int i;
+
+
+	i = 0;
+	n_p = READuint16( &data[i]);
+	i += 2;
+
+	data_p = &data[i];
+	i += n_p;
+	if (i > data_size) {
+		gnutls_assert();
+		return GNUTLS_E_UNEXPECTED_PACKET_LENGTH;
+	}
+	
+	n_g = READuint16( &data[i]);
+	i += 2;
+
+	data_g = &data[i];
+	i += n_g;
+	if (i > data_size) {
+		gnutls_assert();
+		return GNUTLS_E_UNEXPECTED_PACKET_LENGTH;
+	}
+	
+	n_Y = READuint16( &data[i]);
+	i += 2;
+
+	data_Y = &data[i];
+	i += n_Y;
+	if (i > data_size) {
+		gnutls_assert();
+		return GNUTLS_E_UNEXPECTED_PACKET_LENGTH;
+	}
+	_n_Y = n_Y;
+	_n_g = n_g;
+	_n_p = n_p;
+
+	if (gcry_mpi_scan(&state->gnutls_key->client_Y,
+			      GCRYMPI_FMT_USG, data_Y, &_n_Y) != 0) {
+		gnutls_assert();
+		return GNUTLS_E_MPI_SCAN_FAILED;
+	}
+
+	if (gcry_mpi_scan(&state->gnutls_key->client_g,
+			      GCRYMPI_FMT_USG, data_g, &_n_g) != 0) {
+		gnutls_assert();
+		return GNUTLS_E_MPI_SCAN_FAILED;
+	}
+	if (gcry_mpi_scan(&state->gnutls_key->client_p,
+				      GCRYMPI_FMT_USG, data_p, &_n_p) != 0) {
+		gnutls_assert();
+		return GNUTLS_E_MPI_SCAN_FAILED;
+	}
+
+	/* We should check signature in non-anonymous KX 
+	 * this is anonymous however
+	 */
+	gnutls_free(data);
+
+	return 0;
+}
+
+static int proc_dhe_rsa_client_kx( GNUTLS_STATE state, opaque* data, int data_size) {
+	uint16 n_Y;
+	size_t _n_Y;
+	MPI g, p;
+	const X509PKI_CREDENTIALS cred;
+	int bits;
+	
+	cred = _gnutls_get_kx_cred( state->gnutls_key, GNUTLS_X509PKI, NULL);
+	if (cred==NULL) {
+		bits = DEFAULT_BITS; /* default */
+	} else {
+		bits = cred->dh_bits;
+	}
+
+#if 0 /* removed. I do not know why - maybe I didn't get the protocol,
+       * but openssl does not use that byte
+       */
+	if (data[0] != 1) {
+		gnutls_assert();
+		return GNUTLS_E_UNIMPLEMENTED_FEATURE;
+	}
+#endif
+
+	n_Y = READuint16( &data[0]);
+	_n_Y = n_Y;
+
+	if (gcry_mpi_scan(&state->gnutls_key->client_Y,
+		      GCRYMPI_FMT_USG, &data[2], &_n_Y)) {
+		gnutls_assert();
+		return GNUTLS_E_MPI_SCAN_FAILED;
+	}
+
+	g = gnutls_get_dh_params(&p, bits);
+	state->gnutls_key->KEY = gnutls_calc_dh_key( state->gnutls_key->client_Y, state->gnutls_key->dh_secret, p);
+	
+	_gnutls_mpi_release(&g);
+	_gnutls_mpi_release(&p);
+
+	_gnutls_mpi_release(&state->gnutls_key->client_Y);
+	_gnutls_mpi_release(&state->gnutls_key->dh_secret);
+
+	return 0;
+}
+
+
