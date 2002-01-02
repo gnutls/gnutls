ff9a02c7dbf287e5f86a512a847de57cc4d45560
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-01-02 16:33:58 +0000

    *** empty log message ***

--- lib/gnutls_int.h
+++ lib/gnutls_int.h
@@ -30,10 +30,10 @@
 #define HARD_DEBUG
 #define WRITE_DEBUG
 #define READ_DEBUG
-#define RECORD_DEBUG*/
 #define HANDSHAKE_DEBUG // Prints some information on handshake 
+#define RECORD_DEBUG
 #define DEBUG
-
+*/
 
 /* It might be a good idea to replace int with void*
  * here.
