54cb6673dc39186539455468a925fdbac222c171
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-01-26 16:10:46 +0000

    added _gnutls_mem_cpy()

--- lib/gnutls_str.h
+++ lib/gnutls_str.h
@@ -1,2 +1,3 @@
 void _gnutls_str_cpy( char* dest, size_t dest_tot_size, const char* src);
+void _gnutls_mem_cpy( char* dest, size_t dest_tot_size, const char* src, size_t src_size);
 void _gnutls_str_cat( char* dest, size_t dest_tot_size, const char* src);
