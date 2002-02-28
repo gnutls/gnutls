afcb686771ec88bdb2106f5521a5a6f200b14a4e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-02-28 07:00:13 +0000

    added missing files

---
+++ src/tls_test-gaa.h
@@ -0,0 +1,37 @@
+
+#line 104 "gaa.skel"
+/* GAA HEADER */
+#ifndef GAA_HEADER_POKY
+#define GAA_HEADER_POKY
+
+typedef struct _gaainfo gaainfo;
+
+struct _gaainfo
+{
+#line 9 "tls_test.gaa"
+	char **rest_args;
+#line 8 "tls_test.gaa"
+	int nrest_args;
+#line 3 "tls_test.gaa"
+	int pp;
+
+#line 114 "gaa.skel"
+};
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+    int gaa(int argc, char *argv[], gaainfo *gaaval);
+
+    void gaa_help();
+    
+    int gaa_file(char *name, gaainfo *gaaval);
+    
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
