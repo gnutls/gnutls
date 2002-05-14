ad4d43c60528ee9d7b36a0aa8800de1b953c1080
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-05-14 11:40:01 +0000

    License changed to LGPL

--- lib/debug.c
+++ lib/debug.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 #include "gnutls_int.h"
 #include "gnutls_errors.h"
+#include <gcrypt.h>
 
 #ifdef DEBUG
 
@@ -30,7 +31,7 @@ void _gnutls_dump_mpi(char* prefix, GNUTLS_MPI a)
 	char buf[400];
 	size_t n = sizeof buf;
 	
-	if (gcry_mpi_print(GCRYGNUTLS_MPI_FMT_HEX, buf, &n, a))
+	if (gcry_mpi_print(GCRYMPI_FMT_HEX, buf, &n, a))
 		strcpy(buf, "[can't print value]"); /* Flawfinder: ignore */
 	_gnutls_log( "GNUTLS_MPI: length: %d\n\t%s%s\n", (n-1)/2, prefix, buf);
 }
