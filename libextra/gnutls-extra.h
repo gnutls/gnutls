88619e99001cfddcdb3dc1df2f419bd1a63017b7
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-06-16 10:07:37 +0000

    *** empty log message ***

--- libextra/gnutls-extra.h
+++ libextra/gnutls-extra.h
@@ -1,9 +1,7 @@
 /*
  *      Copyright (C) 2002 Nikos Mavroyanopoulos
  *
- * This file is part of GNUTLS.
- *
- * GNUTLS is free software; you can redistribute it and/or modify
+ * GNUTLS-EXTRA is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
