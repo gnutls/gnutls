e953612fab306a0c04bbb105e007b3098c98923e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-10-09 21:40:44 +0000

    cleanups

--- lib/x509_xml.c
+++ lib/x509_xml.c
@@ -1,9 +1,9 @@
 /*
- *      Copyright (C) 2002 Nikos Mavroyanopoulos
+ * Copyright (C) 2002 Nikos Mavroyanopoulos
  *
  * This file is part of GNUTLS.
  *
- * The LIBTASN1 library is free software; you can redistribute it and/or
+ * The GNUTLS library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public   
  * License as published by the Free Software Foundation; either 
  * version 2.1 of the License, or (at your option) any later version.
