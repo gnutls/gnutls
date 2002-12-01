3b021f80721304448214a523d743dac979ecb0be
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2002-12-01 20:15:27 +0000

    removed all files related to srpsha1 encoding. The are not needed any more.

--- libextra/crypt.h
+++
@@ -1,4 +0,0 @@
-/* crypt functions */
-
-char * _gnutls_srp_crypt(const char* username, const char *passwd, int salt, MPI g, MPI n);
-int _gnutls_srp_crypt_vrfy(const char* username, const char *passwd, char* salt, MPI g, MPI n);
--- libextra/crypt_srpsha1.h
+++
@@ -1,3 +0,0 @@
-char *_gnutls_crypt_srpsha1_wrapper(const char* username, const char *pass_new, int salt, MPI g, MPI n);
-char *_gnutls_crypt_srpsha1(const char *username, const char *passwd,
-		    const char *salt, GNUTLS_MPI g, GNUTLS_MPI n);
