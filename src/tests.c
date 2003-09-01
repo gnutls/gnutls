aa81cab5ba496f59164bc66c2f74b70cfefd4a41
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2003-09-01 13:52:02 +0000

    corrected bug which made some tests now to be compiled.

--- src/tests.c
+++ src/tests.c
@@ -18,6 +18,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
  */
 
+#include <config.h>
 #include <gnutls/gnutls.h>
 #include <gnutls/extra.h>
 #include <gnutls/x509.h>
