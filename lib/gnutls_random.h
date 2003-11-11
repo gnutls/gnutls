ef3cac8d81098ce7971bae4e18039d0afb301afe
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2003-11-11 21:33:36 +0000

    *** empty log message ***

--- lib/gnutls_random.h
+++ lib/gnutls_random.h
@@ -1,11 +1,6 @@
 #include "defines.h"
 
-#ifndef USE_GCRYPT
-# define GNUTLS_WEAK_RANDOM 0 
-# define GNUTLS_STRONG_RANDOM 1
-#else
-# define GNUTLS_WEAK_RANDOM GCRY_WEAK_RANDOM
-# define GNUTLS_STRONG_RANDOM GCRY_STRONG_RANDOM
-#endif
+#define GNUTLS_WEAK_RANDOM GCRY_WEAK_RANDOM
+#define GNUTLS_STRONG_RANDOM GCRY_STRONG_RANDOM
 
 int _gnutls_get_random(opaque* res, int bytes, int);
