cafe9370351eaaa85a87eb94dbc4df25be20e821
Simon Josefsson <simon@josefsson.org>
2004-08-22 09:04:37 +0000

    Add self test of generic crypto.

---
+++ crypto/utils.h
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2004 Free Software Foundation
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <string.h>
+# include <stdarg.h>
+
+extern int debug;
+extern int error_count;
+extern int break_on_error;
+
+extern void fail (const char *format, ...);
+extern void success (const char *format, ...);
+
+extern void escapeprint (const char *str, size_t len);
+extern void hexprint (const char *str, size_t len);
+extern void binprint (const char *str, size_t len);
+
+/* This must be implemented elsewhere. */
+extern void doit (void);
+
+#endif /* UTILS_H */
