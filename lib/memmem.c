c108498f15557d7f9ffead66d58d095d2526ae73
Simon Josefsson <simon@josefsson.org>
2004-10-05 19:34:35 +0000

    Don't link to gnulib in lib/ until issues are solved (see bug-gnulib). Move memmem from gl/ to lib/, since it is the only gnulib module lib/ needs.

--- lib/memmem.c
+++ lib/memmem.c
@@ -2,16 +2,16 @@
    This file is part of the GNU C Library.
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation; either version 2.1, or (at your option)
    any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public License along
+   You should have received a copy of the GNU Lesser General Public License along
    with this program; if not, write to the Free Software Foundation,
    Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
@@ -24,6 +24,7 @@
 
 #undef memmem
 
+#ifndef HAVE_MEMMEM
 /* Return the first occurrence of NEEDLE in HAYSTACK.  */
 void *
 memmem (haystack, haystack_len, needle, needle_len)
@@ -55,3 +56,4 @@ memmem (haystack, haystack_len, needle, needle_len)
 
   return NULL;
 }
+#endif
