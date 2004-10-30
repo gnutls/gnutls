89ebc0e6cbd6b290caa2f0645d2a114ea30d21ea
Simon Josefsson <simon@josefsson.org>
2004-10-30 01:21:49 +0000

    Add.

---
+++ tests/openssl.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2004 Free Software Foundation
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdio.h>
+
+#include "utils.h"
+
+#include <gnutls/openssl.h>
+
+void
+doit (void)
+{
+  MD5_CTX c;
+  unsigned char md[MD5_DIGEST_LENGTH];
+
+  if (!gnutls_check_version (LIBGNUTLS_VERSION))
+    success ("gnutls_check_version ERROR\n");
+
+  MD5_Init(&c);
+  MD5_Update(&c, "abc", 3);
+  MD5_Final(&(md[0]),&c);
+
+  if (memcmp (md, "\x90\x01\x50\x98\x3c\xd2\x4f\xb0"
+	      "\xd6\x96\x3f\x7d\x28\xe1\x7f\x72", sizeof(md)) != 0)
+    {
+      hexprint (md, sizeof(md));
+      fail ("MD5 failure\n");
+    }
+  else
+    success ("MD5 OK\n");
+}
