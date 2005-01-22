9c8526f72aa6399cdf74959dfde92c7dfaa92342
Simon Josefsson <simon@josefsson.org>
2005-01-22 15:41:50 +0000

    Clean up license templates.

--- libextra/openpgp/pgpverify.c
+++ libextra/openpgp/pgpverify.c
@@ -1,23 +1,24 @@
 /*
- *  Copyright (C) 2002 Timo Schulz
- *  Portions Copyright (C) 2003 Nikos Mavroyanopoulos
- *  Copyright 2004 Free Software Foundation
+ * Copyright (C) 2002, 2003, 2004, 2005 Free Software Foundation
  *
- *  This file is part of GNUTLS-EXTRA.
+ * Author: Timo Schulz, Nikos Mavroyanopoulos
  *
- *  The GNUTLS library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public   
- *  License as published by the Free Software Foundation; either 
- *  version 2.1 of the License, or (at your option) any later version.
+ * This file is part of GNUTLS-EXTRA.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ * GNUTLS-EXTRA is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * GNUTLS-EXTRA is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA
  *
  */
 
