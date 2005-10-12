e1ad17c1d1991c32d57f3606bc4fcf0701959d3f
Simon Josefsson <simon@josefsson.org>
2005-10-12 15:00:20 +0000

    Remove unused prototypes.

--- lib/ext_server_name.h
+++ lib/ext_server_name.h
@@ -26,10 +26,3 @@ int _gnutls_server_name_recv_params(gnutls_session_t session,
 				    const opaque * data, size_t data_size);
 int _gnutls_server_name_send_params(gnutls_session_t session,
 				    opaque * data, size_t);
-
-int gnutls_get_server_name(gnutls_session_t session, void *data,
-			   int *data_length, int *type, int indx);
-
-int gnutls_set_server_name(gnutls_session_t session,
-			   gnutls_server_name_type_t type,
-			   const void *name, int name_length);
