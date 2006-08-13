5f1362d36aa7a1418961abd9ff85bb333e137ffe
Simon Josefsson <simon@josefsson.org>
2006-08-13 20:32:27 +0000

    Mark with XXX.

--- libextra/gnutls_openssl.c
+++ libextra/gnutls_openssl.c
@@ -31,7 +31,7 @@
 #include <string.h>
 #include <gnutls/openssl.h>
 
-/* See lib/gnutls_int.h. */
+/* XXX: See lib/gnutls_int.h. */
 #define GNUTLS_POINTER_TO_INT(_) ((int) GNUTLS_POINTER_TO_INT_CAST (_))
 #define GNUTLS_INT_TO_POINTER(_) ((void*) GNUTLS_POINTER_TO_INT_CAST (_))
 
