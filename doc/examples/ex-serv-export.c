8d6b598678f2a550d869f2f11d4c963182a40256
Simon Josefsson <simon@josefsson.org>
2006-08-27 20:50:13 +0000

    Include netinet/in.h for FreeBSD, reported by Roman Bogorodskiy <novel@FreeBSD.org>.

--- doc/examples/ex-serv-export.c
+++ doc/examples/ex-serv-export.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
 #include <gnutls/gnutls.h>
--- doc/examples/tcp.c
+++ doc/examples/tcp.c
@@ -8,6 +8,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <unistd.h>
 
 #define SA struct sockaddr
