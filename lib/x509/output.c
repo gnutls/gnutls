387fb7fe8645046de24140988b3ec61a6cc482ce
Simon Josefsson <simon@josefsson.org>
2007-02-09 13:00:27 +0000

    Really fix.

--- lib/x509/output.c
+++ lib/x509/output.c
@@ -521,7 +521,7 @@ print_cert (gnutls_string * str, gnutls_x509_crt_t cert)
       if (gmtime_r (&tim, &t) == NULL)
 	addf (str, "error: gmtime_r failed (%d)\n", t);
       else if (strftime (s, max, "%a %b %e %H:%M:%S %Z %Y", &t) == 0)
-	strcpy (s, "strftime failed (%d)\n", t);
+	addf (str, "error: strftime failed (%d)\n", t);
       else
 	addf (str, "\t\tNot Before: %s\n", s);
     }
@@ -535,7 +535,7 @@ print_cert (gnutls_string * str, gnutls_x509_crt_t cert)
       if (gmtime_r (&tim, &t) == NULL)
 	addf (str, "error: gmtime_r failed (%d)\n", t);
       else if (strftime (s, max, "%a %b %e %H:%M:%S %Z %Y", &t) == 0)
-	strcpy (s, "strftime failed (%d)\n", t);
+	addf (str, "error: strftime failed (%d)\n", t);
       else
 	addf (str, "\t\tNot After: %s\n", s);
     }
