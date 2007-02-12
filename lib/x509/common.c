13374810f1f79be2e658ab207381594aa03c62f5
Simon Josefsson <simon@josefsson.org>
2007-02-12 16:54:53 +0000

    (_gnutls_x509_san_find_type): Support GNUTLS_SAN_DN.

--- lib/x509/common.c
+++ lib/x509/common.c
@@ -699,6 +699,8 @@ _gnutls_x509_san_find_type (char *str_type)
     return GNUTLS_SAN_IPADDRESS;
   if (strcmp (str_type, "otherName") == 0)
     return GNUTLS_SAN_OTHERNAME;
+  if (strcmp (str_type, "directoryName") == 0)
+    return GNUTLS_SAN_DN;
   return (gnutls_x509_subject_alt_name_t) - 1;
 }
 
