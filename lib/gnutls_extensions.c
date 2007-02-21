c6aa45948924cc973aec726f2aaa3182e4e486c7
Simon Josefsson <simon@josefsson.org>
2007-02-21 17:29:56 +0000

    Add authz extension.

--- lib/gnutls_extensions.c
+++ lib/gnutls_extensions.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2001, 2002, 2003, 2004, 2005 Free Software Foundation
+ * Copyright (C) 2001, 2002, 2003, 2004, 2005, 2007 Free Software Foundation
  *
  * Author: Nikos Mavroyanopoulos
  *
@@ -35,6 +35,7 @@
 #include <ext_server_name.h>
 #include <ext_srp.h>
 #include <ext_inner_application.h>
+#include <ext_authz.h>
 #include <gnutls_num.h>
 
 /* Key Exchange Section */
@@ -49,6 +50,12 @@ gnutls_extension_entry _gnutls_extensions[MAX_EXT_SIZE] = {
   GNUTLS_EXTENSION_ENTRY (GNUTLS_EXTENSION_MAX_RECORD_SIZE,
 			  _gnutls_max_record_recv_params,
 			  _gnutls_max_record_send_params),
+  GNUTLS_EXTENSION_ENTRY (GNUTLS_EXTENSION_AUTHZ_CLIENT,
+			  _gnutls_authz_ext_client_recv_params,
+			  _gnutls_authz_ext_client_send_params),
+  GNUTLS_EXTENSION_ENTRY (GNUTLS_EXTENSION_AUTHZ_SERVER,
+			  _gnutls_authz_ext_server_recv_params,
+			  _gnutls_authz_ext_server_send_params),
   GNUTLS_EXTENSION_ENTRY (GNUTLS_EXTENSION_CERT_TYPE,
 			  _gnutls_cert_type_recv_params,
 			  _gnutls_cert_type_send_params),
