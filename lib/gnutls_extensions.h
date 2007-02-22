984d0b3ef6433f294c1a62f4fb70d20f9be6140c
Simon Josefsson <simon@josefsson.org>
2007-02-22 07:43:40 +0000

    Add prototype for _gnutls_extension_list_check, we use it in ext_authz.c.

--- lib/gnutls_extensions.h
+++ lib/gnutls_extensions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005 Free Software Foundation
+ * Copyright (C) 2000, 2001, 2002, 2003, 2004, 2005, 2007 Free Software Foundation
  *
  * Author: Nikos Mavroyanopoulos
  *
@@ -28,6 +28,7 @@ const char *_gnutls_extension_get_name (uint16_t type);
 int _gnutls_parse_extensions (gnutls_session_t, const opaque *, int);
 int _gnutls_gen_extensions (gnutls_session_t session, opaque * data,
 			    size_t data_size);
+int _gnutls_extension_list_check (gnutls_session_t session, uint16_t type);
 
 typedef int (*ext_recv_func) (gnutls_session_t, const opaque *, size_t);	/* recv data */
 typedef int (*ext_send_func) (gnutls_session_t, opaque *, size_t);	/* send data */
