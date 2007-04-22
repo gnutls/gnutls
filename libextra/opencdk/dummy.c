81e7bfcfabacd3187a85842b7ebc9ea52be972f8
Timo Schulz <twoaday@gnutls.org>
2007-04-22 14:26:14 +0000

    *** empty log message ***

---
+++ libextra/opencdk/dummy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "opencdk.h"
+#include "main.h"
+#include "filters.h"
+#include "packet.h"
+
+cdk_error_t
+_cdk_proc_packets (cdk_ctx_t hd, cdk_stream_t inp, cdk_stream_t data,
+		   const char *output, cdk_stream_t outstream,
+		   gcry_md_hd_t md)
+{
+  return 0;
+}
+
