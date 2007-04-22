9869cbab270379674fb51838aa5ee0d3d12a3c06
Simon Josefsson <simon@josefsson.org>
2007-04-22 14:27:35 +0000

    Add, for PKCS#11.

---
+++ includes/gnutls/pkcs11.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2007 Free Software Foundation
+ *
+ * Author: Simon Josefsson
+ *
+ * This file is part of GNUTLS-PKCS11.
+ *
+ * GNUTLS-PKCS11 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * GNUTLS-PKCS11 is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNUTLS-PKCS11; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ */
+
+/* Note the libgnutls-extra is not a standalone library. It requires
+ * to link also against libgnutls.
+ */
+
+#ifndef GNUTLS_PKCS11_H
+# define GNUTLS_PKCS11_H
+
+# include <gnutls/gnutls.h>
+
+# ifdef __cplusplus
+extern "C"
+{
+# endif
+
+# define LIBGNUTLS_PKCS11_VERSION LIBGNUTLS_VERSION
+
+  int gnutls_certificate_set_pkcs11 (gnutls_certificate_credentials_t res);
+
+# ifdef __cplusplus
+}
+# endif
+#endif
