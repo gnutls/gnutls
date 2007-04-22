127087295389ebc818a777491afe4162eb77c578
Simon Josefsson <simon@josefsson.org>
2007-04-22 14:22:24 +0000

    Add PKCS#11 glue.

---
+++ pkcs11/gnutls_pkcs11.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2007 Free Software Foundation
+ *
+ * Author: Simon Josefsson
+ *
+ * This file is part of GNUTLS-PKCS11.
+ *
+ * GNUTLS-PKCS11 is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * GNUTLS-PKCS11 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNUTLS-PKCS11; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ */
+
+#include "gnutls_int.h"
+
+int
+gnutls_certificate_set_pkcs11 (gnutls_certificate_credentials_t res)
+{
+  return 0;
+}
