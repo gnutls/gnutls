11a524b75b3a8194b2242273dd290d15b09755fd
Simon Josefsson <simon@josefsson.org>
2007-05-02 09:24:34 +0000

    New file.

---
+++ tests/pkcs11/pkcs11-ca.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2007 Free Software Foundation
+ *
+ * Author: Simon Josefsson
+ *
+ * This file is part of GNUTLS.
+ *
+ * GNUTLS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * GNUTLS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNUTLS; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifdef HAVE_CONFIG_H
+# include <config.h>
+#endif
+
+#include <stdlib.h>
+
+#include <gnutls/pkcs11.h>
+
+#include "utils.h"
+
+void
+doit (void)
+{
+  int ret;
+  gnutls_x509_crt_t *ca_list;
+  unsigned int ncas;
+
+  ret = gnutls_global_init ();
+  if (ret < 0)
+    fail ("gnutls_global_init failed %d\n", ret);
+
+  ret = gnutls_pkcs11_get_ca_certificates (&ca_list, &ncas);
+  if (ret < 0)
+    fail ("Error getting CAs from PKCS#11: %d\n", ret);
+
+  gnutls_global_deinit ();
+}
