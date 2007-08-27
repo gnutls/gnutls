eda3469efbeaa62cf708c598441f87d0a5f51686
Simon Josefsson <simon@josefsson.org>
2007-08-27 16:37:53 +0200

    Support GNUTLS_CRD_PSK and GNUTLS_CRD_IA.

--- doc/examples/ex-session-info.c
+++ doc/examples/ex-session-info.c
@@ -31,9 +31,20 @@ print_info (gnutls_session_t session)
   cred = gnutls_auth_get_type (session);
   switch (cred)
     {
+    case GNUTLS_CRD_IA:
+      printf ("- TLS/IA session\n");
+      break;
+
+
     case GNUTLS_CRD_SRP:
       printf ("- SRP session with username %s\n",
 	      gnutls_srp_server_get_username (session));
+      break;
+
+    case GNUTLS_CRD_PSK:
+      if (gnutls_psk_server_get_username (session) != NULL)
+	printf ("- PSK authentication. Connected as '%s'\n",
+		gnutls_psk_server_get_username (session));
       break;
 
     case GNUTLS_CRD_ANON:	/* anonymous authentication */
