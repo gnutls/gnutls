b01c59681ab6cc941426adbf195e0edc74b2da6d
Simon Josefsson <simon@josefsson.org>
2007-10-27 21:09:25 +0200

    Update gnulib files.

--- gl/strerror.c
+++ gl/strerror.c
@@ -4,7 +4,7 @@
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 3 of the License, or
+   the Free Software Foundation; either version 2 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -35,7 +35,7 @@ rpl_strerror (int n)
 {
   char *result = strerror (n);
 
-  if (! result)
+  if (result == NULL || result[0] == '\0')
     {
       static char const fmt[] = "Unknown error (%d)";
       static char mesg[sizeof fmt + INT_STRLEN_BOUND (n)];
