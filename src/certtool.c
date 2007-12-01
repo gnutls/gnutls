b6e4b1ff3f7ef8a8d26f2e89c0bc50d2fc9d23f4
Nikos <nmav@crystal.(none)>
2007-12-01 08:25:34 +0200

    corrected typos.

--- src/certtool.c
+++ src/certtool.c
@@ -32,7 +32,7 @@
 #include <gnutls/pkcs12.h>
 #include <unistd.h>
 #include <certtool-cfg.h>
-
+#include <gcrypt.h>
 #include <errno.h>
 
 /* Gnulib portability files. */
@@ -126,7 +126,7 @@ generate_private_key_int (void)
     {
       key_type = GNUTLS_PK_DSA;
       /* FIXME: Remove me once we depend on 1.3.x */
-      if (info.bits > 1024 && gcrypt_check_version("1.3.1")==NULL)
+      if (info.bits > 1024 && gcry_check_version("1.3.1")==NULL)
         info.bits = 1024;
     }
   else
