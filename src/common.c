452de7f169b11905d1d24bd97144c0dbfacb479e
Simon Josefsson <simon@josefsson.org>
2007-12-09 22:48:00 +0100

    Use GPLv3 in src/.

--- src/common.c
+++ src/common.c
@@ -689,7 +689,7 @@ print_license (void)
   fputs ("\nCopyright (C) 2004,2005,2006,2007 Free Software Foundation\n"
 	 "This program is free software; you can redistribute it and/or modify \n"
 	 "it under the terms of the GNU General Public License as published by \n"
-	 "the Free Software Foundation; either version 2 of the License, or \n"
+	 "the Free Software Foundation; either version 3 of the License, or \n"
 	 "(at your option) any later version. \n" "\n"
 	 "This program is distributed in the hope that it will be useful, \n"
 	 "but WITHOUT ANY WARRANTY; without even the implied warranty of \n"
--- src/serv.c
+++ src/serv.c
@@ -1517,7 +1517,7 @@ print_serv_license (void)
 	 "\nCopyright (C) 2004 Free Software Foundation\n"
 	 "This program is free software; you can redistribute it and/or modify \n"
 	 "it under the terms of the GNU General Public License as published by \n"
-	 "the Free Software Foundation; either version 2 of the License, or \n"
+	 "the Free Software Foundation; either version 3 of the License, or \n"
 	 "(at your option) any later version. \n" "\n"
 	 "This program is distributed in the hope that it will be useful, \n"
 	 "but WITHOUT ANY WARRANTY; without even the implied warranty of \n"
