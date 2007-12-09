f7405cbf9f110a17845eb710b62ed7e992965884
Simon Josefsson <simon@josefsson.org>
2007-12-09 22:53:31 +0100

    GPLv3 fixes.

--- includes/gnutls/openssl.h
+++ includes/gnutls/openssl.h
@@ -1,12 +1,12 @@
 /*
- * Copyright (C) 2004, 2005 Free Software Foundation
+ * Copyright (C) 2004, 2005, 2007 Free Software Foundation
  * Copyright (c) 2002 Andrew McDonald <andrew@mcdonald.org.uk>
  *
  * This file is part of GNUTLS-EXTRA.
  *
  * GNUTLS-EXTRA is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
+ * published by the Free Software Foundation; either version 3 of the
  * License, or (at your option) any later version.
  *
  * GNUTLS-EXTRA is distributed in the hope that it will be useful, but
