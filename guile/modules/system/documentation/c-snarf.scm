8e806579b882aecb8ef05ae1f0ce3d3e0f2e874f
Simon Josefsson <simon@josefsson.org>
2007-12-09 23:02:14 +0100

    More GPLv3 fixes.

--- guile/modules/system/documentation/c-snarf.scm
+++ guile/modules/system/documentation/c-snarf.scm
@@ -1,11 +1,11 @@
 ;;; c-snarf.scm  --  Parsing documentation "snarffed" from C files.
 ;;;
-;;; Copyright 2006  Free Software Foundation
+;;; Copyright 2006, 2007  Free Software Foundation
 ;;;
 ;;;
 ;;; This program is free software; you can redistribute it and/or modify
 ;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation; either version 2 of the License, or
+;;; the Free Software Foundation; either version 3 of the License, or
 ;;; (at your option) any later version.
 ;;;
 ;;; This program is distributed in the hope that it will be useful,
