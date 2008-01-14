ecc8945d6535d47f08177ef545a9aff52063dff7
Nikos <nmav@crystal.(none)>
2008-01-14 21:27:49 +0200

    reverted the addition of gnutls_openpgp_get_key_id().

--- includes/gnutls/openpgp.h
+++ includes/gnutls/openpgp.h
@@ -75,8 +75,6 @@ extern "C"
 
   int gnutls_openpgp_crt_get_id (gnutls_openpgp_crt_t key,
 				 unsigned char keyid[8]);
-  int gnutls_openpgp_crt_get_key_id (gnutls_openpgp_crt_t key,
-				 unsigned char keyid[8]);
 
   int gnutls_openpgp_crt_check_hostname (gnutls_openpgp_crt_t key,
 					 const char *hostname);
