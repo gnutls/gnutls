2d0a32c2696554d208d17faf6df5b4f4fae34ada
Simon Josefsson <simon@josefsson.org>
2008-02-18 15:26:59 +0100

    Remove all external APIs already declared in includes/gnutls/openpgp.h.

--- lib/openpgp/openpgp.h
+++ lib/openpgp/openpgp.h
@@ -33,50 +33,15 @@ typedef struct gnutls_openpgp_keyring_int
 } gnutls_openpgp_keyring_int;
 
 int _gnutls_map_cdk_rc (int rc);
-int gnutls_openpgp_crt_get_name (gnutls_openpgp_crt_t key,
-				 int idx, char *buf, size_t * sizeof_buf);
-int gnutls_openpgp_crt_get_fingerprint (gnutls_openpgp_crt_t key,
-					void *fpr, size_t * fprlen);
-gnutls_pk_algorithm_t
-gnutls_openpgp_crt_get_pk_algorithm (gnutls_openpgp_crt_t key,
-				     unsigned int *bits);
-int gnutls_openpgp_crt_get_version (gnutls_openpgp_crt_t key);
-time_t gnutls_openpgp_crt_get_creation_time (gnutls_openpgp_crt_t key);
-time_t gnutls_openpgp_crt_get_expiration_time (gnutls_openpgp_crt_t key);
-int gnutls_openpgp_crt_get_id (gnutls_openpgp_crt_t key,
-			       gnutls_openpgp_keyid_t* keyid);
 
-int gnutls_openpgp_crt_init (gnutls_openpgp_crt_t * key);
-void gnutls_openpgp_crt_deinit (gnutls_openpgp_crt_t key);
-int gnutls_openpgp_crt_import (gnutls_openpgp_crt_t key,
-			       const gnutls_datum_t * data,
-			       gnutls_openpgp_crt_fmt_t format);
 int _gnutls_openpgp_export (cdk_kbnode_t node,
 			       gnutls_openpgp_crt_fmt_t format,
 			       void *output_data, size_t * output_data_size);
-
-void gnutls_openpgp_keyring_deinit (gnutls_openpgp_keyring_t keyring);
-int gnutls_openpgp_keyring_init (gnutls_openpgp_keyring_t * keyring);
-int gnutls_openpgp_keyring_import (gnutls_openpgp_keyring_t keyring,
-				   const gnutls_datum_t * data,
-				   gnutls_openpgp_crt_fmt_t format);
-int gnutls_openpgp_keyring_check_id (gnutls_openpgp_keyring_t ring,
-				     gnutls_openpgp_keyid_t keyid,
-				     unsigned int flags);
-
-int gnutls_openpgp_crt_verify_ring (gnutls_openpgp_crt_t key,
-				    gnutls_openpgp_keyring_t keyring,
-				    unsigned int flags, unsigned int *verify);
-
-int gnutls_openpgp_crt_verify_self (gnutls_openpgp_crt_t key,
-				    unsigned int flags, unsigned int *verify);
 
 int _gnutls_openpgp_crt_to_gcert (gnutls_cert * gcert,
 				  gnutls_openpgp_crt_t cert, gnutls_openpgp_keyid_t keyid);
 int _gnutls_openpgp_privkey_to_gkey (gnutls_privkey * dest,
 				     gnutls_openpgp_privkey_t src, gnutls_openpgp_keyid_t);
-
-void gnutls_openpgp_privkey_deinit (gnutls_openpgp_privkey_t key);
 
 cdk_packet_t _gnutls_get_valid_subkey(cdk_kbnode_t knode, int key_type);
 
