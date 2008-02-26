00d1ec90451646a58d3904ef3375b68c21fd4591
Simon Josefsson <simon@josefsson.org>
2008-02-26 11:01:33 +0100

    No need for rfc2818.h.

--- lib/openpgp/privkey.c
+++ lib/openpgp/privkey.c
@@ -9,12 +9,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *               
+ *
  * GNUTLS-EXTRA is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *                               
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -29,7 +29,6 @@
 #include <gnutls_num.h>
 #include <openpgp_int.h>
 #include <gnutls_openpgp.h>
-#include <x509/rfc2818.h>
 #include <gnutls_cert.h>
 
 /**
