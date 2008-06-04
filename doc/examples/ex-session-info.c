6aea6e159330e2a7a00817deff33546af37c8a18
Simon Josefsson <simon@josefsson.org>
2008-06-04 08:03:58 +0200

    Update example with more PSK printing.

--- doc/examples/ex-session-info.c
+++ doc/examples/ex-session-info.c
@@ -51,6 +51,13 @@ print_info (gnutls_session_t session)
 #endif
 
     case GNUTLS_CRD_PSK:
+      /* This returns NULL in server side.
+       */
+      if (gnutls_psk_client_get_hint (session) != NULL)
+	printf ("- PSK authentication. PSK hint '%s'\n",
+		gnutls_psk_client_get_hint (session));
+      /* This returns NULL in client side.
+       */
       if (gnutls_psk_server_get_username (session) != NULL)
 	printf ("- PSK authentication. Connected as '%s'\n",
 		gnutls_psk_server_get_username (session));
