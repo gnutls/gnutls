bc11d56f6461ff0a2afcae7f864bed54a7b337c1
Simon Josefsson <simon@josefsson.org>
2008-07-02 18:15:04 +0200

    Guile problem fixed.

--- cfg.mk
+++ cfg.mk
@@ -18,7 +18,7 @@
 # along with this file; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-CFGFLAGS ?= --enable-developer-mode --enable-gtk-doc --disable-guile
+CFGFLAGS ?= --enable-developer-mode --enable-gtk-doc
 
 INDENT_SOURCES = `find . -name \*.c|grep -v -e ^./lgl -e ^./gl -e ^./src/cfg -e -gaa.c -e asn1_tab.c`
 
