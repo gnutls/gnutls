ccfc7155f6ee1c21d56073ba3d114b55fa8cdad0
Simon Josefsson <simon@josefsson.org>
2008-09-02 10:26:13 +0200

    Don't return from a void function. Reported by Dave Uhring <duhring@charter.net>.

--- lib/gnutls_cipher_int.c
+++ lib/gnutls_cipher_int.c
@@ -173,8 +173,11 @@ _gnutls_cipher_deinit (cipher_hd_st* handle)
 {
   if (handle != NULL)
     {
-      if (handle->registered && handle->hd.rh.ctx != NULL) {
-        return handle->hd.rh.cc->deinit( handle->hd.rh.ctx);
+      if (handle->registered && handle->hd.rh.ctx != NULL)
+	{
+	  handle->hd.rh.cc->deinit( handle->hd.rh.ctx);
+	  return;
+	}
       }
       gc_cipher_close (handle->hd.gc);
     }
