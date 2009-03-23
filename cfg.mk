fe5597bd4c51cfce5328b0a869a35dd5cda64a6b
Simon Josefsson <simon@josefsson.org>
2009-03-23 18:12:53 +0100

    Fix bootstrap.

--- cfg.mk
+++ cfg.mk
@@ -32,9 +32,9 @@ autoreconf:
 	for f in $(PODIR)/*.po.in; do \
 		cp $$f `echo $$f | sed 's/.in//'`; \
 	done
-	mv lib/build-aux/config.rpath lib/build-aux/config.rpath-
+	mv build-aux/config.rpath build-aux/config.rpath-
 	test -f ./configure || autoreconf --install
-	mv lib/build-aux/config.rpath- lib/build-aux/config.rpath
+	mv build-aux/config.rpath- build-aux/config.rpath
 
 update-po: refresh-po
 	for f in `ls $(PODIR)/*.po | grep -v quot.po`; do \
