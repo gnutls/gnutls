13a6fe039e0ff39395b1d7f9afeaca1025035180
Simon Josefsson <simon@josefsson.org>
2009-05-28 11:39:23 +0200

    Fix paths for alpha release.

--- cfg.mk
+++ cfg.mk
@@ -102,16 +102,16 @@ prepare:
 upload:
 	git push
 	git push --tags
-	build-aux/gnupload --to ftp.gnu.org:$(PACKAGE) $(distdir).tar.bz2
-	scp $(distdir).tar.bz2 $(distdir).tar.bz2.sig igloo.linux.gr:~ftp/pub/gnutls/
-	ssh igloo.linux.gr 'cd ~ftp/pub/gnutls/ && sha1sum *.tar.bz2 > CHECKSUMS'
+	build-aux/gnupload --to alpha.gnu.org:$(PACKAGE) $(distdir).tar.bz2
+	scp $(distdir).tar.bz2 $(distdir).tar.bz2.sig igloo.linux.gr:~ftp/pub/gnutls/devel/
+	ssh igloo.linux.gr 'cd ~ftp/pub/gnutls/devel/ && sha1sum *.tar.bz2 > CHECKSUMS'
 	cp $(distdir).tar.bz2 $(distdir).tar.bz2.sig ../releases/$(PACKAGE)/
 
 web:
 	cd doc && ../build-aux/gendocs.sh --html "--css-include=texinfo.css" \
-		-o ../$(htmldir)/manual/ $(PACKAGE) $(PACKAGE_NAME)
-	cd doc/doxygen && doxygen && cd ../.. && cp -v doc/doxygen/html/* $(htmldir)/doxygen/ && cd doc/doxygen/latex && make refman.pdf && cd ../../../ && cp doc/doxygen/latex/refman.pdf $(htmldir)/doxygen/$(PACKAGE).pdf
-	cp -v doc/reference/html/*.html doc/reference/html/*.png doc/reference/html/*.devhelp doc/reference/html/*.css $(htmldir)/reference/
+		-o ../$(htmldir)/manual/devel/ $(PACKAGE) $(PACKAGE_NAME)
+	cd doc/doxygen && doxygen && cd ../.. && cp -v doc/doxygen/html/* $(htmldir)/doxygen/devel/ && cd doc/doxygen/latex && make refman.pdf && cd ../../../ && cp doc/doxygen/latex/refman.pdf $(htmldir)/doxygen/devel/$(PACKAGE).pdf
+	cp -v doc/reference/html/*.html doc/reference/html/*.png doc/reference/html/*.devhelp doc/reference/html/*.css $(htmldir)/reference/devel/
 
 upload-web:
 	cd $(htmldir) && cvs commit -m "Update." manual/ reference/ doxygen/
