9c8631c68a728584b46b7d2ceff2e872ae8a59dd
Jonathan Bastien-Filiatrault <joe@x2a.org>
2009-08-01 12:29:18 -0400

    Add version check function for selectable signature/hash certificate algorithms.

--- lib/gnutls_algorithms.c
+++ lib/gnutls_algorithms.c
@@ -1287,6 +1287,14 @@ _gnutls_version_has_selectable_sighash (gnutls_protocol_t version)
   return version == GNUTLS_TLS1_2;
 }
 
+/* This function determines if the version specified has selectable
+   signature/hash functions for certificate authentification. */
+int
+_gnutls_version_has_selectable_sighash (gnutls_protocol_t version)
+{
+  return version >= GNUTLS_TLS1_2;
+}
+
 /* This function determines if the version specified has support for
    TLS extensions. */
 int
