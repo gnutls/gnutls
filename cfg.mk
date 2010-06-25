5cac8de8d47bbc6cd65e33232257b0c7f09e3464
Simon Josefsson <simon@josefsson.org>
2010-06-25 08:25:03 +0200

    Release to non-alpha locations.

--- cfg.mk
+++ cfg.mk
@@ -106,9 +106,9 @@ prepare:
 upload:
 	git push
 	git push --tags
-	build-aux/gnupload --to alpha.gnu.org:$(PACKAGE) $(distdir).tar.bz2
+	build-aux/gnupload --to ftp.gnu.org:$(PACKAGE) $(distdir).tar.bz2
 	scp $(distdir).tar.bz2 $(distdir).tar.bz2.sig igloo.linux.gr:~ftp/pub/gnutls/devel/
-	ssh igloo.linux.gr 'cd ~ftp/pub/gnutls/devel/ && sha1sum *.tar.bz2 > CHECKSUMS'
+	ssh igloo.linux.gr 'cd ~ftp/pub/gnutls/ && sha1sum *.tar.bz2 > CHECKSUMS'
 	cp $(distdir).tar.bz2 $(distdir).tar.bz2.sig ../releases/$(PACKAGE)/
 
 web:
