1d91821a83ff5a80b2fbd77ecc6cc0af9e24c7ee
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-02-06 23:35:31 +0100

    check the error of hash set_key.

--- lib/gnutls_hash_int.c
+++ lib/gnutls_hash_int.c
@@ -267,11 +267,16 @@ _gnutls_hmac_init (digest_hd_st * dig, gnutls_mac_algorithm_t algorithm,
       return result;
     }
 
-  _gnutls_mac_ops.setkey (dig->handle, key, keylen);
-
   dig->hash = _gnutls_mac_ops.hash;
   dig->output = _gnutls_mac_ops.output;
   dig->deinit = _gnutls_mac_ops.deinit;
+
+  if (_gnutls_mac_ops.setkey (dig->handle, key, keylen) < 0)
+    {
+      gnutls_assert();
+      dig->deinit(dig->handle);
+      return GNUTLS_E_HASH_FAILED;
+    }
 
   return 0;
 }
