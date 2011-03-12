3b9a0c6c37dc9d68d5bcf190ce614898ae95e015
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-03-12 23:44:41 +0100

    do not print debugging output on non-fatal errors.

--- lib/ext_session_ticket.c
+++ lib/ext_session_ticket.c
@@ -716,7 +716,7 @@ _gnutls_recv_new_session_ticket (gnutls_session_t session)
                                 GNUTLS_HANDSHAKE_NEW_SESSION_TICKET,
                                 MANDATORY_PACKET, &buf);
   if (ret < 0)
-    return gnutls_assert_val(ret);
+    return gnutls_assert_val_fatal(ret);
 
   p = buf.data;
   data_size = buf.length;
