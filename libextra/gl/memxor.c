a523d2afa6b083401c589576b54ac5af91482647
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-04-13 20:03:57 +0200

    renamed memxor to gl_memxor.

--- libextra/gl/memxor.c
+++ libextra/gl/memxor.c
@@ -23,7 +23,7 @@
 #include "memxor.h"
 
 void *
-memxor (void *restrict dest, const void *restrict src, size_t n)
+gl_memxor (void *restrict dest, const void *restrict src, size_t n)
 {
   char const *s = src;
   char *d = dest;
