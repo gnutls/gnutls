5ec2c7d456483f147727d7e6e710cb285371b10e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-04-15 09:27:55 +0200

    Added interoperability tests with openssl.

--- tests/scripts/common.sh
+++ tests/scripts/common.sh
@@ -20,6 +20,7 @@
 
 fail() {
    echo "Failure: $1" >&2
+   kill $PID
    exit 1
 }
 
@@ -34,6 +35,21 @@ launch_server() {
        if [ "${LOCALRET}" != "0" -a "${LOCALRET}" != "143" ] ; then
                # Houston, we'v got a problem...
                echo "Failed to launch a gnutls-serv server !"
+               kill -10 ${PARENT}
+       fi
+}
+
+launch_bare_server() {
+       PARENT=$1;
+       shift;
+       $SERV $* >/dev/null 2>&1 &
+       LOCALPID="$!";
+       trap "[ ! -z \"${LOCALPID}\" ] && kill ${LOCALPID};" 15
+       wait "${LOCALPID}"
+       LOCALRET="$?"
+       if [ "${LOCALRET}" != "0" -a "${LOCALRET}" != "143" ] ; then
+               # Houston, we'v got a problem...
+               echo "Failed to launch server !"
                kill -10 ${PARENT}
        fi
 }
