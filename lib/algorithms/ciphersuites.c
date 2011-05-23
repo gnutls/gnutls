19e53ecaea095e0d4e6b3531a0e4a6d2f28e35fb
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-05-23 12:54:16 +0200

    corrected bug in ciphersuite name searching.

--- lib/algorithms/ciphersuites.c
+++ lib/algorithms/ciphersuites.c
@@ -662,14 +662,14 @@ gnutls_cipher_suite_get_name (gnutls_kx_algorithm_t kx_algorithm,
 {
   const char *ret = NULL;
 
-  /* avoid prefix */
   GNUTLS_CIPHER_SUITE_LOOP (
-    if (kx_algorithm == p->kx_algorithm &&
-        cipher_algorithm == p->block_algorithm && mac_algorithm == p->mac_algorithm)
-      {
-        ret = p->name + sizeof ("GNUTLS_") - 1);
-        break;
-      }
+      if (kx_algorithm == p->kx_algorithm &&
+          cipher_algorithm == p->block_algorithm && mac_algorithm == p->mac_algorithm)
+        {
+          ret = p->name + sizeof ("GNUTLS_") - 1;
+          break;
+        }
+  );
 
   return ret;
 }
