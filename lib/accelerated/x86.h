23df2cf3d4e719b51d6be784b0249b68139d1668
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-05-29 12:01:16 +0200

    Use cpuid.h if it exists, to use the x86 CPUID instruction.

--- lib/accelerated/x86.h
+++ lib/accelerated/x86.h
@@ -1,3 +1,12 @@
+#include <config.h>
+
+#ifdef HAVE_CPUID_H
+# include <cpuid.h>
+# define cpuid __cpuid
+
+#else
 #define cpuid(func,ax,bx,cx,dx)\
   __asm__ __volatile__ ("cpuid":\
   "=a" (ax), "=b" (bx), "=c" (cx), "=d" (dx) : "a" (func));
+
+#endif
