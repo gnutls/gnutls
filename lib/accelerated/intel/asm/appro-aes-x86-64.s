bc8910cb1cd4c4534119e71f6de842b0c5c61233
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-06-01 17:27:10 +0200

    typos and date fix in license.

--- lib/accelerated/intel/asm/appro-aes-x86-64.s
+++ lib/accelerated/intel/asm/appro-aes-x86-64.s
@@ -1,20 +1,19 @@
-# Copyright (c) 2006, Andy Polyakov by <appro@openssl.org>
+# Copyright (c) 2011, Andy Polyakov by <appro@openssl.org>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
 # are met:
 # 
-#     *        Redistributions of source code must retain copyright
-#     * notices,
+#     * Redistributions of source code must retain copyright notices,
 #      this list of conditions and the following disclaimer.
 #
-#     *        Redistributions in binary form must reproduce the above
+#     * Redistributions in binary form must reproduce the above
 #      copyright notice, this list of conditions and the following
 #      disclaimer in the documentation and/or other materials
 #      provided with the distribution.
 #
-#     *        Neither the name of the Andy Polyakov nor the names of its
+#     * Neither the name of the Andy Polyakov nor the names of its
 #      copyright holder and contributors may be used to endorse or
 #      promote products derived from this software without specific
 #      prior written permission.
