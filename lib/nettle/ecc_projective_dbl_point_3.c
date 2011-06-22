e04f3fa37f950f91b4280f32687e9a47b8bf5828
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-06-23 00:38:13 +0200

    Upgraded to LGPLv3.

--- lib/nettle/ecc_projective_dbl_point_3.c
+++ lib/nettle/ecc_projective_dbl_point_3.c
@@ -5,7 +5,7 @@
  *
  * The GNUTLS library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1 of
+ * as published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful, but
@@ -13,10 +13,8 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
- * USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
  */
 
