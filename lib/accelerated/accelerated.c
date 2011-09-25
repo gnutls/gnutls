a006a1f4b2f5d3d2efb951b72752a1ebb7e0af62
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2011-09-25 18:17:30 +0200

    Better usage of X86 conditionals to simplify and avoid an undefined warning in x86-32.

--- lib/accelerated/accelerated.c
+++ lib/accelerated/accelerated.c
@@ -23,7 +23,7 @@
 #include <accelerated.h>
 #if defined(ASM_X86_32) || defined(ASM_X86_64)
 # include <intel/aes-x86.h>
-# include <x86.h>
+# include <intel/x86.h>
 #endif
 
 void _gnutls_register_accel_crypto(void)
