a171181a4c1a39954a0868c2f77973ef4f8a360e
Simon Josefsson <simon@josefsson.org>
2011-11-08 21:18:21 +0100

    Improve syntax-check rules.

--- cfg.mk
+++ cfg.mk
@@ -40,19 +40,17 @@ local-checks-to-skip = sc_GPL_version sc_bindtextdomain			\
 	sc_require_config_h_first sc_texinfo_acronym sc_trailing_blank	\
 	sc_unmarked_diagnostics sc_useless_cpp_parens
 
-VC_LIST_ALWAYS_EXCLUDE_REGEX = ^maint.mk|(build-aux/|gl/|src/cfg/|tests/suite/ecore/|doc/protocol/).*
+VC_LIST_ALWAYS_EXCLUDE_REGEX = ^maint.mk|(build-aux/|gl/|src/cfg/|tests/suite/ecore/|doc/protocol/).*$$
 
 # Explicit syntax-check exceptions.
-exclude_file_name_regexp--sc_cast_of_alloca_return_value = ^guile/
-exclude_file_name_regexp--sc_error_message_uppercase = ^doc/examples/ex-cxx.cpp|guile/src/extra.c|src/certtool.c|tests/pkcs12_encode.c
-exclude_file_name_regexp--sc_file_system = ^doc/doxygen/Doxyfile
-exclude_file_name_regexp--sc_prohibit_cvs_keyword = ^lib/nettle/
-exclude_file_name_regexp--sc_prohibit_doubled_word = ^tests/rsa-md5-collision/README$$
-exclude_file_name_regexp--sc_prohibit_undesirable_word_seq = ^tests/nist-pkits/gnutls-nist-tests.html
-exclude_file_name_regexp--sc_space_tab = ^doc/.*.(pdf|png)|tests/nist-pkits/|tests/suite/x509paths/
-exclude_file_name_regexp--sc_two_space_separator_in_usage = ^doc/cha-programs.texi|tests/sha2/sha2|tests/sha2/sha2-dsa$$
+exclude_file_name_regexp--sc_cast_of_alloca_return_value = ^guile/modules/gnutls/build/priorities.scm|guile/src/core.c$$
 exclude_file_name_regexp--sc_error_message_period = ^src/crywrap/crywrap.c$$
-exclude_file_name_regexp--sc_error_message_uppercase = ^doc/examples/ex-cxx.cpp|guile/src/extra.c|src/certtool.c|src/crywrap/crywrap.c|tests/pkcs12_encode.c$$
+exclude_file_name_regexp--sc_error_message_uppercase = ^doc/examples/ex-cxx.cpp|guile/src/core.c|src/certtool.c|src/crywrap/crywrap.c|tests/pkcs12_encode.c$$
+exclude_file_name_regexp--sc_file_system = ^doc/doxygen/Doxyfile
+exclude_file_name_regexp--sc_prohibit_cvs_keyword = ^lib/nettle/.*$$
+exclude_file_name_regexp--sc_prohibit_undesirable_word_seq = ^tests/nist-pkits/gnutls-nist-tests.html$$
+exclude_file_name_regexp--sc_space_tab = ^gtk-doc.make|doc/.*.(pdf|png)|tests/nist-pkits/|tests/suite/x509paths/.*$$
+exclude_file_name_regexp--sc_two_space_separator_in_usage = ^doc/cha-programs.texi|tests/sha2/sha2|tests/sha2/sha2-dsa$$
 
 autoreconf:
 	for f in $(PODIR)/*.po.in; do \
