ab06513726eebecd815ea0ed40fed2c5351b5e23
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-01-14 10:43:17 +0100

    caption is set on enumerations in texinfo. Unfortunately they are not visible to the pdf output due to a probable bug in texinfo.

--- doc/scripts/split-texi.pl
+++ doc/scripts/split-texi.pl
@@ -21,7 +21,7 @@ sub key_of_record {
   my ($key) = $lines[$i]; 
 
   if ($enum == 1) {
-    while( !($key =~ m/^\@float Table,(.*)$/) && ($i < 5)) { $i=$i+1; $key = $lines[$i]; }
+    while( !($key =~ m/\@c\s(.*)\n/) && ($i < 5)) { $i=$i+1; $key = $lines[$i]; }
   } else {
     while( !($key =~ m/^\\functionTitle\{(.*)\}/) && ($i < 5)) { $i=$i+1; $key = $lines[$i]; }
   }
@@ -30,19 +30,24 @@ sub key_of_record {
 }
 
 if ($enum == 1) {
-  $/="\n\@end float";          # Records are separated by blank lines.
+  $/="\@end itemize";          # Records are separated by blank lines.
 } else {
   $/="\n\\end{function}";          # Records are separated by blank lines.
 }
 @records = <>;  # Read in whole file, one record per array element.
 
+$/="\n";
+
 mkdir $dir;
 
-@records = sort { key_of_record($a) cmp key_of_record($b) } @records;
+if ($enum == 0) {
+  @records = sort { key_of_record($a) cmp key_of_record($b) } @records;
+}
+
 foreach (@records) {
   $key = $_;
   if ($enum == 1) {
-    $key =~ m/\@float Table,(.*)/;
+    $key =~ m/\@c\s(.*)\n/;
     $key = $1;
   } else {
     $key =~ m/\\functionTitle\{(.*)\}/;
