419ad44e36f4e087190943ff72e711fc00042dd9
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-03-02 00:17:05 +0100

    Added SRP test.

---
+++ tests/srp/mini-srp.c
@@ -0,0 +1,271 @@
+/*
+ * Copyright (C) 2012 Free Software Foundation, Inc.
+ *
+ * Author: Nikos Mavrogiannopoulos
+ *
+ * This file is part of GnuTLS.
+ *
+ * GnuTLS is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GnuTLS is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GnuTLS; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#if defined(_WIN32)
+
+int main()
+{
+  exit(77);
+}
+
+#else
+
+#include <string.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <gnutls/gnutls.h>
+
+#include "utils.h"
+
+static void terminate(void);
+
+/* This program tests the rehandshake in DTLS
+ */
+
+static void
+server_log_func (int level, const char *str)
+{
+  fprintf (stderr, "server|<%d>| %s", level, str);
+}
+
+static void
+client_log_func (int level, const char *str)
+{
+  fprintf (stderr, "client|<%d>| %s", level, str);
+}
+
+/* A very basic TLS client, with anonymous authentication.
+ */
+
+
+static void
+client (int fd)
+{
+  int ret;
+  gnutls_session_t session;
+  gnutls_srp_client_credentials_t srp_cred;
+  /* Need to enable anonymous KX specifically. */
+
+  gnutls_global_init ();
+
+  if (debug)
+    {
+      gnutls_global_set_log_function (client_log_func);
+      gnutls_global_set_log_level (4711);
+    }
+
+  gnutls_srp_allocate_client_credentials (&srp_cred);
+  gnutls_srp_set_client_credentials (srp_cred, "test", "test");
+
+  /* Initialize TLS session
+   */
+  gnutls_init (&session, GNUTLS_CLIENT);
+
+  /* Use default priorities */
+  gnutls_priority_set_direct (session, "NORMAL:+SRP", NULL);
+
+  /* put the anonymous credentials to the current session
+   */
+  gnutls_credentials_set (session, GNUTLS_CRD_SRP, srp_cred);
+
+  gnutls_transport_set_ptr (session, (gnutls_transport_ptr_t) fd);
+
+  /* Perform the TLS handshake
+   */
+  do 
+    {
+      ret = gnutls_handshake (session);
+    }
+  while (ret < 0 && gnutls_error_is_fatal(ret) == 0);
+
+  if (ret < 0)
+    {
+      fail ("client: Handshake failed\n");
+      gnutls_perror (ret);
+      exit(1);
+    }
+  else
+    {
+      if (debug)
+        success ("client: Handshake was completed\n");
+    }
+
+  if (debug)
+    success ("client: TLS version is: %s\n",
+             gnutls_protocol_get_name (gnutls_protocol_get_version
+                                       (session)));
+
+  gnutls_bye (session, GNUTLS_SHUT_WR);
+
+  close (fd);
+
+  gnutls_deinit (session);
+
+  gnutls_srp_free_client_credentials (srp_cred);
+
+  gnutls_global_deinit ();
+}
+
+
+/* These are global */
+gnutls_srp_server_credentials_t s_srp_cred;
+pid_t child;
+
+static gnutls_session_t
+initialize_tls_session (void)
+{
+  gnutls_session_t session;
+
+  gnutls_init (&session, GNUTLS_SERVER);
+
+  /* avoid calling all the priority functions, since the defaults
+   * are adequate.
+   */
+  gnutls_priority_set_direct (session, "NORMAL:+SRP", NULL);
+
+  gnutls_credentials_set (session, GNUTLS_CRD_SRP, s_srp_cred);
+
+  return session;
+}
+
+static void terminate(void)
+{
+int status;
+
+  kill(child, SIGTERM);
+  wait(&status);
+  exit(1);
+}
+
+static void
+server (int fd)
+{
+int ret;
+gnutls_session_t session;
+
+  /* this must be called once in the program
+   */
+  gnutls_global_init ();
+
+  if (debug)
+    {
+      gnutls_global_set_log_function (server_log_func);
+      gnutls_global_set_log_level (4711);
+    }
+
+  gnutls_srp_allocate_server_credentials (&s_srp_cred);
+  gnutls_srp_set_server_credentials_file (s_srp_cred, "tpasswd",
+                                          "tpasswd.conf");
+
+  session = initialize_tls_session ();
+
+  gnutls_transport_set_ptr (session, (gnutls_transport_ptr_t) fd);
+
+  do 
+    {
+      ret = gnutls_handshake (session);
+    }
+  while (ret < 0 && gnutls_error_is_fatal(ret) == 0);
+  if (ret < 0)
+    {
+      close (fd);
+      gnutls_deinit (session);
+      fail ("server: Handshake has failed (%s)\n\n", gnutls_strerror (ret));
+      terminate();
+    }
+  if (debug)
+    success ("server: Handshake was completed\n");
+
+  if (debug)
+    success ("server: TLS version is: %s\n",
+             gnutls_protocol_get_name (gnutls_protocol_get_version
+                                       (session)));
+
+  /* do not wait for the peer to close the connection.
+   */
+  gnutls_bye (session, GNUTLS_SHUT_WR);
+
+  close (fd);
+  gnutls_deinit (session);
+
+  gnutls_srp_free_server_credentials (s_srp_cred);
+
+  gnutls_global_deinit ();
+
+  if (debug)
+    success ("server: finished\n");
+}
+
+static void start (void)
+{
+  int fd[2];
+  int ret;
+  
+  ret = socketpair(AF_LOCAL, SOCK_STREAM, 0, fd);
+  if (ret < 0)
+    {
+      perror("socketpair");
+      exit(1);
+    }
+
+  child = fork ();
+  if (child < 0)
+    {
+      perror ("fork");
+      fail ("fork");
+      exit(1);
+    }
+
+  if (child)
+    {
+      int status;
+      /* parent */
+      server (fd[0]);
+      wait (&status);
+      if (WEXITSTATUS(status) != 0)
+        fail("Child died with status %d\n", WEXITSTATUS(status));
+    }
+  else 
+    {
+      client (fd[1]);
+      exit(0);
+    }
+}
+
+void
+doit (void)
+{
+  start();
+}
+
+#endif /* _WIN32 */
