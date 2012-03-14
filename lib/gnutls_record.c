0afc9569e44a011494abcfe8e653fb888cfce386
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-03-14 22:44:39 +0100

    corrected call to gnutls_error_is_fatal().

--- lib/gnutls_record.c
+++ lib/gnutls_record.c
@@ -865,7 +865,7 @@ gnutls_datum_t raw; /* raw headers */
   if ((ret =
        _gnutls_io_read_buffered (session, record->header_size, -1)) != record->header_size)
     {
-      if (gnutls_error_is_fatal (ret) == 0)
+      if (ret < 0 && gnutls_error_is_fatal (ret) == 0)
         return ret;
       
       if (ret >= 0)
