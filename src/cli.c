e3f90ae0ffdf6e23f5ea031df84bdb588967c4c5
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-05-14 17:56:54 +0200

    Print certificate if --print-cert is given, even on verification failure.

--- src/cli.c
+++ src/cli.c
@@ -1288,6 +1288,7 @@ do_handshake (socket_st * socket)
     }
   else
     {
+      print_cert_info (socket->session, verbose, print_cert);
       gnutls_alert_send_appropriate (socket->session, ret);
       shutdown (socket->fd, SHUT_RDWR);
     }
