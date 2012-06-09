71e7aa41ee8c969de04c839fcfc89f09afda60ad
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-06-09 11:48:02 +0200

    Fixed leaks in PKCS #8 decoding

--- lib/x509/privkey_pkcs8.c
+++ lib/x509/privkey_pkcs8.c
@@ -1362,7 +1362,7 @@ read_pbkdf2_params (ASN1_TYPE pbes2_asn,
   /* We don't read the PRF. We only use the default.
    */
 
-  return 0;
+  result = 0;
 
 error:
   asn1_delete_structure (&pbkdf2_asn);
@@ -1570,8 +1570,8 @@ read_pbe_enc_params (ASN1_TYPE pbes2_asn,
       goto error;
     }
   _gnutls_hard_log ("IV.size: %d\n", params->iv_size);
-
-  return 0;
+  
+  result = 0;
 
 error:
   asn1_delete_structure (&pbe_asn);
