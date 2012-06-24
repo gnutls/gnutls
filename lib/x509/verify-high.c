2045918365458b1f2e4ed0e1720ddd710d8dcc26
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-06-24 20:56:43 +0200

    document limitations

--- lib/x509/verify-high.c
+++ lib/x509/verify-high.c
@@ -460,6 +460,8 @@ int gnutls_x509_trust_list_get_issuer(gnutls_x509_trust_list_t list,
  * This function will try to verify the given certificate and return
  * its status.
  *
+ * Limitation: Pathlen constraints or key usage flags are not consulted.
+ *
  * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, otherwise a
  *   negative error value.
  *
