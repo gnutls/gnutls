2ec96fecc191d4b21430ef8f63f254f92c5e538e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-08-25 15:20:38 +0200

    doc update

--- lib/gnutls_privkey.c
+++ lib/gnutls_privkey.c
@@ -844,6 +844,9 @@ gnutls_privkey_decrypt_data (gnutls_privkey_t key,
  * This function will import the given private key to the abstract
  * #gnutls_privkey_t structure. 
  *
+ * The supported formats are typical X.509, PKCS #8 and the openssl
+ * format.
+ *
  * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, otherwise a
  *   negative error value.
  *
