0e8bafb0a55ab6e05df0d7c663c8a58f03c5f051
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-09-24 20:13:27 +0200

    Added boilerplate.

--- tests/suite/x509random.pl
+++ tests/suite/x509random.pl
@@ -1,6 +1,20 @@
 #!/usr/bin/perl -w
 #
-# Author David Howells <dhowells@redhat.com>
+# Copyright David Howells <dhowells@redhat.com>
+#
+# This file is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This file is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this file; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #
 # Generate validly formatted X.509 certificates filled with mostly random data,
 # including for the RSA key and signature fields (so it is extremely improbable
