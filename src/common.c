bc0f87278e42efd06bf80a98d4c46600e78cfe4b
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-11-10 00:25:09 +0100

    updated

--- src/common.c
+++ src/common.c
@@ -340,7 +340,7 @@ cert_verify (gnutls_session_t session, const char* hostname)
           return 0;
       }
 
-    printf ("- %s\n", out.data);
+    printf ("- Status: %s\n", out.data);
     
     gnutls_free(out.data);
 
