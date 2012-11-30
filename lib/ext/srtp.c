f8bb152d7ba4109cec16733527730a47255aca45
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2012-11-30 11:20:09 +0100

    corrected copyright

--- lib/ext/srtp.c
+++ lib/ext/srtp.c
@@ -1,5 +1,4 @@
 /*
- * Copyright (C) 2012 Martin Storsjo
  * Copyright (C) 2012 Free Software Foundation
  * 
  * Author: Martin Storsjo
--- lib/ext/srtp.h
+++ lib/ext/srtp.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Martin Storsjo
+ * Copyright (C) 2012 Free Software Foundation
  *
  * Author: Martin Storsjo
  *
