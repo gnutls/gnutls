95131da08f7c13cd70b07209cc4f69e0b7263756
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-03-14 19:27:17 +0100

    simplified code

--- lib/gnutls_pcert.c
+++ lib/gnutls_pcert.c
@@ -48,35 +48,18 @@ int gnutls_pcert_import_x509 (gnutls_pcert_st* pcert,
 	gnutls_x509_crt_t crt, unsigned int flags)
 {
 int ret;
-size_t sz;
 
   memset(pcert, 0, sizeof(*pcert));
 
   pcert->type = GNUTLS_CRT_X509;
   pcert->cert.data = NULL;
 
-  sz = 0;
-  ret = gnutls_x509_crt_export(crt, GNUTLS_X509_FMT_DER, NULL, &sz);
-  if (ret < 0 && ret != GNUTLS_E_SHORT_MEMORY_BUFFER)
-    {
-      ret = gnutls_assert_val(ret);
-      goto cleanup;
-    }
-
-  pcert->cert.data = gnutls_malloc(sz);
-  if (pcert->cert.data == NULL)
-    {
-      ret = gnutls_assert_val(GNUTLS_E_MEMORY_ERROR);
-      goto cleanup;
-    }
-
-  ret = gnutls_x509_crt_export(crt, GNUTLS_X509_FMT_DER, pcert->cert.data, &sz);
+  ret = gnutls_x509_crt_export2(crt, GNUTLS_X509_FMT_DER, &pcert->cert);
   if (ret < 0)
     {
       ret = gnutls_assert_val(ret);
       goto cleanup;
     }
-  pcert->cert.size = sz;
 
   ret = gnutls_pubkey_init(&pcert->pubkey);
   if (ret < 0)
