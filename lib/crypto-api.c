97fc2e20df39b51ddfdef2183fac882aa51757ee
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-04-27 00:29:43 +0300

    updated documentation

--- lib/crypto-api.c
+++ lib/crypto-api.c
@@ -329,7 +329,8 @@ gnutls_hmac (gnutls_hmac_hd_t handle, const void *text, size_t textlen)
  * @handle: is a #gnutls_hmac_hd_t structure.
  * @digest: is the output value of the MAC
  *
- * This function will output the current MAC value.
+ * This function will output the current MAC value
+ * and reset the state of the MAC.
  *
  * Since: 2.10.0
  **/
@@ -450,7 +451,8 @@ gnutls_hash (gnutls_hash_hd_t handle, const void *text, size_t textlen)
  * @handle: is a #gnutls_hash_hd_t structure.
  * @digest: is the output value of the hash
  *
- * This function will output the current hash value.
+ * This function will output the current hash value
+ * and reset the state of the hash.
  *
  * Since: 2.10.0
  **/
