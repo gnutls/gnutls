e3436a2487852435a8b1ef5bb457ee0e5614545e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-05-08 12:34:02 +0200

    use C's style comments to compile in old MacOSX systems. Reported by Ryan Schmidt.

--- lib/accelerated/x86/macosx/padlock-x86-macosx.s
+++ lib/accelerated/x86/macosx/padlock-x86-macosx.s
@@ -1,3 +1,4 @@
+/*
 # Copyright (c) 2011-2012, Andy Polyakov <appro@openssl.org>
 # All rights reserved.
 #
@@ -37,6 +38,7 @@
 #
 # *** This file is auto-generated ***
 #
+*/
 .file	"devel/perlasm/e_padlock-x86.s"
 .text
 .globl	_padlock_capability
