4cd2ba975bc6e1ab4f068127ceab5e66367d3ff8
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-06-19 23:02:45 +0200

    enforce the maximum TLS size when setting MTU

--- lib/gnutls_dtls.c
+++ lib/gnutls_dtls.c
@@ -578,7 +578,7 @@ void gnutls_dtls_set_timeouts (gnutls_session_t session, unsigned int retrans_ti
  **/
 void gnutls_dtls_set_mtu (gnutls_session_t session, unsigned int mtu)
 {
-  session->internals.dtls.mtu  = mtu;
+  session->internals.dtls.mtu  = MIN(mtu, DEFAULT_MAX_RECORD_SIZE);
 }
 
 static int record_overhead(const cipher_entry_st* cipher, const mac_entry_st* mac, 
