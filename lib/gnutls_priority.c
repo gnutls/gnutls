5edeff380bb875442d59ecdb9b8fc3f927fd0368
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-08-27 20:29:10 +0300

    corrected typo

--- lib/gnutls_priority.c
+++ lib/gnutls_priority.c
@@ -577,7 +577,7 @@ prio_remove (priority_st * priority_list, unsigned int algo)
       if (priority_list->priority[i] == algo) 
         {
           priority_list->algorithms--;
-          if (priority_list->algorithms-i) > 0)
+          if ((priority_list->algorithms-i) > 0)
             memmove(&priority_list->priority[i], &priority_list->priority[i+1], (priority_list->algorithms-i)*sizeof(priority_list->priority[0]));
           priority_list->priority[priority_list->algorithms] = 0;
           break;
