d8a42e6cca198b1db4cc8822fbea3ef54bfbe75c
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-10-31 10:12:27 +0100

    debug_log -> record_log

--- lib/gnutls_record.c
+++ lib/gnutls_record.c
@@ -1226,7 +1226,7 @@ begin:
       ret = _dtls_record_check(record_params, packet_sequence);
       if (ret < 0)
         {
-          _gnutls_debug_log("Discarded duplicate message[%u.%u]: %s\n",
+          _gnutls_record_log("REC[%p]: Discarded duplicate message[%u.%u]: %s\n", session,
             (unsigned int)record.sequence.i[0]*256 +(unsigned int)record.sequence.i[1],
             (unsigned int) _gnutls_uint64touint32 (packet_sequence), _gnutls_packet2str (record.type));
           goto sanity_check_error;
