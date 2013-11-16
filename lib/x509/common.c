d093a1c476b689ebe64134e0ae983a87a10dc45e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-11-16 10:41:08 +0100

    Dates and time that would overflow the GeneralTime are also truncated.

--- lib/x509/common.c
+++ lib/x509/common.c
@@ -728,7 +728,11 @@ gtime2generalTime (time_t gtime, char *str_time, size_t str_time_size)
   size_t ret;
   struct tm _tm;
 	
-  if (gtime == (time_t)-1) 
+  if (gtime == (time_t)-1
+#if SIZEOF_LONG == 8
+                       || gtime >= 253402210800
+#endif
+     )
     {
        	snprintf(str_time, str_time_size, "99991231235959Z");
        	return 0;
