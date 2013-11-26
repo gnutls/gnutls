5335b36851cdfb6f684935cd42a1b88e2f754a77
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2013-11-26 23:46:39 +0100

    increased buffers

--- lib/debug.c
+++ lib/debug.c
@@ -31,7 +31,7 @@
 void _gnutls_dump_mpi(const char *prefix, bigint_t a)
 {
 	char buf[400];
-	char buf_hex[2 * sizeof(buf)];
+	char buf_hex[2 * sizeof(buf) + 1];
 	size_t n = sizeof buf;
 
 	if (_gnutls_mpi_print(a, buf, &n))
@@ -44,7 +44,7 @@ void _gnutls_dump_mpi(const char *prefix, bigint_t a)
 void
 _gnutls_dump_vector(const char *prefix, const uint8_t * a, size_t a_size)
 {
-	char buf_hex[2 * a_size];
+	char buf_hex[2 * a_size + 1];
 
 	_gnutls_debug_log("Vector: length: %d\n\t%s%s\n", (int) a_size,
 			  prefix, _gnutls_bin2hex(a, a_size, buf_hex,
