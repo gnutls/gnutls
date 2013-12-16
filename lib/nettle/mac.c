b486fc3e5d7c81cf9c07acdec7fde5ccf04909e0
Nikos Mavrogiannopoulos <nmav@redhat.com>
2013-12-16 10:05:16 +0100

    Avoid verbose logging

--- lib/nettle/mac.c
+++ lib/nettle/mac.c
@@ -257,7 +257,7 @@ wrap_nettle_mac_set_nonce(void *_ctx, const void *nonce, size_t noncelen)
 	struct nettle_mac_ctx *ctx = _ctx;
 
 	if (ctx->set_nonce == NULL)
-		return gnutls_assert_val(GNUTLS_E_INVALID_REQUEST);
+		return GNUTLS_E_INVALID_REQUEST;
 
 	ctx->set_nonce(ctx->ctx_ptr, noncelen, nonce);
 
