9b8271f0015526f017aa70b8d1f3a8fa1f8d3a62
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-04-19 18:32:07 +0200

    Treat othername as printable (i.e., null terminate it), as the XMPP printing code assumes that.

--- lib/x509/x509.c
+++ lib/x509/x509.c
@@ -847,7 +847,8 @@ gnutls_x509_crt_get_subject_key_id(gnutls_x509_crt_t cert, void *ret,
 inline static int is_type_printable(int type)
 {
 	if (type == GNUTLS_SAN_DNSNAME || type == GNUTLS_SAN_RFC822NAME ||
-	    type == GNUTLS_SAN_URI || type == GNUTLS_SAN_OTHERNAME_XMPP)
+	    type == GNUTLS_SAN_URI || type == GNUTLS_SAN_OTHERNAME_XMPP ||
+	    type == GNUTLS_SAN_OTHERNAME)
 		return 1;
 	else
 		return 0;
