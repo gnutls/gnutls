3623959abd55f6bf6c2433cf8175c4c512d7fdbf
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-05-04 12:50:06 +0200

    initialize to null the SRP extension data on allocation.
    Issue identified using valgrind and the Codenomicon TLS test suite.

--- lib/ext_srp.c
+++ lib/ext_srp.c
@@ -133,7 +133,7 @@ _gnutls_srp_send_params (gnutls_session_t session, opaque * data,
       if (cred == NULL)
         return 0;
 
-      priv = gnutls_malloc (sizeof (*priv));
+      priv = gnutls_calloc (1, sizeof (*priv));
       if (priv == NULL)
         {
           gnutls_assert ();
