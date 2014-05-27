389b3f0d6c8a5239ff806ee6863a88e5353c2f71
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-05-27 15:47:43 +0200

    safe_memset: allow memset of zero bytes.

--- lib/safe-memset.c
+++ lib/safe-memset.c
@@ -46,9 +46,11 @@ void gnutls_memset(void *data, int c, size_t size)
 	 * sent by David Jacobson in the openssl-dev mailing list.
 	 */
 
-	do {
-		memset(data, c, size);
-	} while(vdata[volatile_zero] != c);
+	if (size > 0) {
+		do {
+			memset(data, c, size);
+		} while(vdata[volatile_zero] != c);
+	}
 }
 
 #ifdef TEST_SAFE_MEMSET
