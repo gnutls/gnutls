23a5083e2902dc6d202fc74c512e9f3340665722
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-07-04 15:44:38 +0200

    pkcs11: Removed length check of attribute as a sanity check for valid keys.
    There can be keys where the id or label is empty and thus with zero length.

--- lib/pkcs11_privkey.c
+++ lib/pkcs11_privkey.c
@@ -386,9 +386,9 @@ gnutls_pkcs11_privkey_import_url(gnutls_pkcs11_privkey_t pkey,
 	}
 
 	attr = p11_kit_uri_get_attribute(pkey->info, CKA_ID);
-	if (!attr || !attr->value_len) {
+	if (!attr) {
 		attr = p11_kit_uri_get_attribute(pkey->info, CKA_LABEL);
-		if (!attr || !attr->value_len) {
+		if (!attr) {
 			gnutls_assert();
 			return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
 		}
