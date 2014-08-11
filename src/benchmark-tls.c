784c1c2ac83a87785c6772fd641464cb6c7274f1
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-08-11 17:12:30 +0200

    gnutls-cli: removed salsa20 from the benchmarked ciphers

--- src/benchmark-tls.c
+++ src/benchmark-tls.c
@@ -54,7 +54,6 @@ const char *side = "";
 #define PRIO_ARCFOUR_128_MD5 "NONE:+VERS-TLS1.0:+ARCFOUR-128:+MD5:+SIGN-ALL:+COMP-NULL:+RSA"
 #define PRIO_AES_GCM "NONE:+VERS-TLS1.2:+AES-128-GCM:+AEAD:+SIGN-ALL:+COMP-NULL:+RSA"
 #define PRIO_CAMELLIA_CBC_SHA1 "NONE:+VERS-TLS1.0:+CAMELLIA-128-CBC:+SHA1:+SIGN-ALL:+COMP-NULL:+RSA"
-#define PRIO_SALSA20_256_SHA1 "NONE:+VERS-TLS1.0:+SALSA20-256:+SHA1:+SIGN-ALL:+COMP-NULL:+RSA"
 
 static const int rsa_bits = 3072, ec_bits = 256;
 
@@ -502,7 +501,6 @@ void benchmark_tls(int debug_level, int ciphers)
 		    ("Testing throughput in cipher/MAC combinations (payload: %d bytes)\n",
 		     size);
 
-		test_ciphersuite(PRIO_SALSA20_256_SHA1, size);
 		test_ciphersuite(PRIO_ARCFOUR_128_SHA1, size);
 		test_ciphersuite(PRIO_ARCFOUR_128_MD5, size);
 		test_ciphersuite(PRIO_AES_GCM, size);
@@ -513,7 +511,6 @@ void benchmark_tls(int debug_level, int ciphers)
 		printf
 		    ("\nTesting throughput in cipher/MAC combinations (payload: %d bytes)\n",
 		     size);
-		test_ciphersuite(PRIO_SALSA20_256_SHA1, size);
 		test_ciphersuite(PRIO_ARCFOUR_128_SHA1, size);
 		test_ciphersuite(PRIO_ARCFOUR_128_MD5, size);
 		test_ciphersuite(PRIO_AES_GCM, size);
