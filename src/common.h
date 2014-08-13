c89308de783727c3dbf953544bce7a0dacec7e1b
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-08-13 08:13:48 +0200

    gnutls-cli/danetool: corrected check on ipv6 IPs

--- src/common.h
+++ src/common.h
@@ -73,7 +73,7 @@ inline static int is_ip(const char *hostname)
 {
 int len = strlen(hostname);
 
-	if (strchr(hostname, ':') == 0)
+	if (strchr(hostname, ':') != 0)
 		return 1;
 	else if (len > 2 && c_isdigit(hostname[0]) && c_isdigit(hostname[len-1]))
 		return 1;
