9cfb421c41499b0758747dc5ffeeab2a6a23d8f1
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-08-29 19:55:31 +0200

    refer to rfc6125

--- lib/x509/hostname-verify.c
+++ lib/x509/hostname-verify.c
@@ -34,7 +34,7 @@
  *
  * This function will check if the given certificate's subject matches
  * the given hostname.  This is a basic implementation of the matching
- * described in RFC2818 (HTTPS), which takes into account wildcards,
+ * described in RFC6125, and takes into account wildcards,
  * and the DNSName/IPAddress subject alternative name PKIX extension.
  *
  * For details see also gnutls_x509_crt_check_hostname2().
@@ -99,7 +99,7 @@ static int has_embedded_null(const char *str, unsigned size)
  *
  * This function will check if the given certificate's subject matches
  * the given hostname.  This is a basic implementation of the matching
- * described in RFC2818 (HTTPS), which takes into account wildcards,
+ * described in RFC6125, and takes into account wildcards,
  * and the DNSName/IPAddress subject alternative name PKIX extension.
  *
  * IPv4 addresses are accepted by this function in the dotted-decimal
