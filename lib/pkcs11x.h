3b744219b91ec835c6cd00fd8f888e6d914649d1
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-09-17 16:54:05 +0200

    depend on p11-kit 0.20.6

--- lib/pkcs11x.h
+++ lib/pkcs11x.h
@@ -21,32 +21,6 @@
 #ifndef PKCS11X_H
 #define PKCS11X_H
 
-#ifdef HAVE_PKCS11X_H
-# include <p11-kit/pkcs11x.h>
-#else
-
-# include <p11-kit/pkcs11.h>
-
-# ifndef CKA_X_VENDOR
-#  define CKA_X_VENDOR (CKA_VENDOR_DEFINED | 0x58444700UL)
-# endif
-
-# ifndef CKO_X_VENDOR
-#  define CKO_X_VENDOR   (CKA_VENDOR_DEFINED | 0x58444700UL)
-# endif
-
-# ifndef CKA_X_DISTRUSTED
-#  define CKA_X_DISTRUSTED (CKA_X_VENDOR + 100)
-# endif
-
-# ifndef CKO_X_CERTIFICATE_EXTENSION
-#  define CKO_X_CERTIFICATE_EXTENSION                  (CKO_X_VENDOR + 200)
-# endif
-
-# ifndef CKA_PUBLIC_KEY_INFO
-#  define CKA_PUBLIC_KEY_INFO                          0x00000129UL
-# endif
-
-#endif
+#include <p11-kit/pkcs11x.h>
 
 #endif
