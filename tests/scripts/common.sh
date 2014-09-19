d8b277f39b693265e8070bd2b4eac64229ff206d
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-09-19 10:40:44 +0200

    tests: enhanced test suite to pass more of the PKCS #11 API under valgrind

--- tests/scripts/common.sh
+++ tests/scripts/common.sh
@@ -41,6 +41,23 @@ launch_server() {
        fi
 }
 
+launch_pkcs11_server() {
+       PARENT=$1;
+       shift;
+       PROVIDER=$1;
+       shift;
+       $VALGRIND $SERV $PROVIDER $DEBUG -p $PORT $* &
+       LOCALPID="$!";
+       trap "[ ! -z \"${LOCALPID}\" ] && kill ${LOCALPID};" 15
+       wait "${LOCALPID}"
+       LOCALRET="$?"
+       if [ "${LOCALRET}" != "0" ] && [ "${LOCALRET}" != "143" ] ; then
+               # Houston, we'v got a problem...
+               echo "Failed to launch a gnutls-serv server !"
+               kill -10 ${PARENT}
+       fi
+}
+
 launch_bare_server() {
        PARENT=$1;
        shift;
