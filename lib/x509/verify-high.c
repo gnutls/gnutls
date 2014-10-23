2207f4b538f44f450259465ef1f9459083fb2fcd
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-10-23 08:47:27 +0200

    corrected bug in gnutls_x509_trust_list_get_issuer() when used without the GNUTLS_TL_GET_COPY flag

--- lib/x509/verify-high.c
+++ lib/x509/verify-high.c
@@ -950,6 +950,7 @@ int gnutls_x509_trust_list_get_issuer(gnutls_x509_trust_list_t list,
 				gnutls_x509_crt_deinit(crt);
 				return gnutls_assert_val(ret);
 			}
+			*issuer = crt;
 			return ret;
 		}
 	}
