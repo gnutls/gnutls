bfe12f00475795c825359c5b6d649273c476b531
Nikos Mavrogiannopoulos <nmav@redhat.com>
2014-11-21 16:10:33 +0100

    The record version in the client Hello will be set to the lowest supported protocol
    There should have been no harm in keeping it SSL 3.0 but
    unfortunately in draft-thomson-sslv3-diediedie-00
    it has been marked as MUST NOT do that. That will be fixed in a later
    revision but since then there are servers not accepting SSL 3.0
    as a valid record version (note that this is about the record
    version, which describes the format of the packet, nothing to
    do with the negotiated version).

--- lib/algorithms.h
+++ lib/algorithms.h
@@ -33,7 +33,7 @@
 
 /* Functions for version handling. */
 const version_entry_st *version_to_entry(gnutls_protocol_t c);
-gnutls_protocol_t _gnutls_version_lowest(gnutls_session_t session);
+const version_entry_st *_gnutls_version_lowest(gnutls_session_t session);
 gnutls_protocol_t _gnutls_version_max(gnutls_session_t session);
 int _gnutls_version_priority(gnutls_session_t session,
 			     gnutls_protocol_t version);
