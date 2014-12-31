b8cd84390dbd681265f09719bcc111c2cb6f3d3c
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2014-12-31 12:09:41 +0200

    generate manpages for urls.h and system-keys.h

--- doc/doc.mk
+++ doc/doc.mk
@@ -25,6 +25,6 @@ HEADER_FILES = $(top_srcdir)/lib/includes/gnutls/gnutls.h.in \
 	$(top_srcdir)/lib/includes/gnutls/dtls.h $(top_srcdir)/lib/includes/gnutls/crypto.h \
 	$(top_srcdir)/lib/includes/gnutls/ocsp.h $(top_srcdir)/lib/includes/gnutls/tpm.h \
 	$(top_srcdir)/libdane/includes/gnutls/dane.h $(top_srcdir)/lib/includes/gnutls/x509-ext.h \
-	$(top_srcdir)/lib/includes/gnutls/urls.h
+	$(top_srcdir)/lib/includes/gnutls/urls.h $(top_srcdir)/lib/includes/gnutls/system-keys.h
 
 C_SOURCE_FILES = $(top_srcdir)/lib/*/*.c $(top_srcdir)/lib/*.c  $(top_srcdir)/libdane/*.c 
