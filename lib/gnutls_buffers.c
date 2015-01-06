f1a141e5c247b706fb2022d50666d5f5a71bb6ff
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-01-06 09:59:25 +0100

    Revert "Give precedence to vector push function"
    This reverts commit cb4ea413569803cbbf291abb27d30d14bfa971c5.

--- lib/gnutls_buffers.c
+++ lib/gnutls_buffers.c
@@ -483,13 +483,11 @@ _gnutls_writev(gnutls_session_t session, const giovec_t * giovec,
 		}
 	}
 
-	if (session->internals.vec_push_func != NULL && no_writev == 0)
-		i = session->internals.vec_push_func(fd, giovec,
-						     giovec_cnt);
-	else if (session->internals.push_func)
+	if (session->internals.push_func != NULL || no_writev != 0)
 		i = _gnutls_writev_emu(session, fd, giovec, giovec_cnt);
 	else
-		return gnutls_assert_val(GNUTLS_E_INTERNAL_ERROR);
+		i = session->internals.vec_push_func(fd, giovec,
+						     giovec_cnt);
 
 	if (i == -1) {
 		int err = get_errno(session);
