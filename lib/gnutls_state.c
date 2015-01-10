e887805a9465f803a1c8682fa16f8f2b9066bce8
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2015-01-10 12:12:25 +0100

    When setting up TLS with cert-type OpenPGP from a client, the server verifies if it supports the extension’s contents in _gnutls_session_cert_type_supported().  This function checks for cred->get_cert_callback but not cred->get_cert_callback2. As a result, servers setup for OpenPGP certificate credential callback with gnutls_certificate_set_retrieve_function2() are unable to use the OpenPGP certificate type.
    The solution is to consider cred->get_cert_callback2 alongside
    cred->get_cert_callback in _gnutls_session_cert_type_supported().
    
    Patch by Rick van Rein.

--- lib/gnutls_state.c
+++ lib/gnutls_state.c
@@ -190,7 +190,8 @@ _gnutls_session_cert_type_supported(gnutls_session_t session,
 			return GNUTLS_E_UNSUPPORTED_CERTIFICATE_TYPE;
 
 		if (cred->server_get_cert_callback == NULL
-		    && cred->get_cert_callback == NULL) {
+		    && cred->get_cert_callback == NULL
+		    && cred->get_cert_callback2 == NULL) {
 			for (i = 0; i < cred->ncerts; i++) {
 				if (cred->certs[i].cert_list[0].type ==
 				    cert_type) {
