93b59db2c11269b6d8464cd8a01793a4c95cc519
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-02-24 10:28:26 +0100

    doc update: document that session_get_data() must be used in non-resumed sessions

--- lib/gnutls_session.c
+++ lib/gnutls_session.c
@@ -34,7 +34,8 @@
  * Returns all session parameters needed to be stored to support resumption.
  * The client should call this, and store the returned session data. A session
  * may be resumed later by calling gnutls_session_set_data().  
- * This function must be called after a successful handshake. 
+ * This function must be called after a successful (full) handshake. It should
+ * not be used in resumed sessions --see gnutls_session_is_resumed().
  *
  * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, otherwise
  *   an error code is returned.
@@ -83,7 +84,9 @@ gnutls_session_get_data(gnutls_session_t session,
  * Returns all session parameters needed to be stored to support resumption.
  * The client should call this, and store the returned session data. A session
  * may be resumed later by calling gnutls_session_set_data().  
- * This function must be called after a successful handshake. 
+ * This function must be called after a successful (full) handshake. It should
+ * not be used in resumed sessions --see gnutls_session_is_resumed().
+ *
  * The returned @data are allocated and must be released using gnutls_free().
  *
  * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, otherwise
