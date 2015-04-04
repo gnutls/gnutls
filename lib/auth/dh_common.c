7be559bc8ae50fdf27f52e0ec68a0ac698244c18
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2015-04-04 07:36:47 +0200

    DH: do not warn on zero q_bits

--- lib/auth/dh_common.c
+++ lib/auth/dh_common.c
@@ -307,7 +307,7 @@ _gnutls_dh_common_print_server_kx(gnutls_session_t session,
 	int ret;
 	unsigned q_bits = session->key.dh_params.flags;
 
-	if (q_bits < 192) {
+	if (q_bits < 192 && q_bits != 0) {
 		gnutls_assert();
 		_gnutls_debug_log("too small q_bits value for DH: %u\n", q_bits);
 		q_bits = 0; /* auto-detect */
