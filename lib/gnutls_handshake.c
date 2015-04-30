f1a716d8872fc912607a6b0fa97beb0b30ff44f2
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-04-30 16:28:24 +0200

    gnutls_handshake_set_timeout will properly work with DTLS

--- lib/gnutls_handshake.c
+++ lib/gnutls_handshake.c
@@ -2616,6 +2616,11 @@ int gnutls_handshake(gnutls_session_t session)
 void
 gnutls_handshake_set_timeout(gnutls_session_t session, unsigned int ms)
 {
+	if (IS_DTLS(session)) {
+		gnutls_dtls_set_timeouts(session, 1000, ms);
+		return;
+	}
+
 	if (ms == GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT)
 		ms = DEFAULT_HANDSHAKE_TIMEOUT_MS;
 	session->internals.handshake_timeout_ms = ms;
