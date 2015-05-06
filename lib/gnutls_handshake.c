19702630dea69d9f2f9500e9bfd2fe9d3de197d5
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-05-06 11:13:05 +0200

    apply default timeout for DTLS in gnutls_handshake_set_timeout

--- lib/gnutls_handshake.c
+++ lib/gnutls_handshake.c
@@ -2616,13 +2616,14 @@ int gnutls_handshake(gnutls_session_t session)
 void
 gnutls_handshake_set_timeout(gnutls_session_t session, unsigned int ms)
 {
+	if (ms == GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT)
+		ms = DEFAULT_HANDSHAKE_TIMEOUT_MS;
+
 	if (IS_DTLS(session)) {
 		gnutls_dtls_set_timeouts(session, DTLS_RETRANS_TIMEOUT, ms);
 		return;
 	}
 
-	if (ms == GNUTLS_DEFAULT_HANDSHAKE_TIMEOUT)
-		ms = DEFAULT_HANDSHAKE_TIMEOUT_MS;
 	session->internals.handshake_timeout_ms = ms;
 }
 
