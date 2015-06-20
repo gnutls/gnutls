36e35de02c5e0e55ba6d7a8166f7b8806c9eb4f9
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2015-06-20 12:18:26 +0200

    gnutls_str: include gettext.h when dgettext is available

--- lib/gnutls_str.h
+++ lib/gnutls_str.h
@@ -26,8 +26,12 @@
 #include <gnutls_int.h>
 #include <gnutls_datum.h>
 
-#include "gettext.h"
-#define _(String) dgettext (PACKAGE, String)
+#ifdef HAVE_DGETTEXT
+# include "gettext.h"
+# define _(String) dgettext (PACKAGE, String)
+#else
+# define _(String) String
+#endif
 
 void _gnutls_str_cpy(char *dest, size_t dest_tot_size, const char *src);
 void _gnutls_mem_cpy(char *dest, size_t dest_tot_size, const char *src,
