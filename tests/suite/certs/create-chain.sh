435deb84c9c27d22be04e6a598e874e0bd55ff5b
Alon Bar-Lev <alon.barlev@gmail.com>
2015-06-21 20:42:12 +0300

    tests: tab indent + minor style changes
    Signed-off-by: Alon Bar-Lev <alon.barlev@gmail.com>

--- tests/suite/certs/create-chain.sh
+++ tests/suite/certs/create-chain.sh
@@ -6,9 +6,9 @@ TEMPLATE=tmpl
 
 NUM="$1"
 
-if test "${NUM}" = "";then
-  echo "usage: $0 number"
-  exit 1
+if test "${NUM}" = ""; then
+	echo "usage: $0 number"
+	exit 1
 fi
 
 LAST=`expr ${NUM} - 1`
@@ -18,75 +18,73 @@ mkdir -p "${OUTPUT}"
 
 counter=0
 while test ${counter} -lt ${NUM}; do
-  if test ${counter} = ${LAST};then
-    name="server-${counter}"
-  else
-    name="CA-${counter}"
-  fi
-  serial="${counter}"
+	if test ${counter} = ${LAST}; then
+		name="server-${counter}"
+	else
+		name="CA-${counter}"
+	fi
+	serial="${counter}"
 
-  
-  "${CERTTOOL}" --generate-privkey >"${OUTPUT}/${name}.key" 2>/dev/null
-  if test ${counter} = 0;then
-  # ROOT CA
-    echo "cn = ${name}" >"${TEMPLATE}"
-    echo "serial = ${serial}" >>"${TEMPLATE}"
-    echo "ca" >>"${TEMPLATE}"
-    echo "expiration_days = -1" >>"${TEMPLATE}"
-    echo "cert_signing_key" >>"${TEMPLATE}"
-    echo "ocsp_signing_key" >>"${TEMPLATE}"
-    echo "crl_signing_key" >>"${TEMPLATE}"
-    "${CERTTOOL}" --generate-self-signed --load-privkey "${OUTPUT}/${name}.key" --outfile \
-      "${OUTPUT}/${name}.crt" --template "${TEMPLATE}" 2>/dev/null
+	"${CERTTOOL}" --generate-privkey >"${OUTPUT}/${name}.key" 2>/dev/null
+	if test ${counter} = 0; then
+	# ROOT CA
+		echo "cn = ${name}" >"${TEMPLATE}"
+		echo "serial = ${serial}" >>"${TEMPLATE}"
+		echo "ca" >>"${TEMPLATE}"
+		echo "expiration_days = -1" >>"${TEMPLATE}"
+		echo "cert_signing_key" >>"${TEMPLATE}"
+		echo "ocsp_signing_key" >>"${TEMPLATE}"
+		echo "crl_signing_key" >>"${TEMPLATE}"
+		"${CERTTOOL}" --generate-self-signed --load-privkey "${OUTPUT}/${name}.key" --outfile \
+			"${OUTPUT}/${name}.crt" --template "${TEMPLATE}" 2>/dev/null
 
-    echo "serial = ${serial}" >"${TEMPLATE}"
-    echo "expiration_days = -1" >>"${TEMPLATE}"
-    "${CERTTOOL}" --generate-crl --load-ca-privkey "${OUTPUT}/${name}.key" --load-ca-certificate "${OUTPUT}/${name}.crt" --outfile \
-      "${OUTPUT}/${name}.crl" --template "${TEMPLATE}" 2>/dev/null
-  else
-    if test ${counter} = ${LAST};then
-    # END certificate
-      echo "cn = ${name}" >"${TEMPLATE}"
-      echo "dns_name = localhost" >>"${TEMPLATE}"
-      echo "expiration_days = -1" >>"${TEMPLATE}"
-      echo "signing_key" >>"${TEMPLATE}"
-      echo "encryption_key" >>"${TEMPLATE}"
-      echo "ocsp_signing_key" >>"${TEMPLATE}"
-      "${CERTTOOL}" --generate-certificate --load-privkey "${OUTPUT}/${name}.key" \
-        --load-ca-certificate "${OUTPUT}/${prev_name}.crt" \
-        --load-ca-privkey "${OUTPUT}/${prev_name}.key" \
-        --outfile "${OUTPUT}/${name}.crt" --template "${TEMPLATE}" 2>/dev/null
-    else
-    # intermediate CA
-      echo "cn = ${name}" >"${TEMPLATE}"
-      echo "serial = ${serial}" >>"${TEMPLATE}"
-      echo "ca" >>"${TEMPLATE}"
-      echo "expiration_days = -1" >>"${TEMPLATE}"
-      echo "ocsp_signing_key" >>"${TEMPLATE}"
-      echo "cert_signing_key" >>"${TEMPLATE}"
-      echo "signing_key" >>"${TEMPLATE}"
-      "${CERTTOOL}" --generate-certificate --load-privkey "${OUTPUT}/${name}.key" \
-        --load-ca-certificate "${OUTPUT}/${prev_name}.crt" \
-        --load-ca-privkey "${OUTPUT}/${prev_name}.key" \
-        --outfile "${OUTPUT}/${name}.crt" --template "${TEMPLATE}" 2>/dev/null
-    fi
-  fi
+		echo "serial = ${serial}" >"${TEMPLATE}"
+		echo "expiration_days = -1" >>"${TEMPLATE}"
+		"${CERTTOOL}" --generate-crl --load-ca-privkey "${OUTPUT}/${name}.key" --load-ca-certificate "${OUTPUT}/${name}.crt" --outfile \
+			"${OUTPUT}/${name}.crl" --template "${TEMPLATE}" 2>/dev/null
+	else
+		if test ${counter} = ${LAST}; then
+		# END certificate
+			echo "cn = ${name}" >"${TEMPLATE}"
+			echo "dns_name = localhost" >>"${TEMPLATE}"
+			echo "expiration_days = -1" >>"${TEMPLATE}"
+			echo "signing_key" >>"${TEMPLATE}"
+			echo "encryption_key" >>"${TEMPLATE}"
+			echo "ocsp_signing_key" >>"${TEMPLATE}"
+			"${CERTTOOL}" --generate-certificate --load-privkey "${OUTPUT}/${name}.key" \
+				--load-ca-certificate "${OUTPUT}/${prev_name}.crt" \
+				--load-ca-privkey "${OUTPUT}/${prev_name}.key" \
+				--outfile "${OUTPUT}/${name}.crt" --template "${TEMPLATE}" 2>/dev/null
+		else
+		# intermediate CA
+			echo "cn = ${name}" >"${TEMPLATE}"
+			echo "serial = ${serial}" >>"${TEMPLATE}"
+			echo "ca" >>"${TEMPLATE}"
+			echo "expiration_days = -1" >>"${TEMPLATE}"
+			echo "ocsp_signing_key" >>"${TEMPLATE}"
+			echo "cert_signing_key" >>"${TEMPLATE}"
+			echo "signing_key" >>"${TEMPLATE}"
+			"${CERTTOOL}" --generate-certificate --load-privkey "${OUTPUT}/${name}.key" \
+				--load-ca-certificate "${OUTPUT}/${prev_name}.crt" \
+				--load-ca-privkey "${OUTPUT}/${prev_name}.key" \
+				--outfile "${OUTPUT}/${name}.crt" --template "${TEMPLATE}" 2>/dev/null
+		fi
+	fi
 
 
-  counter=`expr ${counter} + 1`
-  prev_name=${name}
+	counter=`expr ${counter} + 1`
+	prev_name=${name}
 done
 
 counter=`expr ${NUM} - 1`
 while test ${counter} -ge 0; do
-  if test ${counter} = ${LAST};then
-    name="server-${counter}"
-  else
-    name="CA-${counter}"
-  fi
+	if test ${counter} = ${LAST}; then
+		name="server-${counter}"
+	else
+		name="CA-${counter}"
+	fi
 
-  cat "${OUTPUT}/${name}.crt" >> "${OUTPUT}/chain"
-  
-  counter=`expr ${counter} - 1`
+	cat "${OUTPUT}/${name}.crt" >> "${OUTPUT}/chain"
+
+	counter=`expr ${counter} - 1`
 done
-
