2af6c0f53ac8547c2867da5d672e05159107f554
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-06-25 11:08:19 +0200

    _gnutls_get_asn_mpis() will release any data on failure
    Resolves #15

--- lib/x509/mpi.c
+++ lib/x509/mpi.c
@@ -117,7 +117,6 @@ _gnutls_get_asn_mpis(ASN1_TYPE asn, const char *root,
 	 * then the issuer's parameters should be used. This is not
 	 * done yet.
 	 */
-
 	if (pk_algorithm != GNUTLS_PK_RSA) {	/* RSA doesn't use parameters */
 		result = _gnutls_x509_read_value(asn, name, &tmp);
 		if (result < 0) {
@@ -125,10 +124,11 @@ _gnutls_get_asn_mpis(ASN1_TYPE asn, const char *root,
 			goto error;
 		}
 
-		if ((result =
+		result =
 		     _gnutls_x509_read_pubkey_params(pk_algorithm,
 						     tmp.data, tmp.size,
-						     params)) < 0) {
+						     params);
+		if (result < 0) {
 			gnutls_assert();
 			goto error;
 		}
@@ -137,6 +137,8 @@ _gnutls_get_asn_mpis(ASN1_TYPE asn, const char *root,
 	result = 0;
 
       error:
+	if (result < 0)
+		gnutls_pk_params_release(params);
 	_gnutls_free_datum(&tmp);
 	return result;
 }
