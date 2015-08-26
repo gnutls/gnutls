a69c18667e6457b91683649cffe872bdc6776be7
Nikos Mavrogiannopoulos <nmav@redhat.com>
2015-08-26 09:13:52 +0200

    corrected typo in ex-server-anon

--- doc/examples/ex-serv-anon.c
+++ doc/examples/ex-serv-anon.c
@@ -93,7 +93,7 @@ int main(void)
         for (;;) {
                 gnutls_init(&session, GNUTLS_SERVER);
                 gnutls_priority_set_direct(session,
-                                           "NORMAL::+ANON-ECDH:+ANON-DH",
+                                           "NORMAL:+ANON-ECDH:+ANON-DH",
                                            NULL);
                 gnutls_credentials_set(session, GNUTLS_CRD_ANON, anoncred);
 
