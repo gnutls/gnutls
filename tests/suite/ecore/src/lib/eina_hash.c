b351bbf123586e0c115c201c0fc5aa9967d7571c
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-02-29 13:23:25 +0100

    tests: always used the slow (portable) version of get16bits
    This prevents issues with misaligned addresses and undefined sanitizer.

--- tests/suite/ecore/src/lib/eina_hash.c
+++ tests/suite/ecore/src/lib/eina_hash.c
@@ -131,16 +131,8 @@ struct _Eina_Hash_Each {
 	const void *data;
 };
 
-#undef get16bits
-#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
-   || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
-#define get16bits(d) (*((const uint16_t *)(d)))
-#endif
-
-#if !defined (get16bits)
 #define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8) \
                        + (uint32_t)(((const uint8_t *)(d))[0]))
-#endif
 
 static inline int
 _eina_hash_hash_rbtree_cmp_hash(const Eina_Hash_Head * hash_head,
