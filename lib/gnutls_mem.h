1672b934965f0297c0c01e40b0fe3e557341f4e1
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-02-29 09:43:32 +0100

    x509: Fixes to prevent undefined behavior (found with libubsan)

--- lib/gnutls_mem.h
+++ lib/gnutls_mem.h
@@ -31,6 +31,15 @@ void *gnutls_realloc_fast(void *ptr, size_t size);
 void *_gnutls_calloc(size_t nmemb, size_t size);
 char *_gnutls_strdup(const char *);
 
+/* To avoid undefined behavior when s1 or s2 are null and n = 0 */
+inline static
+int safe_memcmp(const void *s1, const void *s2, size_t n)
+{
+        if (n == 0)
+                return 0;
+        return memcmp(s1, s2, n);
+}
+
 #define zrelease_mpi_key(mpi) if (*mpi!=NULL) { \
                 _gnutls_mpi_clear(*mpi); \
                 _gnutls_mpi_release(mpi); \
