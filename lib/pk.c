c672fb3a7b45b8756beeaf7d24347e361378f680
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-05-02 08:37:45 +0200

    gnutls_decode_ber_digest_info: return more precise error code on unknown hash
    That is instead of returning GNUTLS_E_UNKNOWN_ALGORITHM on unknown hash,
    return GNUTLS_E_UNKNOWN_HASH_ALGORITHM.

--- lib/pk.c
+++ lib/pk.c
@@ -418,7 +418,7 @@ gnutls_decode_ber_digest_info(const gnutls_datum_t * info,
 
 		gnutls_assert();
 		asn1_delete_structure(&dinfo);
-		return GNUTLS_E_UNKNOWN_ALGORITHM;
+		return GNUTLS_E_UNKNOWN_HASH_ALGORITHM;
 	}
 
 	len = sizeof(str) - 1;
