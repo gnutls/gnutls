gnutls/gnutls

commit 78ee98e06c7862df38131b12083adc1a0c5eea4a

gnutls_assert_val: was modified to be in line with gnutls_assert()

lib/errors.h
@@ -100,7 +100,7 @@ static inline
 #endif
 int gnutls_assert_val_int(int val, const char *file, int line)
 {
-	_gnutls_assert_log("ASSERT: %s:%d\n", file, line);
+	_gnutls_assert_log( "ASSERT: %s[%s]:%d\n", __FILE__,__func__,__LINE__);
 	return val;
 }
 

commit 190b059429aa2762e98ab781b79540ecf02ff525

gnutls_assert_val: corrected regression from 78ee98e06c7862df38131b12083adc1a0c5eea4a

lib/errors.h
@@ -98,13 +98,13 @@ static inline
 #ifdef __GNUC__
     __attribute__ ((always_inline))
 #endif
-int gnutls_assert_val_int(int val, const char *file, int line)
+int gnutls_assert_val_int(int val, const char *file, const char *func, int line)
 {
-	_gnutls_assert_log( "ASSERT: %s[%s]:%d\n", __FILE__,__func__,__LINE__);
+	_gnutls_assert_log( "ASSERT: %s[%s]:%d\n", file,func,line);
 	return val;
 }
 
-#define gnutls_assert_val(x) gnutls_assert_val_int(x, __FILE__, __LINE__)
-#define gnutls_assert_val_fatal(x) (((x)!=GNUTLS_E_AGAIN && (x)!=GNUTLS_E_INTERRUPTED)?gnutls_assert_val_int(x, __FILE__, __LINE__):(x))
+#define gnutls_assert_val(x) gnutls_assert_val_int(x, __FILE__, __func__, __LINE__)
+#define gnutls_assert_val_fatal(x) (((x)!=GNUTLS_E_AGAIN && (x)!=GNUTLS_E_INTERRUPTED)?gnutls_assert_val_int(x, __FILE__, __func__, __LINE__):(x))
 
 #endif				/* GNUTLS_ERRORS_H */