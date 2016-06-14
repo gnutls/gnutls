07a281936e5e4bc7f9875873744dd40a701b1557
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2016-06-14 13:13:32 +0200

    tests: added check on gnutls_x509_crt_set_crq_extension_by_oid()

--- tests/crq_apis.c
+++ tests/crq_apis.c
@@ -55,44 +55,27 @@ static unsigned char key_pem[] =
     "-----END RSA PRIVATE KEY-----\n";
 const gnutls_datum_t key = { key_pem, sizeof(key_pem) };
 
-void doit(void)
+static gnutls_x509_crq_t generate_crq(void)
 {
-	gnutls_x509_privkey_t pkey;
-	gnutls_x509_crt_t crt;
 	gnutls_x509_crq_t crq;
-	const char *err = NULL;
-
-	gnutls_datum_t out;
-
-	size_t s = 0;
-
-	char smallbuf[10];
-
+	gnutls_x509_privkey_t pkey;
+	const char *err;
 	int ret;
-
-	ret = global_init();
-	if (ret < 0)
-		fail("global_init\n");
-
-	gnutls_global_set_log_function(tls_log_func);
-	if (debug)
-		gnutls_global_set_log_level(4711);
-
-	ret = gnutls_x509_crq_init(&crq);
-	if (ret != 0)
-		fail("gnutls_x509_crq_init\n");
+	size_t s = 0;
+	char smallbuf[10];
+	gnutls_datum_t out;
 
 	ret = gnutls_x509_privkey_init(&pkey);
 	if (ret != 0)
 		fail("gnutls_x509_privkey_init\n");
 
-	ret = gnutls_x509_crt_init(&crt);
-	if (ret != 0)
-		fail("gnutls_x509_crt_init\n");
-
 	ret = gnutls_x509_privkey_import(pkey, &key, GNUTLS_X509_FMT_PEM);
 	if (ret != 0)
 		fail("gnutls_x509_privkey_import\n");
+
+	ret = gnutls_x509_crq_init(&crq);
+	if (ret != 0)
+		fail("gnutls_x509_crq_init\n");
 
 	ret = gnutls_x509_crq_set_version(crq, 0);
 	if (ret != 0)
@@ -206,6 +189,30 @@ void doit(void)
 	if (ret < 0)
 		fail("gnutls_x509_crq_sign2: %s\n", gnutls_strerror(ret));
 
+	gnutls_x509_privkey_deinit(pkey);
+	return crq;
+}
+
+static void run_set_extensions(gnutls_x509_crq_t crq)
+{
+	gnutls_x509_crt_t crt;
+	const char *err = NULL;
+	gnutls_datum_t out;
+	int ret;
+
+	ret = global_init();
+	if (ret < 0)
+		fail("global_init\n");
+
+	gnutls_global_set_log_function(tls_log_func);
+	if (debug)
+		gnutls_global_set_log_level(4711);
+
+
+	ret = gnutls_x509_crt_init(&crt);
+	if (ret != 0)
+		fail("gnutls_x509_crt_init\n");
+
 	ret = gnutls_x509_crt_set_crq(crt, crq);
 	if (ret != 0)
 		fail("gnutls_x509_crt_set_crq: %s\n", gnutls_strerror(ret));
@@ -250,9 +257,111 @@ void doit(void)
 	}
 	gnutls_free(out.data);
 
-	gnutls_x509_crq_deinit(crq);
 	gnutls_x509_crt_deinit(crt);
-	gnutls_x509_privkey_deinit(pkey);
 
 	gnutls_global_deinit();
+}
+
+static void run_set_extension_by_oid(gnutls_x509_crq_t crq)
+{
+	gnutls_x509_crt_t crt;
+	const char *err = NULL;
+	size_t oid_size;
+	gnutls_datum_t out, out2;
+	unsigned i;
+	int ret;
+	char oid[128];
+
+	ret = global_init();
+	if (ret < 0)
+		fail("global_init\n");
+
+	gnutls_global_set_log_function(tls_log_func);
+	if (debug)
+		gnutls_global_set_log_level(4711);
+
+
+	ret = gnutls_x509_crt_init(&crt);
+	if (ret != 0)
+		fail("gnutls_x509_crt_init\n");
+
+	ret = gnutls_x509_crt_set_crq(crt, crq);
+	if (ret != 0)
+		fail("gnutls_x509_crt_set_crq: %s\n", gnutls_strerror(ret));
+
+	ret = gnutls_x509_crt_set_issuer_dn(crt, "cn = my CA, o = big\\, and one", &err);
+	if (ret < 0) {
+		fail("gnutls_x509_crt_set_issuer_dn: %s, %s\n", gnutls_strerror(ret), err);
+	}
+
+	ret = gnutls_x509_crt_set_version(crt, 3);
+	if (ret != 0)
+		fail("gnutls_x509_crt_set_version\n");
+
+	ret = gnutls_x509_crt_set_crq_extension_by_oid(crt, crq, GNUTLS_X509EXT_OID_EXTENDED_KEY_USAGE, 0);
+	if (ret != 0)
+		fail("gnutls_x509_crt_set_crq_extension_by_oid\n");
+
+	oid_size = sizeof(oid);
+	ret = gnutls_x509_crt_get_extension_info(crt, 0, oid, &oid_size, NULL);
+	if (ret != 0)
+		fail("gnutls_x509_crt_get_extension_info\n");
+
+	if (strcmp(oid, GNUTLS_X509EXT_OID_EXTENDED_KEY_USAGE) != 0)
+		fail("strcmp\n");
+
+	ret = gnutls_x509_crt_get_extension_data2(crt, 0, &out);
+	if (ret != 0)
+		fail("gnutls_x509_crt_get_extension_data2\n");
+
+	for (i=0;;i++) {
+		oid_size = sizeof(oid);
+		ret = gnutls_x509_crq_get_extension_info(crq, i, oid, &oid_size, NULL);
+		if (ret < 0)
+			fail("loop: ext not found: %s\n", gnutls_strerror(ret));
+		if (strcmp(oid, GNUTLS_X509EXT_OID_EXTENDED_KEY_USAGE) == 0) {
+			ret = gnutls_x509_crq_get_extension_data2(crq, 3, &out2);
+			if (ret != 0)
+				fail("gnutls_x509_crt_get_extension_data2\n");
+			break;
+		}
+
+	}
+
+	if (out.size != out2.size || memcmp(out.data, out2.data, out.size) != 0) {
+		fail("memcmp %d, %d\n", out.size, out2.size);
+	}
+
+	gnutls_free(out.data);
+	gnutls_free(out2.data);
+
+	oid_size = sizeof(oid);
+	ret = gnutls_x509_crt_get_extension_info(crt, 1, oid, &oid_size, NULL);
+	if (ret != GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE)
+		fail("gnutls_x509_crt_get_extension_info\n");
+
+
+	ret = gnutls_x509_crt_get_raw_dn(crt, &out);
+	if (ret < 0 || out.size == 0)
+		fail("gnutls_x509_crt_get_raw_dn: %s\n", gnutls_strerror(ret));
+
+	if (out.size != 45 ||
+	    memcmp(out.data, "\x30\x2b\x31\x0e\x30\x0c\x06\x03\x55\x04\x03\x13\x05\x6e\x69\x6b\x6f\x73\x31\x19\x30\x17\x06\x03\x55\x04\x0a\x13\x10\x6e\x6f\x6e\x65\x20\x74\x6f\x2c\x20\x6d\x65\x6e\x74\x69\x6f\x6e", 45) != 0) {
+		fail("DN comparison failed\n");
+	}
+	gnutls_free(out.data);
+
+	gnutls_x509_crt_deinit(crt);
+
+	gnutls_global_deinit();
+}
+
+void doit(void)
+{
+	gnutls_x509_crq_t crq = generate_crq();
+
+	run_set_extensions(crq);
+	run_set_extension_by_oid(crq);
+
+	gnutls_x509_crq_deinit(crq);
 }
