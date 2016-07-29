719ada95688699c8a35b5b13632ae1abb5474a47
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-07-29 12:19:25 +0200

    getrandom: use SYS_getrandom instead of __NR_getrandom
    These are identical definitions, but according to syscall()
    SYS_getrandom is the expected value.

--- lib/nettle/rnd-linux.c
+++ lib/nettle/rnd-linux.c
@@ -56,7 +56,7 @@ static dev_t _gnutls_urandom_fd_rdev = 0;
 # else
 #  include <sys/syscall.h>
 #  undef getrandom
-#  define getrandom(dst,s,flags) syscall(__NR_getrandom, (void*)dst, (size_t)s, (unsigned int)flags)
+#  define getrandom(dst,s,flags) syscall(SYS_getrandom, (void*)dst, (size_t)s, (unsigned int)flags)
 # endif
 
 static unsigned have_getrandom(void)
