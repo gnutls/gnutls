c59df1554b55748ea95ad1a89a6bcb6ab91963d2
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-09-12 10:22:37 +0200

    gnutls_certificate_set_*key: ensure proper cleanup on key mismatch failures
    That is, ensure that we keep no local references that are shared with
    the caller, and that we properly free all initialized values.

--- lib/x509.c
+++ lib/x509.c
@@ -1169,9 +1169,12 @@ gnutls_certificate_set_x509_key(gnutls_certificate_credentials_t res,
 
 	res->ncerts++;
 
+	/* after this point we do not deinitialize anything on failure to avoid
+	 * double freeing. We intentionally keep everything as the credentials state
+	 * is documented to be on undefined state. */
 	if ((ret = _gnutls_check_key_cert_match(res)) < 0) {
 		gnutls_assert();
-		goto cleanup;
+		return ret;
 	}
 
 	return res->ncerts-1;
@@ -1387,9 +1390,15 @@ gnutls_certificate_set_key(gnutls_certificate_credentials_t res,
 
 	res->ncerts++;
 
+	/* Unlike gnutls_certificate_set_x509_key, we deinitialize everything
+	 * local after a failure. That is because the caller is responsible for
+	 * freeing these values after a failure, and if we keep references we
+	 * lead to double freeing */
 	if ((ret = _gnutls_check_key_cert_match(res)) < 0) {
 		gnutls_assert();
-		return ret;
+		gnutls_free(new_pcert_list);
+		res->ncerts--;
+		goto cleanup;
 	}
 
 	return res->ncerts-1;
