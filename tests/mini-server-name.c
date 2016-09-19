6d507f38798ae0b201d45050e127f1131b8261f5
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-09-19 15:36:21 +0200

    tests: mini-server-name: skip invalid UTF-8 check if compiled without libidn
    This allows the test suite to run in systems without libidn. Reported
    by Thomas Klausner.

--- tests/mini-server-name.c
+++ tests/mini-server-name.c
@@ -341,8 +341,10 @@ void doit(void)
 	start("empty", 0, "", 0, "", 0);
 	start("test.example.com", 0, "test.example.com", strlen("test.example.com"), "test.example.com", strlen("test.example.com"));
 	start("longtest.example.com", 0, "longtest.example.com.", strlen("longtest.example.com"), "longtest.example.com.", strlen("longtest.example.com"));
+#ifdef HAVE_LIBIDN
 	/* test invalid UTF8 */
 	start("invalid-utf8", 1, "invalid\xff.example.com.", sizeof("invalid\xff.example.com")-1, NULL, 0);
+#endif
 	/* test embedded NULL */
 	start("embedded-NULL", 1, "invalid\x00.example.com.", sizeof("invalid\x00.example.com")-1, NULL, 0);
 }
