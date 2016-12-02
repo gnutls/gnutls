7f93e7f5c649d50e15f8ac3d253cb5926e5757d6
Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>
2016-12-02 06:26:55 +0300

    Add GOST key transport support
    Signed-off-by: Dmitry Eremin-Solenikov <dbaryshkov@gmail.com>

--- lib/crypto-backend.h
+++ lib/crypto-backend.h
@@ -434,4 +434,18 @@ _gnutls_prf_raw(gnutls_mac_algorithm_t mac,
 		size_t seed_size, const uint8_t *seed, size_t outsize,
 		char *out);
 
+int _gnutls_gost_key_wrap(gnutls_gost_paramset_t gost_params,
+			  const gnutls_datum_t *kek,
+			  const gnutls_datum_t *ukm,
+			  const gnutls_datum_t *cek,
+			  gnutls_datum_t *enc,
+			  gnutls_datum_t *imit);
+
+int _gnutls_gost_key_unwrap(gnutls_gost_paramset_t gost_params,
+			    const gnutls_datum_t *kek,
+			    const gnutls_datum_t *ukm,
+			    const gnutls_datum_t *enc,
+			    const gnutls_datum_t *imit,
+			    gnutls_datum_t *cek);
+
 #endif /* GNUTLS_LIB_CRYPTO_BACKEND_H */
