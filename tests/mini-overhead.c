7829c45ebb20309ec89690653b9115b18c701262
Nikos Mavrogiannopoulos <nmav@redhat.com>
2016-12-06 13:07:57 +0100

    tests: updated overhead calculation for new code

--- tests/mini-overhead.c
+++ tests/mini-overhead.c
@@ -319,13 +319,20 @@ void doit(void)
 {
 	signal(SIGCHLD, ch_handler);
 
-	/* 13 + 20(sha1) + 16(iv) + 16(max pad) */
+	/* overhead for CBC depends on MTU */
+
+	/* 13 + 20(sha1) + 16(iv) + 16(pad) */
 	start
-	    ("NONE:+VERS-DTLS1.0:+AES-128-CBC:+SHA1:+SIGN-ALL:+COMP-NULL:+RSA",
+	    ("NONE:+VERS-DTLS1.0:%NO_ETM:+AES-128-CBC:+SHA1:+SIGN-ALL:+COMP-NULL:+RSA",
 	     65);
+
+	/* 13 + 20(sha1) + 8(iv) + 8(max pad) */
+	start
+	    ("NONE:+VERS-DTLS1.0:+3DES-CBC:%NO_ETM:+SHA1:+SIGN-ALL:+COMP-NULL:+RSA",
+	     49);
 	/* 13 + 16(tag) + 4(iv) */
 	start
-	    ("NONE:+VERS-DTLS1.2:+AES-128-GCM:+AEAD:+SIGN-ALL:+COMP-NULL:+RSA",
+	    ("NONE:+VERS-DTLS1.2:+AES-128-GCM:%NO_ETM:+AEAD:+SIGN-ALL:+COMP-NULL:+RSA",
 	     37);
 }
 
