5fdfff4b269b5680d4c9132824a0e6bf7eb048db
Alex Gaynor <alex.gaynor@gmail.com>
2016-12-27 09:45:31 -0500

    Added a fuzzer for OpenPGP cert parsing
    Signed-off-by: Alex Gaynor <alex.gaynor@gmail.com>

---
+++ devel/fuzz/gnutls_openpgp_cert_parser_fuzzer.cc
@@ -0,0 +1,47 @@
+/*
+# Copyright 2016 Alex Gaynor.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
+*/
+
+#include <assert.h>
+#include <stdint.h>
+
+#include <gnutls/gnutls.h>
+#include <gnutls/openpgp.h>
+
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+    gnutls_datum_t raw;
+    gnutls_datum_t out;
+    gnutls_openpgp_crt_t crt;
+    int ret;
+
+    raw.data = (unsigned char *)data;
+    raw.size = size;
+
+    ret = gnutls_openpgp_crt_init(&crt);
+    assert(ret >= 0);
+
+    ret = gnutls_openpgp_crt_import(crt, &raw, GNUTLS_OPENPGP_FMT_RAW);
+    if (ret >= 0) {
+        ret = gnutls_openpgp_crt_print(crt, GNUTLS_CRT_PRINT_FULL, &out);
+        assert(ret >= 0);
+        gnutls_free(out.data);
+    }
+
+    gnutls_openpgp_crt_deinit(crt);
+    return 0;
+}
