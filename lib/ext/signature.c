ef1fc57d142b56071c6191dd89f739c62e369ddb
Nikos Mavrogiannopoulos <nmav@redhat.com>
2017-03-03 16:18:44 +0100

    ext/signature: reject an extension size of zero
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

--- lib/ext/signature.c
+++ lib/ext/signature.c
@@ -128,7 +128,7 @@ _gnutls_sign_algorithm_parse_data(gnutls_session_t session,
 	sig_ext_st *priv;
 	extension_priv_data_t epriv;
 
-	if (data_size % 2 != 0)
+	if (data_size == 0 || data_size % 2 != 0)
 		return gnutls_assert_val(GNUTLS_E_UNEXPECTED_PACKET_LENGTH);
 
 	priv = gnutls_calloc(1, sizeof(*priv));
