2942fd30df4515fc1219c4652e8d320b01d156a2
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2017-07-31 10:50:52 +0200

    fuzz: added SRP server and client fuzzers
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@gnutls.org>

---
+++ devel/fuzz/gnutls_srp_client_fuzzer.cc
@@ -0,0 +1,125 @@
+/*
+ * Copyright (C) 2017 Nikos Mavrogiannopoulos
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include <assert.h>
+#include <fcntl.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <gnutls/gnutls.h>
+
+#include "srp.h"
+
+struct mem_st {
+	const uint8_t *data;
+	size_t size;
+};
+
+#define MIN(x,y) ((x)<(y)?(x):(y))
+static ssize_t
+client_push(gnutls_transport_ptr_t tr, const void *data, size_t len)
+{
+	return len;
+}
+
+static ssize_t client_pull(gnutls_transport_ptr_t tr, void *data, size_t len)
+{
+	struct mem_st *p = (struct mem_st *)tr;
+
+	if (p->size == 0) {
+		return 0;
+	}
+
+	len = MIN(len, p->size);
+	memcpy(data, p->data, len);
+
+	p->size -= len;
+	p->data += len;
+
+	return len;
+}
+
+int client_pull_timeout_func(gnutls_transport_ptr_t tr, unsigned int ms)
+{
+	struct mem_st *p = (struct mem_st *)tr;
+
+	if (p->size > 0)
+		return 1;	/* available data */
+	else
+		return 0;	/* timeout */
+}
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int LLVMFuzzerTestOneInput(const uint8_t * data, size_t size)
+{
+	int res;
+	gnutls_session_t session;
+	gnutls_srp_client_credentials_t pcred;
+	struct mem_st memdata;
+
+	res = gnutls_init(&session, GNUTLS_CLIENT);
+	assert(res >= 0);
+
+	res = gnutls_srp_allocate_client_credentials(&pcred);
+	assert(res >= 0);
+
+	res = gnutls_srp_set_client_credentials(pcred, USERNAME, PASSWORD);
+	assert(res >= 0);
+
+	res = gnutls_credentials_set(session, GNUTLS_CRD_SRP, pcred);
+	assert(res >= 0);
+
+	res = gnutls_priority_set_direct(session, "NORMAL:-KX-ALL:+SRP:+SRP-RSA:+SRP-DSS", NULL);
+	assert(res >= 0);
+
+	memdata.data = data;
+	memdata.size = size;
+
+	gnutls_transport_set_push_function(session, client_push);
+	gnutls_transport_set_pull_function(session, client_pull);
+	gnutls_transport_set_pull_timeout_function(session,
+						   client_pull_timeout_func);
+	gnutls_transport_set_ptr(session, &memdata);
+
+	do {
+		res = gnutls_handshake(session);
+	} while (res < 0 && gnutls_error_is_fatal(res) == 0);
+	if (res >= 0) {
+		while (true) {
+			char buf[16384];
+			res = gnutls_record_recv(session, buf, sizeof(buf));
+			if (res <= 0) {
+				break;
+			}
+		}
+	}
+
+	gnutls_deinit(session);
+	gnutls_srp_free_client_credentials(pcred);
+	return 0;
+}
