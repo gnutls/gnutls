1260c275f38be4ef25754aa202f88346693310ff
Rowan Thorpe <rowan@rowanthorpe.com>
2017-09-27 21:41:43 +0300

    Fix autoreconf invocation to actually run autopoint
    Signed-off-by: Rowan Thorpe <rowan@rowanthorpe.com>

--- cfg.mk
+++ cfg.mk
@@ -64,14 +64,13 @@ autoreconf:
 	for f in $(PODIR)/*.po.in; do \
 		cp $$f `echo $$f | sed 's/.in//'`; \
 	done
-	autopoint
 	for i in po.m4 nls.m4 gettext.m4 codeset.m4 glibc21.m4 glibc2.m4 iconv.m4 intdiv0.m4 intldir.m4 intl.m4 intlmacosx.m4 intmax.m4 inttypes_h.m4 inttypes-pri.m4 lcmessage.m4 lib-ld.m4 lib-link.m4 lib-prefix.m4 lock.m4 longlong.m4 printf-posix.m4 progtest.m4 size_max.m4 stdint_h.m4 uintmax_t.m4 wchar_t.m4 wint_t.m4 visibility.m4 xsize.m4;do \
 		if test -f /usr/share/aclocal/$$i;then \
 			rm -f m4/$$i; \
 		fi; \
 	done
 	touch ChangeLog
-	test -f ./configure || AUTOPOINT=true autoreconf --install
+	test -f ./configure || AUTOPOINT=autopoint autoreconf --install
 
 update-po: refresh-po
 	for f in `ls $(PODIR)/*.po | grep -v quot.po`; do \
