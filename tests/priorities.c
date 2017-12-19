b0cc77556290b8fff7c73c71e931dfc226bfda82
Nikos Mavrogiannopoulos <nmav@redhat.com>
2017-12-19 16:16:29 +0100

    tests: updated to account for HMAC-SHA384 and CAMELLIA removal
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

--- tests/priorities.c
+++ tests/priorities.c
@@ -114,18 +114,18 @@ try_prio_err(const char *prio, int err)
 void doit(void)
 {
 	const int null = 4;
-	int sec128_cs = 53;
-	int sec256_cs = 22;
-	int normal_cs = 53;
-	int normal_ciphers = 11;
-	int pfs_cs = 39;
+	int sec128_cs = 31;
+	int sec256_cs = 12;
+	int normal_cs = 31;
+	int normal_ciphers = 7;
+	int pfs_cs = 23;
 
 	if (gnutls_fips140_mode_enabled()) {
-		normal_cs = 30;
+		normal_cs = 28;
 		normal_ciphers = 6;
 		pfs_cs = 22;
-		sec256_cs = 11;
-		sec128_cs = 30;
+		sec256_cs = 9;
+		sec128_cs = 28;
 	}
 
 	try_prio("NORMAL", normal_cs, normal_ciphers, __LINE__);
@@ -133,17 +133,17 @@ void doit(void)
 
 	if (!gnutls_fips140_mode_enabled()) {
 		try_prio("PFS", pfs_cs, normal_ciphers, __LINE__);
-		try_prio("NORMAL:+CIPHER-ALL", normal_cs, 11, __LINE__);	/* all (except null) */
+		try_prio("NORMAL:+CIPHER-ALL", normal_cs, 7, __LINE__);	/* all (except null) */
 		try_prio("NORMAL:-CIPHER-ALL:+NULL", null, 1, __LINE__);	/* null */
-		try_prio("NORMAL:-CIPHER-ALL:+NULL:+CIPHER-ALL", normal_cs + null, 12, __LINE__);	/* should be null + all */
+		try_prio("NORMAL:-CIPHER-ALL:+NULL:+CIPHER-ALL", normal_cs + null, 8, __LINE__);	/* should be null + all */
 		try_prio("NORMAL:-CIPHER-ALL:+NULL:+CIPHER-ALL:-CIPHER-ALL:+AES-128-CBC", 8, 1, __LINE__);	/* should be null + all */
 	}
 
 	try_prio("PERFORMANCE", normal_cs, normal_ciphers, __LINE__);
-	try_prio("SECURE256", sec256_cs, 6, __LINE__);
-	try_prio("SECURE128", sec128_cs, 11, __LINE__);
-	try_prio("SECURE128:+SECURE256", sec128_cs, 11, __LINE__);	/* should be the same as SECURE128 */
-	try_prio("SECURE128:+SECURE256:+NORMAL", normal_cs, 11, __LINE__);	/* should be the same as NORMAL */
+	try_prio("SECURE256", sec256_cs, 4, __LINE__);
+	try_prio("SECURE128", sec128_cs, 7, __LINE__);
+	try_prio("SECURE128:+SECURE256", sec128_cs, 7, __LINE__);	/* should be the same as SECURE128 */
+	try_prio("SECURE128:+SECURE256:+NORMAL", normal_cs, 7, __LINE__);	/* should be the same as NORMAL */
 	try_prio("SUITEB192", 1, 1, __LINE__);
 	try_prio("SUITEB128", 2, 2, __LINE__);
 	/* check legacy strings */
