gnutls/gnutls

commit 20886264fecba2983b8c4a1b4d3f5c3011f413c5

Use gnutls_strdup() instead of strdup() in library code
Signed-off-by: Tim Rühsen <tim.ruehsen@gmx.de>

lib/priority.c
@@ -1154,7 +1154,7 @@ size_t n, n2 = 0, line_size;
 			ret[n] = 0;
 		}
 	} else {
-		return strdup(p);
+		return gnutls_strdup(p);
 	}
 
 finish:

lib/ext/srp.c
@@ -158,13 +158,13 @@ _gnutls_srp_send_params(gnutls_session_t session,
 			goto cleanup;
 		}
 
-		priv->username = strdup(cred->username);
+		priv->username = gnutls_strdup(cred->username);
 		if (priv->username == NULL) {
 			gnutls_assert();
 			goto cleanup;
 		}
 
-		priv->password = strdup(cred->password);
+		priv->password = gnutls_strdup(cred->password);
 		if (priv->password == NULL) {
 			gnutls_assert();
 			goto cleanup;

commit 9b53438bce4b9b758895f68d5b77235a5e6e37d9

Fix issue introduced in 20886264fe
This makes _gnutls_resolve_priorities() return a string that is always
allocated with the gnutls memory functions.

Signed-off-by: Tim Rühsen <tim.ruehsen@gmx.de>

lib/priority.c
@@ -1056,7 +1056,7 @@ void _gnutls_unload_system_priorities(void)
  * priorities, appended with any additional present in
  * the priorities string.
  *
- * The returned string must be released using free().
+ * The returned string must be released using gnutls_free().
  */
 char *_gnutls_resolve_priorities(const char* priorities)
 {
@@ -1140,7 +1140,7 @@ size_t n, n2 = 0, line_size;
 		if (additional)
 			n2 = strlen(additional);
 
-		ret = malloc(n+n2+1+1);
+		ret = gnutls_malloc(n+n2+1+1);
 		if (ret == NULL) {
 			goto finish;
 		}
@@ -1859,7 +1859,7 @@ gnutls_priority_init(gnutls_priority_t * priority_cache,
 		goto error_cleanup;
 	}
 
-	free(darg);
+	gnutls_free(darg);
 
 	return 0;
 

tests/system-prio-file.c
@@ -57,7 +57,7 @@ try_prio(const char *prio, const char *expected_str)
 	}
 
  ok:
-	free(p);
+	gnutls_free(p);
 	gnutls_global_deinit();
 }
 
@@ -84,4 +84,3 @@ void doit(void)
 	try_prio("@HELLONO:+AES-128-CBC", NULL);
 	try_prio("@HELLONO,:+AES-128-CBC", NULL);
 }
-