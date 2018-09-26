706b1811aa2c0a4bef6e28680f4643318c30cc13
Tim Rühsen <tim.ruehsen@gmx.de>
2018-09-26 12:54:37 +0200

    Make tlsproxy/buffer.c compilable by gcc 4.4.7
    Signed-off-by: Tim Rühsen <tim.ruehsen@gmx.de>

--- doc/examples/tlsproxy/crypto-gnutls.c
+++ doc/examples/tlsproxy/crypto-gnutls.c
@@ -49,7 +49,7 @@ OTHER DEALINGS IN THE SOFTWARE.
 #define FALSE 0
 #define TRUE 1
 
-typedef struct tlssession
+struct tlssession
 {
   gnutls_certificate_credentials_t creds;
   gnutls_session_t session;
@@ -58,7 +58,7 @@ typedef struct tlssession
   int (*erroutfn) (void *opaque, const char *format, va_list ap);
   int debug;
   void *opaque;
-} tlssession_t;
+};
 
 #define BUF_SIZE 65536
 #define BUF_HWM ((BUF_SIZE*3)/4)
