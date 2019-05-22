990fd2f30afa6fcfa66ef1f73cecc2da9910836c
Daiki Ueno <dueno@redhat.com>
2019-05-22 11:51:57 +0200

    tlsfuzzer: use %ALLOW_SMALL_RECORDS for testing
    Signed-off-by: Daiki Ueno <dueno@redhat.com>

--- tests/suite/tls-fuzzer/tls-fuzzer-nocert-tls13.sh
+++ tests/suite/tls-fuzzer/tls-fuzzer-nocert-tls13.sh
@@ -21,7 +21,7 @@
 srcdir="${srcdir:-.}"
 
 tls_fuzzer_prepare() {
-PRIORITY="NORMAL:-VERS-ALL:+VERS-TLS1.3:+VERS-TLS1.2:+VERS-TLS1.1"
+PRIORITY="NORMAL:-VERS-ALL:+VERS-TLS1.3:+VERS-TLS1.2:+VERS-TLS1.1:%ALLOW_SMALL_RECORDS"
 
 sed -e "s|@SERVER@|$SERV|g" -e "s/@PORT@/$PORT/g" -e "s/@PRIORITY@/$PRIORITY/g" ../gnutls-nocert-tls13.json >${TMPFILE}
 }
