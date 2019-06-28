3489f2f6c927605a1a3e2ef9d4e5b09f1aba6714
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2019-06-28 21:08:32 +0200

    gnutls_session_get_desc: avoid printing a NULL value
    When gnutls_session_set_premaster() is used (under openconnect),
    it is possible that gnutls_session_get_desc will print a string like
    this: "(DTLS1.2)-(ECDHE-(null))-(AES-256-GCM)"
    
    With this change we ensure that we do not print null values.
    
    Signed-off-by: Nikos Mavrogiannopoulos <nmav@gnutls.org>

--- lib/session.c
+++ lib/session.c
@@ -413,16 +413,16 @@ char *gnutls_session_get_desc(gnutls_session_t session)
 			return NULL;
 		}
 
-		if (kx == GNUTLS_KX_ECDHE_ECDSA || kx == GNUTLS_KX_ECDHE_RSA || 
-		    kx == GNUTLS_KX_ECDHE_PSK) {
+		if ((kx == GNUTLS_KX_ECDHE_ECDSA || kx == GNUTLS_KX_ECDHE_RSA ||
+		    kx == GNUTLS_KX_ECDHE_PSK) && group_name) {
 			if (sign_str)
 				snprintf(kx_name, sizeof(kx_name), "(ECDHE-%s)-(%s)",
 					 group_name, sign_str);
 			else
 				snprintf(kx_name, sizeof(kx_name), "(ECDHE-%s)",
 					 group_name);
-		} else if (kx == GNUTLS_KX_DHE_DSS || kx == GNUTLS_KX_DHE_RSA || 
-		    kx == GNUTLS_KX_DHE_PSK) {
+		} else if ((kx == GNUTLS_KX_DHE_DSS || kx == GNUTLS_KX_DHE_RSA ||
+		    kx == GNUTLS_KX_DHE_PSK) && group_name) {
 			if (sign_str)
 				snprintf(kx_name, sizeof(kx_name), "(DHE-%s)-(%s)", group_name, sign_str);
 			else
