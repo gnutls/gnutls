gnutls/gnutls

commit bcf4de0371efbdf0846388e2df0cb14b5db09954

handshake: treat reply to HRR as a reply to hello verify request
That is, re-use the client random value on the client hello which
is a reply to a hello retry request.

Relates #299

Signed-off-by: Nikos Mavrogiannopoulos <nmav@redhat.com>

lib/handshake.c
@@ -1868,9 +1868,8 @@ static int send_client_hello(gnutls_session_t session, int again)
 
 		/* Generate random data 
 		 */
-		if (!IS_DTLS(session)
-		    || session->internals.dtls.hsk_hello_verify_requests ==
-		    0) {
+		if (!(session->internals.hsk_flags & HSK_HRR_RECEIVED) &&
+		    !(IS_DTLS(session) && session->internals.dtls.hsk_hello_verify_requests == 0)) {
 			ret = _gnutls_gen_client_random(session);
 			if (ret < 0)
 				return gnutls_assert_val(ret);

commit c01011c2d8533dbbbe754e49e256c109cb848d0d

dtls client hello: fix zeroed random (fixes #960)
This broke with bcf4de03 "handshake: treat reply to HRR as a reply to
hello verify request", which failed to "De Morgan" properly.

Signed-off-by: Stefan Bühler <stbuehler@web.de>

lib/handshake.c
@@ -2167,7 +2167,7 @@ static int send_client_hello(gnutls_session_t session, int again)
 		/* Generate random data
 		 */
 		if (!(session->internals.hsk_flags & HSK_HRR_RECEIVED) &&
-		    !(IS_DTLS(session) && session->internals.dtls.hsk_hello_verify_requests == 0)) {
+		    !(IS_DTLS(session) && session->internals.dtls.hsk_hello_verify_requests != 0)) {
 			ret = _gnutls_gen_client_random(session);
 			if (ret < 0) {
 				gnutls_assert();