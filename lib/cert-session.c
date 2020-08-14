1b1b5b90cc3dd72dc4639c723b97e1ae6792be0a
Daiki Ueno <ueno@gnu.org>
2020-08-14 07:27:40 +0200

    cert-session: check OCSP error responses
    If the OCSP responder returns an error code, such as tryLater, we
    can't proceed to examine the response bytes.  In that case, just skip
    the check unless the stapling is mandatory on this certificate.
    
    Signed-off-by: Daiki Ueno <ueno@gnu.org>

--- lib/cert-session.c
+++ lib/cert-session.c
@@ -224,6 +224,11 @@ gnutls_certificate_set_verify_limits(gnutls_certificate_credentials_t res,
 }
 
 #ifdef ENABLE_OCSP
+static int
+_gnutls_ocsp_verify_mandatory_stapling(gnutls_session_t session,
+				       gnutls_x509_crt_t cert,
+				       unsigned int * ocsp_status);
+
 /* If the certificate is revoked status will be GNUTLS_CERT_REVOKED.
  * 
  * Returns:
@@ -257,6 +262,22 @@ check_ocsp_response(gnutls_session_t session, gnutls_x509_crt_t cert,
 		check_failed = 1;
 		*ostatus |= GNUTLS_CERT_INVALID;
 		*ostatus |= GNUTLS_CERT_INVALID_OCSP_STATUS;
+		goto cleanup;
+	}
+
+	if (gnutls_ocsp_resp_get_status(resp) != GNUTLS_OCSP_RESP_SUCCESSFUL) {
+		ret = _gnutls_ocsp_verify_mandatory_stapling(session, cert, ostatus);
+		if (ret < 0) {
+			gnutls_assert();
+			goto cleanup;
+		}
+		if (*ostatus & GNUTLS_CERT_MISSING_OCSP_STATUS) {
+			_gnutls_audit_log(session,
+					  "Missing basic OCSP response while required: %s.\n",
+					  gnutls_strerror(ret));
+			check_failed = 1;
+		}
+		ret = gnutls_assert_val(0);
 		goto cleanup;
 	}
 
