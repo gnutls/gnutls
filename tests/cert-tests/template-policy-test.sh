72d8c13624d13829f96f373850bd5e16dafa232c
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2021-05-17 13:20:26 -0400

    tests/cert-tests: test a policy without any policyQualifiers.
    Ensure that a policy without policyQualifiers gets created with an
    omitted sequence of qualifiers, rather than an empty sequence of
    qualifiers.
    
    We use NIST's test policy OID for this test.
    
    This tests the fix for #1238.
    
    Signed-off-by: Daniel Kahn Gillmor <dkg@fifthhorseman.net>

---
+++ tests/cert-tests/template-policy-test.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+# Copyright (C) 2021 Free Software Foundation, Inc.
+#
+# This file is part of GnuTLS.
+#
+# GnuTLS is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.
+#
+# GnuTLS is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with GnuTLS; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+#set -e
+
+: ${srcdir=.}
+: ${CERTTOOL=../../src/certtool${EXEEXT}}
+: ${DIFF=diff}
+OUTCERT="policy-cert.$$.tmp"
+
+if ! test -x "${CERTTOOL}"; then
+	exit 77
+fi
+
+export TZ="UTC"
+
+. ${srcdir}/../scripts/common.sh
+
+skip_if_no_datefudge
+
+datefudge -s "2007-04-22" \
+	"${CERTTOOL}" --generate-self-signed \
+		--load-privkey "${srcdir}/data/template-test.key" \
+		--template "${srcdir}/templates/simple-policy.tmpl" \
+		--outfile $OUTCERT #2>/dev/null
+
+${DIFF} "${srcdir}/data/simple-policy.pem" $OUTCERT #>/dev/null 2>&1
+rc=$?
+
+# We're done.
+if test "${rc}" != "0"; then
+	echo "Test with simple policy failed"
+	exit ${rc}
+fi
+
+rm -f "$OUTCERT"
+
+exit 0
