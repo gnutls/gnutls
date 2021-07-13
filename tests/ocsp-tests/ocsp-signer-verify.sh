e666f90be32ae8cf8c77e21f427017cc8e5ab65b
Fiona Klute <fiona.klute@gmx.de>
2021-07-13 23:53:12 +0200

    gnutls_ocsp_resp_verify: Check key purpose if signer not on trust list
    According to [1] the id-kp-OCSPSigning key purpose is only needed for
    delegated signers, not signers explicitly set as trusted. The previous
    code would reject a signature directly from a CA on the trust list
    (without delegation) because the CA certificate didn't contain the
    id-kp-OCSPSigning key purpose.
    
    The tests included in this commit check:
    
    1. Is a signature directly from a CA on the trust list accepted?
    
    2. Is a signature from a delegated signer issued by a CA on the trust
       list accepted?
    
    3. Is a signature from a certificate without id-kp-OCSPSigning issued
       by a CA on the trust list rejected?
    
    Note that the CA in these tests is also the one that issued the
    certificate the OCSP response is for, but the code (current and
    previous) doesn't enforce this.
    
    [1] https://datatracker.ietf.org/doc/html/rfc6960#section-4.2.2.2
    
    Signed-off-by: Fiona Klute <fiona.klute@gmx.de>

---
+++ tests/ocsp-tests/ocsp-signer-verify.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+# Copyright (C) 2021 Fiona Klute
+#
+# This file is part of GnuTLS.
+#
+# GnuTLS is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 3 of the License, or (at
+# your option) any later version.
+#
+# GnuTLS is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+: ${srcdir=.}
+: ${OCSPTOOL=../src/ocsptool${EXEEXT}}
+: ${DIFF=diff}
+
+if ! test -x "${OCSPTOOL}"; then
+    exit 77
+fi
+
+export TZ="UTC"
+
+. "${srcdir}/scripts/common.sh"
+
+skip_if_no_datefudge
+
+date="2021-07-14 00:00"
+sample_dir="${srcdir}/ocsp-tests/signer-verify"
+trusted="${sample_dir}/trust.pem"
+
+verify_response ()
+{
+    echo "verifying ${sample_dir}/${1} using ${trusted}"
+    datefudge --static "${date}" \
+              "${OCSPTOOL}" --infile="${sample_dir}/${1}" \
+              --verify-response --load-trust="${trusted}"
+    return $?
+}
+
+if ! verify_response response-ca.der; then
+    echo "verification of OCSP response signature by CA failed"
+    exit 1
+fi
+
+if ! verify_response response-delegated.der; then
+    echo "verification of OCSP response signature by delegated signer failed"
+    exit 1
+fi
+
+if verify_response response-non-delegated.der; then
+    echo "verification of OCSP response signature by non-signer certificate " \
+         "from the same CA succeeded, but should have failed"
+    exit 1
+fi
