988a3fe07f3ecdd21ff6887a55b775c9b0f4a967
Daiki Ueno <ueno@gnu.org>
2021-09-17 11:03:25 +0200

    wrap_nettle_hash_exists: add missing hash algorithms
    This adds SHAKE-128, SHAKE-256, and RIPEMD-160 to the supported
    algorithms by nettle.  While SHAKEs are not a hash algorithm but an
    XOF, it would be consistent to report they are implemented.
    
    The simple test is expanded to exercise the code
    path (gnutls_digest_get_id → wrap_nettle_hash_exists).
    
    Signed-off-by: Daiki Ueno <ueno@gnu.org>

--- lib/nettle/mac.c
+++ lib/nettle/mac.c
@@ -608,17 +608,20 @@ static int wrap_nettle_hash_exists(gnutls_digest_algorithm_t algo)
 	case GNUTLS_DIG_SHA256:
 	case GNUTLS_DIG_SHA384:
 	case GNUTLS_DIG_SHA512:
-		return 1;
+
+#ifdef NETTLE_SHA3_FIPS202
 	case GNUTLS_DIG_SHA3_224:
 	case GNUTLS_DIG_SHA3_256:
 	case GNUTLS_DIG_SHA3_384:
 	case GNUTLS_DIG_SHA3_512:
-#ifdef NETTLE_SHA3_FIPS202
-		return 1;
-#else
-		return 0;
 #endif
+
+	case GNUTLS_DIG_SHAKE_128:
+	case GNUTLS_DIG_SHAKE_256:
+
 	case GNUTLS_DIG_MD2:
+	case GNUTLS_DIG_RMD160:
+
 #if ENABLE_GOST
 	case GNUTLS_DIG_GOSTR_94:
 	case GNUTLS_DIG_STREEBOG_256:
