a4808160129cce1f06fa1af304afc3df98baa9ae
Alexander Sosedkin <asosedkin@redhat.com>
2022-01-24 17:33:48 +0100

    lib/accelerated/x86/aes-gcm-x86-pclmul-avx: add short buffer checks
    Signed-off-by: Alexander Sosedkin <asosedkin@redhat.com>

--- lib/accelerated/x86/aes-gcm-x86-pclmul-avx.c
+++ lib/accelerated/x86/aes-gcm-x86-pclmul-avx.c
@@ -223,6 +223,9 @@ aes_gcm_decrypt(void *_ctx, const void *src, size_t src_size,
 	if (unlikely(ctx->finished))
 		return gnutls_assert_val(GNUTLS_E_INVALID_REQUEST);
 
+	if (unlikely(dst_size < src_size))
+		return gnutls_assert_val(GNUTLS_E_SHORT_MEMORY_BUFFER);
+
 	gcm_ghash(ctx, src, src_size);
 	ctx->gcm.len.u[1] += src_size;
 
@@ -328,13 +331,16 @@ aesni_gcm_aead_decrypt(void *_ctx,
 	uint8_t tag[MAX_HASH_SIZE];
 	size_t s = 0;
 
-	if (encr_size < tag_size)
+	if (unlikely(encr_size < tag_size))
 		return gnutls_assert_val(GNUTLS_E_DECRYPTION_FAILED);
 
 	aes_gcm_setiv(ctx, nonce, nonce_size);
 	aes_gcm_auth(ctx, auth, auth_size);
 
 	encr_size -= tag_size;
+
+	if (unlikely(plain_size < encr_size))
+		return gnutls_assert_val(GNUTLS_E_SHORT_MEMORY_BUFFER);
 
 	if (encr_size >= 96) {
 		s = aesni_gcm_decrypt(encr, plain, encr_size, ALIGN16(&ctx->expanded_key),
