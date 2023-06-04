04d0277b7b8760e65e9daea996246ee8934c9545
Andreas Metzler <ametzler@bebt.de>
2023-06-04 14:15:45 +0200

    Fix update-copyright-year target for switch from GNU autogen
    Signed-off-by: Andreas Metzler <ametzler@bebt.de>

--- cfg.mk
+++ cfg.mk
@@ -100,7 +100,6 @@ glimport:
 # Update Copyright year in tools and docs
 
 update-copyright-year:
-	$(AM_V_at)$(SED) -i "s/\"2000-[0-9]\{4,\}\"/\"2000-`date +%Y`\"/g" src/args-std.def.in
 	$(AM_V_at)$(SED) -i "s/ 2001-[0-9]\{4,\} / 2001-`date +%Y` /g" doc/gnutls.texi
 	$(AM_V_at)$(SED) -i \
 		-e "s/ 2001-[0-9]\{4,\} / 2001-`date +%Y` /g" \
