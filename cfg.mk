gnutls/gnutls

commit 5a58370864e04f5dbb05b0ae453e6fa592a93175

New make target 'update-copyright-year'
We don't want to automatically update the copyright year as this
prevents reproducible builds.

Instead, 'make update-copyright-year' has to be executed at the
start of each new year and the changes have to be pushed.

Closes #980

Signed-off-by: Tim Rühsen <tim.ruehsen@gmx.de>

cfg.mk
@@ -43,7 +43,6 @@ local-checks-to-skip = sc_GPL_version sc_bindtextdomain			\
 	sc_two_space_separator_in_usage
 
 VC_LIST_ALWAYS_EXCLUDE_REGEX = ^maint.mk|gtk-doc.make|m4/pkg|doc/fdl-1.3.texi|src/.*\.bak|src/crywrap/|(devel/perlasm/|lib/accelerated/x86/|build-aux/|gl/|src/libopts/|tests/suite/ecore/|doc/protocol/).*$$
-update-copyright-env = UPDATE_COPYRIGHT_USE_INTERVALS=1
 
 # Explicit syntax-check exceptions.
 exclude_file_name_regexp--sc_copyright_check = ^./gnulib/.*$$
@@ -77,6 +76,12 @@ glimport:
 	pushd gnulib && git checkout master && git pull && popd
 	echo "If everything looks well, commit the gnulib update with:"
 	echo "  git commit -m "Update gnulib submodule" gnulib"
+
+# Update Copyright year in tools and docs
+
+update-copyright-year:
+	$(AM_V_at)$(SED) -i "s/\"2000-[0-9]\{4,\}\"/\"2000-`date +%Y`\"/g" src/args-std.def.in
+	$(AM_V_at)$(SED) -i "s/ 2001-[0-9]\{4,\} / 2001-`date +%Y` /g" doc/gnutls.texi
 
 # Code Coverage
 

configure.ac
@@ -1098,10 +1098,6 @@ AC_DEFINE([fread_file], [_gnutls_fread_file], [static lib rename])
 AC_DEFINE([read_file], [_gnutls_read_file], [static lib rename])
 AC_DEFINE([read_binary_file], [_gnutls_read_binary_file], [static lib rename])
 
-dnl Some variables needed in makefiles
-YEAR=`date +%Y`
-AC_SUBST([YEAR], $YEAR)
-
 dnl configuration options for config file parsing (inih)
 AC_DEFINE([INI_MAX_LINE], 2048, [inih maximum line size])
 AC_DEFINE([INI_ALLOW_INLINE_COMMENTS], 1, [whether to allowin inline comments])

src/args-std.def.in
@@ -10,7 +10,7 @@ no-misuse-usage;
 export          = '#include <gettext.h>';
 
 copyright = {
-    date  = "2000-@YEAR@";
+    date  = "2000-2020";
     owner = "Free Software Foundation, and others";
     author = "Nikos Mavrogiannopoulos, Simon Josefsson and others; "
              "see /usr/share/doc/gnutls/AUTHORS for a complete list.";

commit ae914ab0ebf99bc3d8a6d504ccf9d552b83ba7c8

Automate and fix manpage copyright year update
Handle manpage copyright year update in update-copyright-year
target, and fix the library manpage copyright year statement, $(YEAR)
expanded to an empty string

Broken by: 5a58370864e04f5dbb05b0ae453e6fa592a93175
Signed-off-by: Andreas Metzler <ametzler@bebt.de>

cfg.mk
@@ -102,6 +102,14 @@ glimport:
 update-copyright-year:
 	$(AM_V_at)$(SED) -i "s/\"2000-[0-9]\{4,\}\"/\"2000-`date +%Y`\"/g" src/args-std.def.in
 	$(AM_V_at)$(SED) -i "s/ 2001-[0-9]\{4,\} / 2001-`date +%Y` /g" doc/gnutls.texi
+	$(AM_V_at)$(SED) -i \
+		-e "s/ 2001-[0-9]\{4,\} / 2001-`date +%Y` /g" \
+		-e "s/ 2020-[0-9]\{4,\} / 2020-`date +%Y` /g" \
+		doc/manpages/Makefile.am
+	$(AM_V_at)$(SED) -i \
+		-e "s/ 2000-[0-9]\{4,\} / 2000-`date +%Y` /g" \
+		doc/Makefile.am \
+		src/Makefile.am
 
 # Code Coverage
 

doc/manpages/Makefile.am
@@ -1367,7 +1367,7 @@ stamp_mans: $(HEADER_FILES)
 			-pkg-site "https://www.gnutls.org" \
 			-include "gnutls/`basename $$header .in`" \
 			-seeinfo $(PACKAGE) -verbatimcopying \
-			-copyright "2001-$(YEAR) Free Software Foundation, Inc., and others" \
+			-copyright "2001-2023 Free Software Foundation, Inc., and others" \
 			-function $$i \
 			$(C_SOURCE_FILES) > $$i.3 && \
 		echo $(ECHO_N) "."; \