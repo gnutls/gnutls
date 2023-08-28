6334d0a55ebb077487e922e9936ac42d5ab3ae2a
Daiki Ueno <ueno@gnu.org>
2023-08-28 15:47:04 +0900

    tests: ignore SIGPIPE in forking tests if the parent is TLS client
    If the tests are forking and the parent process is a client, care must
    be taken to handle SIGPIPE reported by the child process (server) when
    the client closes the connection.
    
    This hadn't been exposed until commit
    dd79ac9bcf90012e090726adf7c1940bcce8333f, thanks to the fact that the
    parent process kept the other pipe end which effectively prevented a
    "broken pipe".
    
    Signed-off-by: Daiki Ueno <ueno@gnu.org>

--- tests/srp.c
+++ tests/srp.c
@@ -271,6 +271,9 @@ static void start(const char *name, const char *prio, const char *user,
 	int ret;
 
 	success("testing: %s\n", name);
+
+	signal(SIGPIPE, SIG_IGN);
+
 	ret = socketpair(AF_UNIX, SOCK_STREAM, 0, fd);
 	if (ret < 0) {
 		perror("socketpair");
