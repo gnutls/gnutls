gnutls/gnutls

commit 38a089b67d635e01c607b6d91f212222defed59e

Updates for padlock hashes in C7 nano. Requires a part of nettle to be included.

lib/accelerated/x86/hmac-padlock.c
@@ -1,0 +1,373 @@
+/*
+ * Copyright (C) 2008, 2010, 2011 Free Software Foundation, Inc.
+ *
+ * Author: Nikos Mavrogiannopoulos
+ *
+ * This file is part of GNUTLS.
+ *
+ * The GNUTLS library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+/* This file provides is the backend hash/mac API for libgcrypt.
+ */
+
+#include <gnutls_int.h>
+#include <gnutls_hash_int.h>
+#include <gnutls_errors.h>
+#include <nettle/sha.h>
+#include <nettle/hmac.h>
+#include <nettle/macros.h>
+#include <aes-padlock.h>
+#include <assert.h>
+#include <sha-padlock.h>
+
+#ifdef ENABLE_NETTLE
+
+#define IPAD 0x36
+#define OPAD 0x5c
+#define MAX_SHA_DIGEST_SIZE (512/8)
+
+typedef void (*update_func) (void *, unsigned, const uint8_t *);
+typedef void (*digest_func) (void *, unsigned, uint8_t *);
+typedef void (*set_key_func) (void *, unsigned, const uint8_t *);
+
+struct padlock_hmac_ctx
+{
+    union
+    {
+        struct hmac_sha224_ctx sha224;
+        struct hmac_sha256_ctx sha256;
+        struct hmac_sha384_ctx sha384;
+        struct hmac_sha512_ctx sha512;
+        struct hmac_sha1_ctx sha1;
+    } ctx;
+
+    /* this is the context just after
+     * the set_key. Used in reset().
+     */
+    union
+    {
+        struct hmac_sha224_ctx sha224;
+        struct hmac_sha256_ctx sha256;
+        struct hmac_sha384_ctx sha384;
+        struct hmac_sha512_ctx sha512;
+        struct hmac_sha1_ctx sha1;
+    } init_ctx;
+    void *ctx_ptr;
+    gnutls_mac_algorithm_t algo;
+    size_t length;
+    update_func update;
+    digest_func digest;
+    set_key_func setkey;
+};
+
+static void
+padlock_hmac_sha1_set_key (struct hmac_sha1_ctx *ctx,
+                           unsigned key_length, const uint8_t * key)
+{
+    HMAC_SET_KEY (ctx, &padlock_sha1, key_length, key);
+}
+
+static void
+padlock_hmac_sha1_update (struct hmac_sha1_ctx *ctx,
+                          unsigned length, const uint8_t * data)
+{
+    padlock_sha1_update (&ctx->state, length, data);
+}
+
+static void
+padlock_hmac_sha1_digest (struct hmac_sha1_ctx *ctx,
+                          unsigned length, uint8_t * digest)
+{
+    HMAC_DIGEST (ctx, &padlock_sha1, length, digest);
+}
+
+static void
+padlock_hmac_sha256_set_key (struct hmac_sha256_ctx *ctx,
+                             unsigned key_length, const uint8_t * key)
+{
+    HMAC_SET_KEY (ctx, &padlock_sha256, key_length, key);
+}
+
+static void
+padlock_hmac_sha256_update (struct hmac_sha256_ctx *ctx,
+                            unsigned length, const uint8_t * data)
+{
+    padlock_sha256_update (&ctx->state, length, data);
+}
+
+static void
+padlock_hmac_sha256_digest (struct hmac_sha256_ctx *ctx,
+                            unsigned length, uint8_t * digest)
+{
+    HMAC_DIGEST (ctx, &padlock_sha256, length, digest);
+}
+
+static void
+padlock_hmac_sha224_set_key (struct hmac_sha224_ctx *ctx,
+                             unsigned key_length, const uint8_t * key)
+{
+    HMAC_SET_KEY (ctx, &padlock_sha224, key_length, key);
+}
+
+static void
+padlock_hmac_sha224_digest (struct hmac_sha224_ctx *ctx,
+                            unsigned length, uint8_t * digest)
+{
+    HMAC_DIGEST (ctx, &padlock_sha224, length, digest);
+}
+
+static void
+padlock_hmac_sha384_set_key (struct hmac_sha384_ctx *ctx,
+                             unsigned key_length, const uint8_t * key)
+{
+    HMAC_SET_KEY (ctx, &padlock_sha384, key_length, key);
+}
+
+static void
+padlock_hmac_sha384_digest (struct hmac_sha384_ctx *ctx,
+                            unsigned length, uint8_t * digest)
+{
+    HMAC_DIGEST (ctx, &padlock_sha384, length, digest);
+}
+
+static void
+padlock_hmac_sha512_set_key (struct hmac_sha512_ctx *ctx,
+                             unsigned key_length, const uint8_t * key)
+{
+    HMAC_SET_KEY (ctx, &padlock_sha512, key_length, key);
+}
+
+static void
+padlock_hmac_sha512_update (struct hmac_sha512_ctx *ctx,
+                            unsigned length, const uint8_t * data)
+{
+    padlock_sha512_update (&ctx->state, length, data);
+}
+
+static void
+padlock_hmac_sha512_digest (struct hmac_sha512_ctx *ctx,
+                            unsigned length, uint8_t * digest)
+{
+    HMAC_DIGEST (ctx, &padlock_sha512, length, digest);
+}
+
+static int
+_hmac_ctx_init (gnutls_mac_algorithm_t algo, struct padlock_hmac_ctx *ctx)
+{
+    switch (algo)
+      {
+      case GNUTLS_MAC_SHA1:
+          ctx->update = (update_func) padlock_hmac_sha1_update;
+          ctx->digest = (digest_func) padlock_hmac_sha1_digest;
+          ctx->setkey = (set_key_func) padlock_hmac_sha1_set_key;
+          ctx->ctx_ptr = &ctx->ctx.sha1;
+          ctx->length = SHA1_DIGEST_SIZE;
+          break;
+      case GNUTLS_MAC_SHA224:
+          ctx->update = (update_func) padlock_hmac_sha256_update;
+          ctx->digest = (digest_func) padlock_hmac_sha224_digest;
+          ctx->setkey = (set_key_func) padlock_hmac_sha224_set_key;
+          ctx->ctx_ptr = &ctx->ctx.sha224;
+          ctx->length = SHA224_DIGEST_SIZE;
+          break;
+      case GNUTLS_MAC_SHA256:
+          ctx->update = (update_func) padlock_hmac_sha256_update;
+          ctx->digest = (digest_func) padlock_hmac_sha256_digest;
+          ctx->setkey = (set_key_func) padlock_hmac_sha256_set_key;
+          ctx->ctx_ptr = &ctx->ctx.sha256;
+          ctx->length = SHA256_DIGEST_SIZE;
+          break;
+      case GNUTLS_MAC_SHA384:
+          ctx->update = (update_func) padlock_hmac_sha512_update;
+          ctx->digest = (digest_func) padlock_hmac_sha384_digest;
+          ctx->setkey = (set_key_func) padlock_hmac_sha384_set_key;
+          ctx->ctx_ptr = &ctx->ctx.sha384;
+          ctx->length = SHA384_DIGEST_SIZE;
+          break;
+      case GNUTLS_MAC_SHA512:
+          ctx->update = (update_func) padlock_hmac_sha512_update;
+          ctx->digest = (digest_func) padlock_hmac_sha512_digest;
+          ctx->setkey = (set_key_func) padlock_hmac_sha512_set_key;
+          ctx->ctx_ptr = &ctx->ctx.sha512;
+          ctx->length = SHA512_DIGEST_SIZE;
+          break;
+      default:
+          gnutls_assert ();
+          return GNUTLS_E_INVALID_REQUEST;
+      }
+
+    return 0;
+}
+
+
+static int
+wrap_padlock_hmac_init (gnutls_mac_algorithm_t algo, void **_ctx)
+{
+    struct padlock_hmac_ctx *ctx;
+    int ret;
+
+    ctx = gnutls_calloc (1, sizeof (struct padlock_hmac_ctx));
+    if (ctx == NULL)
+      {
+          gnutls_assert ();
+          return GNUTLS_E_MEMORY_ERROR;
+      }
+
+    ctx->algo = algo;
+
+    ret = _hmac_ctx_init (algo, ctx);
+    if (ret < 0)
+        return gnutls_assert_val (ret);
+
+    *_ctx = ctx;
+
+    return 0;
+}
+
+static int
+wrap_padlock_hmac_setkey (void *_ctx, const void *key, size_t keylen)
+{
+    struct padlock_hmac_ctx *ctx = _ctx;
+
+    ctx->setkey (ctx->ctx_ptr, keylen, key);
+
+    memcpy (&ctx->init_ctx, &ctx->ctx, sizeof (ctx->ctx));
+
+    return GNUTLS_E_SUCCESS;
+}
+
+static void
+wrap_padlock_hmac_reset (void *_ctx)
+{
+    struct padlock_hmac_ctx *ctx = _ctx;
+
+    memcpy (&ctx->ctx, &ctx->init_ctx, sizeof (ctx->ctx));
+}
+
+static int
+wrap_padlock_hmac_update (void *_ctx, const void *text, size_t textsize)
+{
+    struct padlock_hmac_ctx *ctx = _ctx;
+
+    ctx->update (ctx->ctx_ptr, textsize, text);
+
+    return GNUTLS_E_SUCCESS;
+}
+
+static int
+wrap_padlock_hmac_output (void *src_ctx, void *digest, size_t digestsize)
+{
+    struct padlock_hmac_ctx *ctx;
+    ctx = src_ctx;
+
+    if (digestsize < ctx->length)
+      {
+          gnutls_assert ();
+          return GNUTLS_E_SHORT_MEMORY_BUFFER;
+      }
+
+    ctx->digest (ctx->ctx_ptr, digestsize, digest);
+
+    return 0;
+}
+
+static void
+wrap_padlock_hmac_deinit (void *hd)
+{
+    gnutls_free (hd);
+}
+
+static int
+wrap_padlock_hmac_fast (gnutls_mac_algorithm_t algo,
+                        const void *key, size_t key_size, const void *text,
+                        size_t text_size, void *digest)
+{
+    if (algo == GNUTLS_MAC_SHA1 || algo == GNUTLS_MAC_SHA256)
+      {
+          unsigned char *pad;
+          unsigned char pad2[SHA1_DATA_SIZE + MAX_SHA_DIGEST_SIZE];
+          unsigned char hkey[MAX_SHA_DIGEST_SIZE];
+          unsigned int digest_size = _gnutls_hmac_get_algo_len (algo);
+
+          if (key_size > SHA1_DATA_SIZE)
+            {
+                wrap_padlock_hash_fast (algo, key, key_size, hkey);
+                key = hkey;
+                key_size = digest_size;
+            }
+
+          pad = gnutls_malloc (text_size + SHA1_DATA_SIZE);
+          if (pad == NULL)
+              return gnutls_assert_val (GNUTLS_E_MEMORY_ERROR);
+
+          memset (pad, IPAD, SHA1_DATA_SIZE);
+          memxor (pad, key, key_size);
+
+          memcpy (&pad[SHA1_DATA_SIZE], text, text_size);
+
+          wrap_padlock_hash_fast (algo, pad, text_size + SHA1_DATA_SIZE,
+                                  &pad2[SHA1_DATA_SIZE]);
+
+          gnutls_free (pad);
+
+          memset (pad2, OPAD, SHA1_DATA_SIZE);
+          memxor (pad2, key, key_size);
+
+          wrap_padlock_hash_fast (algo, pad2, digest_size + SHA1_DATA_SIZE,
+                                  digest);
+
+      }
+    else
+      {
+          struct padlock_hmac_ctx ctx;
+          int ret;
+
+          ret = _hmac_ctx_init (algo, &ctx);
+          if (ret < 0)
+              return gnutls_assert_val (ret);
+          ctx.algo = algo;
+
+          wrap_padlock_hmac_setkey (&ctx, key, key_size);
+
+          wrap_padlock_hmac_update (&ctx, text, text_size);
+
+          wrap_padlock_hmac_output (&ctx, digest, ctx.length);
+      }
+
+    return 0;
+}
+
+const gnutls_crypto_mac_st hmac_sha_padlock_struct = {
+    .init = NULL,
+    .setkey = NULL,
+    .hash = NULL,
+    .reset = NULL,
+    .output = NULL,
+    .deinit = NULL,
+    .fast = wrap_padlock_hmac_fast
+};
+
+const gnutls_crypto_mac_st hmac_sha_padlock_nano_struct = {
+    .init = wrap_padlock_hmac_init,
+    .setkey = wrap_padlock_hmac_setkey,
+    .hash = wrap_padlock_hmac_update,
+    .reset = wrap_padlock_hmac_reset,
+    .output = wrap_padlock_hmac_output,
+    .deinit = wrap_padlock_hmac_deinit,
+    .fast = wrap_padlock_hmac_fast,
+};
+
+#endif /* ENABLE_NETTLE */

.gitignore
@@ -489,7 +489,7 @@ gl/tests/test-sys_uio
 gl/tests/inttypes.h 
 gl/time.h
 lib/accelerated/libaccelerated.la
-lib/accelerated/intel/libintel.la
+lib/accelerated/x86/libx86.la
 doc/cyclo/cyclo-gnutls.html
 lib/ext/libgnutls_ext.la
 lib/auth/libgnutls_auth.la

lib/accelerated/Makefile.am
@@ -41,3 +41,11 @@ SUBDIRS += x86
 AM_CFLAGS += -DASM_X86
 libaccelerated_la_LIBADD += x86/libx86.la
 endif
+
+if ASM_X86_32
+AM_CFLAGS += -DASM_X86_32
+endif
+
+if ASM_X86_64
+AM_CFLAGS += -DASM_X86_64
+endif

lib/accelerated/accelerated.c
@@ -20,16 +20,16 @@
  *
  */
 
+#include <config.h>
 #include <accelerated.h>
-#if defined(ASM_X86_32) || defined(ASM_X86_64)
-# include <intel/aes-x86.h>
-# include <intel/x86.h>
+#if defined(ASM_X86)
+# include <x86/aes-x86.h>
+# include <x86/x86.h>
 #endif
 
 void _gnutls_register_accel_crypto(void)
 {
-
-#if defined(ASM_X86_32) || defined(ASM_X86_64)
+#if defined(ASM_X86)
   if (_gnutls_have_cpuid() != 0)
     {
       register_x86_crypto ();

lib/accelerated/x86/Makefile.am
@@ -34,13 +34,13 @@ EXTRA_DIST = README license.txt
 
 noinst_LTLIBRARIES = libx86.la
 
-libx86_la_SOURCES = sha-padlock.c aes-x86.c aes-padlock.c aes-gcm-padlock.c aes-padlock.h aes-x86.h x86.h
+libx86_la_SOURCES = sha-padlock.c hmac-padlock.c aes-x86.c aes-padlock.c aes-gcm-padlock.c aes-padlock.h aes-x86.h x86.h sha-padlock.h
 
 if ASM_X86_64
-AM_CPPFLAGS += -DASM_X86_64
+AM_CFLAGS += -DASM_X86_64 -DASM_X86
 libx86_la_SOURCES += asm/appro-aes-x86-64.s asm/appro-aes-gcm-x86-64.s aes-gcm-x86.c asm/padlock-x86-64.s asm/cpuid-x86-64.s
 else
-AM_CPPFLAGS += -DASM_X86_32
+AM_CFLAGS += -DASM_X86_32 -DASM_X86
 libx86_la_SOURCES += asm/appro-aes-x86.s asm/padlock-x86.s asm/cpuid-x86.s
 endif
 

lib/accelerated/x86/aes-padlock.c
@@ -182,6 +182,15 @@ check_phe (void)
   return ((edx & (0x3 << 10)) == (0x3 << 10));
 }
 
+/* We are actually checking for SHA512 */
+static int
+check_phe_block (void)
+{
+  unsigned int edx = padlock_capability ();
+
+  return ((edx & (0x3 << 25)) == (0x3 << 25));
+}
+
 static unsigned
 check_via (void)
 {
@@ -202,12 +211,8 @@ register_padlock_crypto (void)
 {
   int ret;
 
-  /* Only enable the 32-bit padlock variant, until
-   * the 64-bit code is tested.
-   */
   if (check_via () == 0)
     return;
-
   if (check_padlock ())
     {
       _gnutls_debug_log ("Padlock AES accelerator was detected\n");
@@ -256,7 +261,82 @@ register_padlock_crypto (void)
 #endif
     }
 
-  if (check_phe())
+  if (check_phe_block())
+    {
+      ret =
+          gnutls_crypto_single_digest_register (GNUTLS_DIG_SHA1,
+                                             80, &sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_digest_register (GNUTLS_DIG_SHA256,
+                                             80, &sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_mac_register (GNUTLS_MAC_SHA1,
+                                             80, &hmac_sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_mac_register (GNUTLS_MAC_SHA256,
+                                             80, &hmac_sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_digest_register (GNUTLS_DIG_SHA224,
+                                             80, &sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_digest_register (GNUTLS_DIG_SHA384,
+                                             80, &sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_digest_register (GNUTLS_DIG_SHA512,
+                                             80, &sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_mac_register (GNUTLS_MAC_SHA384,
+                                             80, &hmac_sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+      ret =
+          gnutls_crypto_single_mac_register (GNUTLS_MAC_SHA512,
+                                             80, &hmac_sha_padlock_nano_struct);
+      if (ret < 0)
+        {
+          gnutls_assert ();
+        }
+
+    }
+  else if (check_phe())
     {
       ret =
           gnutls_crypto_single_digest_register (GNUTLS_DIG_SHA1,
@@ -290,6 +370,7 @@ register_padlock_crypto (void)
           gnutls_assert ();
         }
     }
+
 
   return;
 }

lib/accelerated/x86/aes-padlock.h
@@ -31,6 +31,9 @@ extern const gnutls_crypto_cipher_st aes_gcm_padlock_struct;
 extern const gnutls_crypto_mac_st hmac_sha_padlock_struct;
 extern const gnutls_crypto_digest_st sha_padlock_struct;
 
+extern const gnutls_crypto_mac_st hmac_sha_padlock_nano_struct;
+extern const gnutls_crypto_digest_st sha_padlock_nano_struct;
+
 int padlock_aes_cipher_setkey(void *_ctx, const void *userkey, size_t keysize);
 
 /* asm */
@@ -40,7 +43,4 @@ int padlock_ecb_encrypt(void *out, const void *inp,
                         struct padlock_cipher_data *ctx, size_t len);
 int padlock_cbc_encrypt(void *out, const void *inp,
                         struct padlock_cipher_data *ctx, size_t len);
-void padlock_sha1_oneshot(void *ctx, const void *inp, size_t len);
-void padlock_sha256_oneshot(void *ctx, const void *inp, size_t len);
-
 #endif

commit 13e8508fd1b11d76eb414d37d335984c4e3ef4d6

lib/accelerated/x86/hmac-padlock.c: Add gnutls_free() to avoid memory leak
Add gnutls_free() to free ctx if _hmac_ctx_init() fails to avoid memory leak.

Fixes: 38a089b67 ("Updates for padlock hashes in C7 nano. Requires a part of nettle to be included.")
Signed-off-by: JiashengJiang <jiasheng@purdue.edu>

lib/accelerated/x86/hmac-padlock.c
@@ -201,8 +201,10 @@ static int wrap_padlock_hmac_init(gnutls_mac_algorithm_t algo, void **_ctx)
 	ctx->algo = algo;
 
 	ret = _hmac_ctx_init(algo, ctx);
-	if (ret < 0)
+	if (ret < 0) {
+		gnutls_free(ctx);
 		return gnutls_assert_val(ret);
+	}
 
 	*_ctx = ctx;
 