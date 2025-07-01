gnutls/gnutls

commit ead5d40a407d8066f509b8b3f1b542b9e893c7d5

danetool: added option to print the raw entries.

src/danetool.c
@@ -225,6 +225,43 @@ static void dane_check(const char *host, const char *proto,
 		exit(1);
 	}
 
+	if (ENABLED_OPT(PRINT_RAW)) {
+		unsigned entries;
+		gnutls_datum_t t;
+		char **dane_data;
+		int *dane_data_len;
+		int secure;
+		int bogus;
+		
+		ret = dane_query_to_raw_tlsa(q, &entries, &dane_data,
+			&dane_data_len, &secure, &bogus);
+		if (ret < 0) {
+			fprintf(stderr, "dane_query_to_raw_tlsa: %s\n",
+				dane_strerror(ret));
+			exit(1);
+		}
+
+		for (i=0;i<entries;i++) {
+			char *str;
+			size_t str_size;
+			t.data = (void*)dane_data[i];
+			t.size = dane_data_len[i];
+
+			str_size = t.size * 2 + 1;
+			str = gnutls_malloc(str_size);
+
+			ret = gnutls_hex_encode(&t, str, &str_size);
+			if (ret < 0) {
+				fprintf(stderr, "gnutls_hex_encode: %s\n",
+					dane_strerror(ret));
+				exit(1);
+			}
+			fprintf(outfile, "[%u]: %s\n", i, str);
+			gnutls_free(str);
+		}
+		fprintf(outfile, "\n");
+	}
+
 	entries = dane_query_entries(q);
 	for (i = 0; i < entries; i++) {
 		ret = dane_query_data(q, i, &usage, &type, &match, &data);

src/danetool-args.def
@@ -90,6 +90,14 @@ flag = {
 };
 
 flag = {
+    name      = print-raw;
+    descrip   = "Print the received DANE data in raw format";
+    disabled;
+    disable   = "no";
+    doc = "This option will print the received DANE data.";
+};
+
+flag = {
     name      = tlsa-rr;
     descrip   = "Print the DANE RR data on a certificate or public key";
     flags_must = host;

commit 4ae9ded86bb93d462be308b9e8b2f96a3ad1a14d

src/danetool.c: Use gnutls_hex_encode2() instead of gnutls_hex_encode() to avoid memory leak
Fixes: ead5d40a ("danetool: added option to print the raw entries.")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

src/danetool.c
@@ -212,7 +212,6 @@ static void dane_check(const char *host, const char *proto, const char *service,
 	unsigned del = 0;
 	unsigned vflags = DANE_VFLAG_FAIL_IF_NOT_CHECKED;
 	const char *cstr;
-	char *str;
 	gnutls_x509_crt_t *clist = NULL;
 	unsigned int clist_size = 0;
 	gnutls_datum_t certs[MAX_CLIST_SIZE];
@@ -284,22 +283,19 @@ static void dane_check(const char *host, const char *proto, const char *service,
 		}
 
 		for (i = 0; i < entries; i++) {
-			size_t str_size;
+			gnutls_datum_t out = { NULL, 0 };
 			t.data = (void *)dane_data[i];
 			t.size = dane_data_len[i];
 
-			str_size = t.size * 2 + 1;
-			str = gnutls_malloc(str_size);
-
-			ret = gnutls_hex_encode(&t, str, &str_size);
+			ret = gnutls_hex_encode2(&t, &out);
 			if (ret < 0) {
 				fprintf(stderr, "gnutls_hex_encode: %s\n",
 					dane_strerror(ret));
 				retcode = 1;
 				goto error;
 			}
-			fprintf(outfile, "[%u]: %s\n", i, str);
-			gnutls_free(str);
+			fprintf(outfile, "[%u]: %s\n", i, out.data);
+			gnutls_free(out.data);
 		}
 		fprintf(outfile, "\n");
 	}