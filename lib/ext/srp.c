gnutls/gnutls

commit 8b038ab976011a715c495207a750e7cc42165d06

The auth_ and ext_ files were moved to respective directories.

lib/ext/srp.c
@@ -24,12 +24,12 @@
  */
 
 #include <gnutls_int.h>
-#include <ext_srp.h>
+#include <ext/srp.h>
 
 #ifdef ENABLE_SRP
 
 #include "gnutls_auth.h"
-#include "auth_srp.h"
+#include <auth/srp.h>
 #include "gnutls_errors.h"
 #include "gnutls_algorithms.h"
 #include <gnutls_num.h>

lib/auth/srp.c
@@ -28,15 +28,15 @@
 #ifdef ENABLE_SRP
 
 #include "gnutls_errors.h"
-#include "auth_srp_passwd.h"
+#include <auth/srp_passwd.h>
 #include "gnutls_auth.h"
 #include "gnutls_auth.h"
 #include "gnutls_srp.h"
 #include "gnutls_num.h"
-#include "auth_srp.h"
+#include <auth/srp.h>
 #include <gnutls_str.h>
 #include <gnutls_datum.h>
-#include <ext_srp.h>
+#include <ext/srp.h>
 
 const mod_auth_st srp_auth_struct = {
   "SRP",

configure.ac
@@ -415,6 +415,8 @@ AC_CONFIG_FILES([
   tests/suite/Makefile
   lib/accelerated/Makefile
   lib/accelerated/intel/Makefile
+  lib/ext/Makefile
+  lib/auth/Makefile
 ])
 
 AC_OUTPUT

lib/Makefile.am
@@ -24,7 +24,7 @@
 ACLOCAL_AMFLAGS = -I ../m4 -I ../gl/m4
 
 EXTRA_DIST = pakchois/README
-SUBDIRS = includes x509 accelerated
+SUBDIRS = includes x509 accelerated auth ext
 if ENABLE_MINITASN1
 SUBDIRS += minitasn1
 endif
@@ -55,30 +55,24 @@ DISTCLEANFILES = $(pkgconfig_DATA)
 
 lib_LTLIBRARIES = libgnutls.la
 
-SRP_COBJECTS = ext_srp.c gnutls_srp.c auth_srp.c auth_srp_passwd.c	\
-	auth_srp_sb64.c auth_srp_rsa.c
+SRP_COBJECTS = gnutls_srp.c
 
-PSK_COBJECTS = auth_psk.c auth_psk_passwd.c gnutls_psk.c	\
-	auth_dhe_psk.c
-
-SESSION_TICKET_COBJECTS = ext_session_ticket.c
+PSK_COBJECTS = gnutls_psk.c
 
 COBJECTS = gnutls_record.c gnutls_compress.c debug.c gnutls_cipher.c	\
 	gnutls_mbuffers.c gnutls_buffers.c gnutls_handshake.c gnutls_num.c \
 	gnutls_errors.c gnutls_algorithms.c gnutls_dh.c gnutls_kx.c	\
 	gnutls_priority.c gnutls_hash_int.c gnutls_cipher_int.c		\
-	gnutls_session.c gnutls_db.c x509_b64.c auth_anon.c		\
+	gnutls_session.c gnutls_db.c x509_b64.c 		\
 	gnutls_extensions.c gnutls_auth.c gnutls_v2_compat.c		\
-	gnutls_datum.c auth_rsa.c gnutls_session_pack.c gnutls_mpi.c	\
+	gnutls_datum.c gnutls_session_pack.c gnutls_mpi.c	\
 	gnutls_pk.c gnutls_cert.c gnutls_global.c gnutls_constate.c	\
 	gnutls_anon_cred.c pkix_asn1_tab.c gnutls_asn1_tab.c		\
-	gnutls_mem.c auth_cert.c gnutls_ui.c gnutls_sig.c auth_dhe.c	\
-	gnutls_dh_primes.c ext_max_record.c gnutls_alert.c		\
-	gnutls_str.c gnutls_state.c gnutls_x509.c ext_cert_type.c	\
-	gnutls_rsa_export.c auth_rsa_export.c ext_server_name.c		\
-	auth_dh_common.c gnutls_helper.c gnutls_supplemental.c		\
-	crypto.c random.c  ext_signature.c cryptodev.c system.c \
-	crypto-api.c ext_safe_renegotiation.c gnutls_privkey.c \
+	gnutls_mem.c gnutls_ui.c gnutls_sig.c cryptodev.c	\
+	gnutls_dh_primes.c gnutls_alert.c system.c		\
+	gnutls_str.c gnutls_state.c gnutls_x509.c gnutls_rsa_export.c  	\
+	gnutls_helper.c gnutls_supplemental.c crypto.c random.c  \
+	crypto-api.c gnutls_privkey.c \
 	pkcs11.c pkcs11_privkey.c gnutls_pubkey.c pkcs11_write.c locks.c \
 	pkcs11_secret.c hash.c gnutls_dtls.c system_override.c
 
@@ -93,18 +87,16 @@ HFILES = abstract_int.h debug.h gnutls_compress.h gnutls_cipher.h	\
 	gnutls_buffers.h gnutls_errors.h gnutls_int.h gnutls_dtls.h	\
 	gnutls_handshake.h gnutls_num.h gnutls_algorithms.h		\
 	gnutls_dh.h gnutls_kx.h gnutls_hash_int.h gnutls_cipher_int.h	\
-	gnutls_db.h gnutls_auth.h auth_anon.h gnutls_extensions.h	\
-	x509_b64.h gnutls_v2_compat.h gnutls_datum.h auth_cert.h	\
+	gnutls_db.h gnutls_auth.h gnutls_extensions.h	\
+	x509_b64.h gnutls_v2_compat.h gnutls_datum.h \
 	gnutls_mpi.h gnutls_pk.h gnutls_record.h gnutls_cert.h		\
 	gnutls_constate.h gnutls_global.h gnutls_sig.h gnutls_mem.h	\
-	ext_max_record.h gnutls_session_pack.h gnutls_str.h		\
-	gnutls_state.h gnutls_x509.h ext_cert_type.h			\
-	gnutls_rsa_export.h ext_server_name.h auth_dh_common.h		\
-	ext_srp.h gnutls_srp.h auth_srp.h auth_srp_passwd.h		\
-	gnutls_helper.h auth_psk.h auth_psk_passwd.h			\
-	gnutls_supplemental.h crypto.h random.h system.h		\
-	ext_session_ticket.h ext_signature.h gnutls_cryptodev.h		\
-	ext_safe_renegotiation.h locks.h gnutls_mbuffers.h pkcs11_int.h \
+	gnutls_session_pack.h gnutls_str.h		\
+	gnutls_state.h gnutls_x509.h 			\
+	gnutls_rsa_export.h gnutls_srp.h auth/srp.h auth/srp_passwd.h	\
+	gnutls_helper.h gnutls_supplemental.h crypto.h random.h system.h\
+	gnutls_cryptodev.h		\
+	locks.h gnutls_mbuffers.h pkcs11_int.h \
 	hash.h
 
 COBJECTS+=pakchois/pakchois.c pakchois/errors.c pakchois/dlopen.c
@@ -113,14 +105,15 @@ HFILES+=pakchois/pakchois.h pakchois/pakchois11.h pakchois/dlopen.h
 # Separate so we can create the documentation
 
 libgnutls_la_SOURCES = $(HFILES) $(COBJECTS) $(SRP_COBJECTS)	\
-	$(PSK_COBJECTS) $(SESSION_TICKET_COBJECTS) \
+	$(PSK_COBJECTS) \
 	gnutls.asn pkix.asn libgnutls.map
 
 libgnutls_la_LDFLAGS = -no-undefined \
 	-version-info $(LT_CURRENT):$(LT_REVISION):$(LT_AGE)
 
 libgnutls_la_LIBADD = ../gl/libgnu.la x509/libgnutls_x509.la \
-	accelerated/libaccelerated.la \
+	accelerated/libaccelerated.la ext/libgnutls_ext.la \
+	auth/libgnutls_auth.la \
 	$(LTLIBZ)  $(LTLIBINTL) $(LIBSOCKET) $(LTLIBDL)
 
 if ENABLE_OPENPGP

lib/auth/Makefile.am
@@ -1,0 +1,41 @@
+## Process this file with automake to produce Makefile.in
+# Copyright (C) 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
+# Free Software Foundation, Inc.
+#
+# Author: Nikos Mavrogiannopoulos
+#
+# This file is part of GnuTLS.
+#
+# The GnuTLS is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation; either version 2.1 of
+# the License, or (at your option) any later version.
+#
+# The GnuTLS is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with GnuTLS; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA
+
+AM_CFLAGS = $(WERROR_CFLAGS) $(WSTACK_CFLAGS) $(WARN_CFLAGS)
+AM_CPPFLAGS = \
+	-I$(srcdir)/../../gl		\
+	-I$(srcdir)/../includes		\
+	-I$(builddir)/../includes	\
+	-I$(srcdir)/..			\
+	-I$(srcdir)/../opencdk
+
+if ENABLE_MINITASN1
+AM_CPPFLAGS += -I$(srcdir)/../minitasn1
+endif
+
+noinst_LTLIBRARIES = libgnutls_auth.la
+
+libgnutls_auth_la_SOURCES = anon.c cert.c dh_common.c dhe.c \
+	dhe_psk.c psk.c psk_passwd.c rsa.c rsa_export.c srp.c \
+	srp_passwd.c srp_rsa.c srp_sb64.c anon.h cert.h dh_common.h \
+	psk.h psk_passwd.h srp.h srp_passwd.h

lib/auth/anon.c
@@ -35,11 +35,11 @@
 #include "gnutls_auth.h"
 #include "gnutls_errors.h"
 #include "gnutls_dh.h"
-#include "auth_anon.h"
+#include "auth/anon.h"
 #include "gnutls_num.h"
 #include "gnutls_mpi.h"
 #include <gnutls_state.h>
-#include <auth_dh_common.h>
+#include <auth/dh_common.h>
 
 static int gen_anon_server_kx (gnutls_session_t, gnutls_buffer_st*);
 static int proc_anon_client_kx (gnutls_session_t, opaque *, size_t);

lib/auth/anon.h
@@ -25,7 +25,7 @@
 
 /* this is not to be included by gnutls_anon.c */
 #include <gnutls_auth.h>
-#include <auth_dh_common.h>
+#include <auth/dh_common.h>
 
 typedef struct gnutls_anon_server_credentials_st
 {

lib/auth/cert.c
@@ -31,12 +31,12 @@
 #include "gnutls_auth.h"
 #include "gnutls_errors.h"
 #include <gnutls_cert.h>
-#include <auth_cert.h>
+#include <auth/cert.h>
 #include "gnutls_dh.h"
 #include "gnutls_num.h"
 #include "libtasn1.h"
 #include "gnutls_datum.h"
-#include "ext_signature.h"
+#include "ext/signature.h"
 #include <gnutls_pk.h>
 #include <gnutls_algorithms.h>
 #include <gnutls_global.h>

commit e75c58ae30839167b727cfcc5a9ff1965e722e72

lib/ext/srp.c: Add gnutls_free() in the error path
Add gnutls_free() in the error path to avoid potential memory leak if BUFFER_POP_DATUM fails.

Fixes: 8b038ab97 ("The auth_ and ext_ files were moved to respective directories.")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

lib/ext/srp.c
@@ -258,6 +258,7 @@ static int _gnutls_srp_unpack(gnutls_buffer_st *ps,
 error:
 	_gnutls_free_datum(&username);
 	_gnutls_free_datum(&password);
+	gnutls_free(priv);
 	return ret;
 }
 