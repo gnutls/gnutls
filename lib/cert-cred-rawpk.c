gnutls/gnutls

commit 565efaeac828e89d2c1bac7a88c27303d1b62547

Implemented support for raw public-key functionality (RFC7250).
Signed-off-by: Tom Vrancken <dev@tomvrancken.nl>

lib/cert-cred-rawpk.c
@@ -1,0 +1,360 @@
+/*
+ * Copyright (C) 2017 - 2018 ARPA2 project
+ *
+ * Author: Tom Vrancken (dev@tomvrancken.nl)
+ *
+ * This file is part of GnuTLS.
+ *
+ * The GnuTLS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+#include "gnutls_int.h"
+#include <gnutls/gnutls.h>
+#include "datum.h"
+#include "auth/cert.h"
+#include "x509.h"
+#include "cert-cred.h"
+#include "read-file.h"
+#include <stdint.h>
+
+
+/**
+ * gnutls_certificate_set_rawpk_key_mem:
+ * @cred: is a #gnutls_certificate_credentials_t type.
+ * @spki: contains a raw public key in
+ *   PKIX.SubjectPublicKeyInfo format.
+ * @pkey: contains a raw private key.
+ * @format: encoding of the keys. DER or PEM.
+ * @pass: an optional password to unlock the private key pkey.
+ * @key_usage: An ORed sequence of %GNUTLS_KEY_* flags.
+ * @names: is an array of DNS names belonging to the public-key (NULL if none).
+ * @names_length: holds the length of the names list.
+ * @flags: an ORed sequence of #gnutls_pkcs_encrypt_flags_t.
+ *   These apply to the private key pkey.
+ *
+ * This function sets a public/private keypair in the
+ * #gnutls_certificate_credentials_t type to be used for authentication
+ * and/or encryption. @spki and @privkey should match otherwise set
+ * signatures cannot be validated. In case of no match this function
+ * returns %GNUTLS_E_CERTIFICATE_KEY_MISMATCH. This function should
+ * be called once for the client because there is currently no mechanism
+ * to determine which raw public-key to select for the peer when there
+ * are multiple present. Multiple raw public keys for the server can be
+ * distinghuished by setting the @names.
+ *
+ * Note here that @spki is a raw public-key as defined
+ * in RFC7250. It means that there is no surrounding certificate that
+ * holds the public key and that there is therefore no direct mechanism
+ * to prove the authenticity of this key. The keypair can be used during
+ * a TLS handshake but its authenticity should be established via a
+ * different mechanism (e.g. TOFU or known fingerprint).
+ *
+ * The supported formats are basic unencrypted key, PKCS8, PKCS12,
+ * and the openssl format and will be autodetected.
+ *
+ * If the raw public-key and the private key are given in PEM encoding
+ * then the strings that hold their values must be null terminated.
+ *
+ * Key usage (as defined by X.509 extension (2.5.29.15)) can be explicitly
+ * set because there is no certificate structure around the key to define
+ * this value. See for more info gnutls_x509_crt_get_key_usage().
+ *
+ * Note that, this function by default returns zero on success and a
+ * negative value on error. Since 3.5.6, when the flag %GNUTLS_CERTIFICATE_API_V2
+ * is set using gnutls_certificate_set_flags() it returns an index
+ * (greater or equal to zero). That index can be used in other functions
+ * to refer to the added key-pair.
+ *
+ * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, in case the
+ *   key pair does not match %GNUTLS_E_CERTIFICATE_KEY_MISMATCH is returned,
+ *   in other erroneous cases a different negative error code is returned.
+ *
+ * Since: 3.6.6
+ **/
+int gnutls_certificate_set_rawpk_key_mem(gnutls_certificate_credentials_t cred,
+				    const gnutls_datum_t* spki,
+				    const gnutls_datum_t* pkey,
+				    gnutls_x509_crt_fmt_t format,
+				    const char* pass,
+				    unsigned int key_usage,
+				    const char **names,
+				    unsigned int names_length,
+				    unsigned int flags)
+{
+	int ret;
+	gnutls_privkey_t privkey;
+	gnutls_pcert_st* pcert;
+	gnutls_str_array_t str_names;
+	unsigned int i;
+
+	if (pkey == NULL || spki == NULL) {
+		return gnutls_assert_val(GNUTLS_E_INSUFFICIENT_CREDENTIALS);
+	}
+
+	/* Import our private key. This function does all the necessary
+	 * inits, checks and imports. */
+	ret = _gnutls_read_key_mem(cred, pkey->data, pkey->size,
+				format, pass, flags, &privkey);
+	if (ret < 0) {
+		return gnutls_assert_val(ret);
+	}
+
+	/* We now convert our raw public key to a parsed certificate (pcert) structure */
+	pcert = gnutls_calloc(1, sizeof(*pcert));
+	if (pcert == NULL) {
+		gnutls_privkey_deinit(privkey);
+
+		return gnutls_assert_val(GNUTLS_E_MEMORY_ERROR);
+	}
+	// Import our raw public key to the pcert structure
+	ret = gnutls_pcert_import_rawpk_raw(pcert, spki,
+					format, key_usage, 0);
+	if (ret < 0) {
+		gnutls_privkey_deinit(privkey);
+
+		return gnutls_assert_val(ret);
+	}
+
+	/* Process the names, if any */
+	_gnutls_str_array_init(&str_names);
+
+	if (names != NULL && names_length > 0) {
+		for (i = 0; i < names_length; i++) {
+			ret =
+			    _gnutls_str_array_append_idna(&str_names, names[i],
+						     strlen(names[i]));
+			if (ret < 0) {
+				gnutls_privkey_deinit(privkey);
+				_gnutls_str_array_clear(&str_names);
+
+				return gnutls_assert_val(ret);
+			}
+		}
+	}
+
+	/* Now that we have converted the key material to our internal structures
+	 * we can now add them to the credentials structure */
+	ret = _gnutls_certificate_credential_append_keypair(cred, privkey, str_names, pcert, 1);
+	// Check for errors
+	if (ret < 0) {
+		gnutls_privkey_deinit(privkey);
+		gnutls_pcert_deinit(pcert);
+		gnutls_free(pcert);
+
+		return gnutls_assert_val(ret);
+	}
+	// Successfully added a certificate
+	cred->ncerts++;
+
+	/* Check whether the key pair matches.
+	 * After this point we do not deinitialize anything on failure to avoid
+	 * double freeing. We intentionally keep everything as the credentials state
+	 * is documented to be in undefined state. */
+	if ((ret = _gnutls_check_key_cert_match(cred)) < 0) {
+		return gnutls_assert_val(ret);
+	}
+
+	CRED_RET_SUCCESS(cred);
+}
+
+
+/**
+ * gnutls_certificate_set_rawpk_key_file:
+ * @cred: is a #gnutls_certificate_credentials_t type.
+ * @rawpkfile: contains a raw public key in
+ *   PKIX.SubjectPublicKeyInfo format.
+ * @privkeyfile: contains a file path to a private key.
+ * @format: encoding of the keys. DER or PEM.
+ * @pass: an optional password to unlock the private key privkeyfile.
+ * @key_usage: an ORed sequence of %GNUTLS_KEY_* flags.
+ * @names: is an array of DNS names belonging to the public-key (NULL if none).
+ * @names_length: holds the length of the names list.
+ * @privkey_flags: an ORed sequence of #gnutls_pkcs_encrypt_flags_t.
+ *   These apply to the private key pkey.
+ * @pkcs11_flags: one of gnutls_pkcs11_obj_flags. These apply to URLs.
+ *
+ * This function sets a public/private keypair read from file in the
+ * #gnutls_certificate_credentials_t type to be used for authentication
+ * and/or encryption. @spki and @privkey should match otherwise set
+ * signatures cannot be validated. In case of no match this function
+ * returns %GNUTLS_E_CERTIFICATE_KEY_MISMATCH. This function should
+ * be called once for the client because there is currently no mechanism
+ * to determine which raw public-key to select for the peer when there
+ * are multiple present. Multiple raw public keys for the server can be
+ * distinghuished by setting the @names.
+ *
+ * Note here that @spki is a raw public-key as defined
+ * in RFC7250. It means that there is no surrounding certificate that
+ * holds the public key and that there is therefore no direct mechanism
+ * to prove the authenticity of this key. The keypair can be used during
+ * a TLS handshake but its authenticity should be established via a
+ * different mechanism (e.g. TOFU or known fingerprint).
+ *
+ * The supported formats are basic unencrypted key, PKCS8, PKCS12,
+ * and the openssl format and will be autodetected.
+ *
+ * If the raw public-key and the private key are given in PEM encoding
+ * then the strings that hold their values must be null terminated.
+ *
+ * Key usage (as defined by X.509 extension (2.5.29.15)) can be explicitly
+ * set because there is no certificate structure around the key to define
+ * this value. See for more info gnutls_x509_crt_get_key_usage().
+ *
+ * Note that, this function by default returns zero on success and a
+ * negative value on error. Since 3.5.6, when the flag %GNUTLS_CERTIFICATE_API_V2
+ * is set using gnutls_certificate_set_flags() it returns an index
+ * (greater or equal to zero). That index can be used in other functions
+ * to refer to the added key-pair.
+ *
+ * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, in case the
+ *   key pair does not match %GNUTLS_E_CERTIFICATE_KEY_MISMATCH is returned,
+ *   in other erroneous cases a different negative error code is returned.
+ *
+ * Since: 3.6.6
+ */
+int gnutls_certificate_set_rawpk_key_file(gnutls_certificate_credentials_t cred,
+				      const char* rawpkfile,
+				      const char* privkeyfile,
+				      gnutls_x509_crt_fmt_t format,
+				      const char *pass,
+				      unsigned int key_usage,
+				      const char **names,
+				      unsigned int names_length,
+				      unsigned int privkey_flags,
+				      unsigned int pkcs11_flags)
+{
+	int ret;
+	gnutls_privkey_t privkey;
+	gnutls_pubkey_t pubkey;
+	gnutls_pcert_st* pcert;
+	gnutls_datum_t rawpubkey = { NULL, 0 }; // to hold rawpk data from file
+	size_t key_size;
+	gnutls_str_array_t str_names;
+	unsigned int i;
+
+	if (rawpkfile == NULL || privkeyfile == NULL) {
+		return gnutls_assert_val(GNUTLS_E_INSUFFICIENT_CREDENTIALS);
+	}
+
+	/* Import our private key. This function does all the necessary
+	 * inits, checks and imports. */
+	ret = _gnutls_read_key_file(cred, privkeyfile, format, pass, privkey_flags, &privkey);
+	if (ret < 0) {
+		return gnutls_assert_val(ret);
+	}
+
+	pcert = gnutls_calloc(1, sizeof(*pcert));
+	if (pcert == NULL) {
+		gnutls_privkey_deinit(privkey);
+
+		return gnutls_assert_val(GNUTLS_E_MEMORY_ERROR);
+	}
+
+	/* Check whether we are importing our raw public-key from a URL
+	 * or from a regular file.
+	 */
+	if (gnutls_url_is_supported(rawpkfile)) {
+
+		ret = gnutls_pubkey_init(&pubkey);
+		if (ret < 0) {
+			gnutls_privkey_deinit(privkey);
+
+			return gnutls_assert_val(ret);
+		}
+
+		ret = gnutls_pubkey_import_url(pubkey, rawpkfile, pkcs11_flags);
+		if (ret < 0) {
+			gnutls_privkey_deinit(privkey);
+			gnutls_pubkey_deinit(pubkey);
+
+			return gnutls_assert_val(ret);
+		}
+
+		ret = gnutls_pcert_import_rawpk(pcert, pubkey, 0);
+		if (ret < 0) {
+			gnutls_privkey_deinit(privkey);
+			gnutls_pubkey_deinit(pubkey);
+
+			return gnutls_assert_val(ret);
+		}
+
+	} else {
+		/* Read our raw public-key into memory from file */
+		rawpubkey.data = (void*) read_binary_file(rawpkfile, &key_size);
+		if (rawpubkey.data == NULL) {
+			gnutls_privkey_deinit(privkey);
+
+			return gnutls_assert_val(GNUTLS_E_FILE_ERROR);
+		}
+		rawpubkey.size = key_size; // Implicit type casting
+
+		/* We now convert our raw public key that we've loaded into memory to
+		 * a parsed certificate (pcert) structure. Note that rawpubkey will
+		 * be copied into pcert. Therefore we can directly cleanup rawpubkey.
+		 */
+		ret = gnutls_pcert_import_rawpk_raw(pcert, &rawpubkey,
+						format, key_usage, 0);
+
+		_gnutls_free_datum(&rawpubkey);
+
+		if (ret < 0) {
+			gnutls_privkey_deinit(privkey);
+
+			return gnutls_assert_val(ret);
+		}
+
+	}
+
+	/* Process the names, if any */
+	_gnutls_str_array_init(&str_names);
+
+	if (names != NULL && names_length > 0) {
+		for (i = 0; i < names_length; i++) {
+			ret =
+			    _gnutls_str_array_append_idna(&str_names, names[i],
+						     strlen(names[i]));
+			if (ret < 0) {
+				gnutls_privkey_deinit(privkey);
+				_gnutls_str_array_clear(&str_names);
+
+				return gnutls_assert_val(ret);
+			}
+		}
+	}
+
+	/* Now that we have converted the key material to our internal structures
+	 * we can now add them to the credentials structure */
+	ret = _gnutls_certificate_credential_append_keypair(cred, privkey, str_names, pcert, 1);
+	if (ret < 0) {
+		gnutls_privkey_deinit(privkey);
+		gnutls_pcert_deinit(pcert);
+		gnutls_free(pcert);
+
+		return gnutls_assert_val(ret);
+	}
+	// Successfully added a certificate
+	cred->ncerts++;
+
+	/* Check whether the key pair matches.
+	 * After this point we do not deinitialize anything on failure to avoid
+	 * double freeing. We intentionally keep everything as the credentials state
+	 * is documented to be in undefined state. */
+	if ((ret = _gnutls_check_key_cert_match(cred)) < 0) {
+		return gnutls_assert_val(ret);
+	}
+
+	CRED_RET_SUCCESS(cred);
+}
+

.gitignore
@@ -600,6 +600,7 @@ tests/privkey-verify-broken
 tests/psk-file
 tests/pskself
 tests/pubkey-import-export
+tests/rawpk-api
 tests/random-art
 tests/record-pad
 tests/record-retvals
@@ -688,6 +689,7 @@ tests/sign-verify-ed25519-rfc8080
 tests/sign-verify-ext
 tests/sign-verify-ext4
 tests/simple
+tests/slow/cipher-api-test
 tests/slow/cipher-compat
 tests/slow/cipher-openssl-compat
 tests/slow/cipher-override

doc/Makefile.am
@@ -763,6 +763,10 @@ FUNCS += functions/gnutls_certificate_set_params_function
 FUNCS += functions/gnutls_certificate_set_params_function.short
 FUNCS += functions/gnutls_certificate_set_pin_function
 FUNCS += functions/gnutls_certificate_set_pin_function.short
+FUNCS += functions/gnutls_certificate_set_rawpk_key_file
+FUNCS += functions/gnutls_certificate_set_rawpk_key_file.short
+FUNCS += functions/gnutls_certificate_set_rawpk_key_mem
+FUNCS += functions/gnutls_certificate_set_rawpk_key_mem.short
 FUNCS += functions/gnutls_certificate_set_retrieve_function
 FUNCS += functions/gnutls_certificate_set_retrieve_function.short
 FUNCS += functions/gnutls_certificate_set_retrieve_function2
@@ -1265,6 +1269,10 @@ FUNCS += functions/gnutls_pcert_import_openpgp
 FUNCS += functions/gnutls_pcert_import_openpgp.short
 FUNCS += functions/gnutls_pcert_import_openpgp_raw
 FUNCS += functions/gnutls_pcert_import_openpgp_raw.short
+FUNCS += functions/gnutls_pcert_import_rawpk
+FUNCS += functions/gnutls_pcert_import_rawpk.short
+FUNCS += functions/gnutls_pcert_import_rawpk_raw
+FUNCS += functions/gnutls_pcert_import_rawpk_raw.short
 FUNCS += functions/gnutls_pcert_import_x509
 FUNCS += functions/gnutls_pcert_import_x509.short
 FUNCS += functions/gnutls_pcert_import_x509_list

doc/cha-cert-auth.texi
@@ -53,6 +53,7 @@ to use this key exchange algorithm.
 @menu
 * X.509 certificates::
 * OpenPGP certificates::
+* Raw public-keys::
 * Advanced certificate verification::
 * Digital signatures::
 @end menu
@@ -478,6 +479,37 @@ use-cases, is a distraction that consumes considerable resources for
 improving and testing the library. For that we have decided to drop
 this functionality completely in 3.6.0.
 
+@node Raw public-keys
+@subsection Raw public-keys
+@cindex Raw public-keys
+
+There are situations in which a rather large certificate / certificate chain is undesirable or impractical.
+An example could be a resource contrained sensor network in which you do want to use authentication of and
+encryption between your devices but where your devices lack loads of memory or processing power. Furthermore,
+there are situations in which you don't want to or can't rely on a PKIX. TLS is, next to a PKIX environment,
+also commonly used with self-signed certificates in smaller deployments where the self-signed certificates
+are distributed to all involved protocol endpoints out-of-band. This practice does, however, still require
+the overhead of the certificate generation even though none of the information found in the certificate is
+actually used.
+
+With raw public-keys, only a subset of the information found in typical certificates is utilized: namely,
+the SubjectPublicKeyInfo structure (in ASN.1 format) of a PKIX certificate that carries the parameters
+necessary to describe the public-key. Other parameters found in PKIX certificates are omitted. By omitting
+various certificate-related structures, the resulting raw public-key is kept fairly small in comparison to
+the original certificate, and the code to process the keys can be simpler.
+
+It should be noted however, that the authenticity of these raw keys must be verified by an out-of-band mechanism
+or something like @acronym{TOFU}.
+
+@menu
+* Importing raw public-keys::
+@end menu
+
+@node Importing raw public-keys
+@subsubsection Importing raw public-keys
+Raw public-keys and their private counterparts can best be handled by using the abstract types
+@code{gnutls_pubkey_t} and @code{gnutls_privkey_t} respectively. To learn how to use these
+see @ref{Abstract key types}.
 
 @node Advanced certificate verification
 @subsection Advanced certificate verification

doc/cha-gtls-app.texi
@@ -476,6 +476,7 @@ current session using @funcref{gnutls_credentials_set}.
 
 @menu
 * Certificate credentials::
+* Raw public-key credentials::
 * SRP credentials::
 * PSK credentials::
 * Anonymous credentials::
@@ -492,7 +493,7 @@ with the credential types is shown in @ref{tab:key-exchange-cred}.
 
 @headitem Authentication method @tab Key exchange @tab Client credentials @tab Server credentials
 
-@item Certificate
+@item Certificate and Raw public-key
 @tab @code{KX_RSA},
 @code{KX_DHE_RSA},
 @code{KX_DHE_DSS},
@@ -537,6 +538,10 @@ a pair, but a server could require it. In this section we discuss
 general issues applying to both client and server certificates. The next
 section will elaborate on issues arising from client authentication only.
 
+In order to use certificate credentials one must first initialize a credentials
+structure of type @code{gnutls_certificate_credentials_t}. After use this structure must
+be freed. This can be done with the following functions.
+
 @showfuncB{gnutls_certificate_allocate_credentials,gnutls_certificate_free_credentials}
 
 After the credentials structures are initialized, the certificate 
@@ -578,7 +583,7 @@ In that case a certificate should be selected according the peer's signature
 algorithm preferences. To get those preferences use
 @funcref{gnutls_sign_algorithm_get_requested}. Both functions are shown below.
 
-@showfuncC{gnutls_certificate_set_retrieve_function,gnutls_certificate_set_retrieve_function2,gnutls_sign_algorithm_get_requested}
+@showfuncD{gnutls_certificate_set_retrieve_function,gnutls_certificate_set_retrieve_function2,gnutls_certificate_set_retrieve_function3,gnutls_sign_algorithm_get_requested}
 
 The functions above do not handle the requested server name automatically.
 A server would need to check the name requested by the client
@@ -613,8 +618,8 @@ available in certificate authentication.
 @subsubheading Client certificate authentication
 
 If a certificate is to be requested from the client during the handshake, the server
-will send a certificate request message. This behavior is controlled @funcref{gnutls_certificate_server_set_request}.
-The request contains a list of the acceptable by the server certificate signers. This list
+will send a certificate request message. This behavior is controlled by @funcref{gnutls_certificate_server_set_request}.
+The request contains a list of the by the server accepted certificate signers. This list
 is constructed using the trusted certificate authorities of the server.
 In cases where the server supports a large number of certificate authorities
 it makes sense not to advertise all of the names to save bandwidth. That can
@@ -639,6 +644,7 @@ list by the server contains CAs other than its signer. That can be achieved
 using the @code{GNUTLS_FORCE_CLIENT_CERT} flag in @funcref{gnutls_init}.
 
 @showfuncC{gnutls_certificate_set_x509_key_file,gnutls_certificate_set_x509_simple_pkcs12_file,gnutls_certificate_set_retrieve_function2}
+
 
 @subsubheading Client or server certificate verification
 
@@ -670,6 +676,22 @@ can be printed using @funcref{gnutls_certificate_verification_status_print}.
 @showfuncdesc{gnutls_session_set_verify_cert}
 
 @showfuncB{gnutls_certificate_verify_peers3,gnutls_certificate_set_verify_function}
+
+Note that when using raw public-keys verification will not work because there is no corresponding
+certificate body belonging to the raw key that can be verified. In that case the @funcref{gnutls_certificate_verify_peers}
+family of functions will return a GNUTLS_E_INVALID_REQUEST error code. For authenticating raw public-keys
+one must use an out-of-band mechanism, e.g. by comparing hashes or using trust on first use
+(see @ref{Verifying a certificate using trust on first use authentication}).
+
+
+@node Raw public-key credentials
+@subsection Raw public-keys
+As of version 3.6.6 GnuTLS supports @ref{Raw public-keys}. With raw public-keys only the
+public-key part (that is normally embedded in a certificate) is transmitted to the peer.
+In order to load a raw public-key and its corresponding private key in a credentials
+structure one can use the following functions.
+
+@showfuncC{gnutls_certificate_set_key,gnutls_certificate_set_rawpk_key_mem,gnutls_certificate_set_rawpk_key_file}
 
 
 @node SRP credentials
@@ -1438,16 +1460,16 @@ that the CURVE keyword is kept for backwards compatibility only, for new
 applications see the GROUP keyword above.
 
 @item Certificate types @tab
-Certificate type negotitation must be explicitly enabled via the
-GNUTLS_ENABLE_CERT_TYPE_NEG flag in gnutls_init().
 Certificate types can be given in a symmetric fashion (i.e. the same for
 both client and server) or, as of GnuTLS 3.6.4, in an asymmetric fashion
-(i.e. different for the client than for the server).
+(i.e. different for the client than for the server). Alternative certificate
+types must be explicitly enabled via flags in @funcref{gnutls_init}.
 
-Currently supported types are:
-CTYPE-X509 or CTYPE-X.509. Catch all is CTYPE-ALL.
-CTYPE-CLI-X509 or CTYPE-CLI-X.509, CTYPE-SRV-X509 or CTYPE-SRV-X.509.
-Catch all is CTYPE-CLI-ALL and CTYPE-SRV-ALL.
+The currently supported types are CTYPE-X509, CTYPE-RAWPK which apply both to
+client and server; catch all is CTYPE-ALL. The types CTYPE-CLI-X509, CTYPE-SRV-X509,
+CTYPE-CLI-RAWPK, CTYPE-SRV-RAWPK can be used to specialize on client or server;
+catch all is CTYPE-CLI-ALL and CTYPE-SRV-ALL. The type 'X509' is aliased to 'X.509'
+for legacy reasons.
 
 @end multitable
 @caption{The supported algorithm keywords in priority strings.}

doc/cha-tokens.texi
@@ -80,8 +80,12 @@ gnutls_privkey_t abs_key;
 
 @node Abstract public keys
 @subsection Public keys
-An abstract @code{gnutls_pubkey_t} can be initialized
-using the functions below. It can be imported through
+An abstract @code{gnutls_pubkey_t} can be initialized and freed by
+using the functions below.
+
+@showfuncB{gnutls_pubkey_init,gnutls_pubkey_deinit}
+
+After initialization its values can be imported from
 an existing structure like @code{gnutls_x509_crt_t},
 or through an ASN.1 encoding of the X.509 @code{SubjectPublicKeyInfo}
 sequence.
@@ -120,8 +124,12 @@ To export the key-specific parameters, or obtain a unique key ID the following f
 
 @node Abstract private keys
 @subsection Private keys
-An abstract @code{gnutls_privkey_t} can be initialized
-using the functions below. It can be imported through
+An abstract @code{gnutls_privkey_t} can be initialized and freed by
+using the functions below.
+
+@showfuncB{gnutls_privkey_init,gnutls_privkey_deinit}
+
+After initialization its values can be imported from
 an existing structure like @code{gnutls_x509_privkey_t},
 but unlike public keys it cannot be exported. That is
 to allow abstraction over keys stored in hardware that 

doc/manpages/Makefile.am
@@ -183,6 +183,8 @@ APIMANS += gnutls_certificate_set_ocsp_status_request_function2.3
 APIMANS += gnutls_certificate_set_ocsp_status_request_mem.3
 APIMANS += gnutls_certificate_set_params_function.3
 APIMANS += gnutls_certificate_set_pin_function.3
+APIMANS += gnutls_certificate_set_rawpk_key_file.3
+APIMANS += gnutls_certificate_set_rawpk_key_mem.3
 APIMANS += gnutls_certificate_set_retrieve_function.3
 APIMANS += gnutls_certificate_set_retrieve_function2.3
 APIMANS += gnutls_certificate_set_retrieve_function3.3
@@ -434,6 +436,8 @@ APIMANS += gnutls_pcert_export_openpgp.3
 APIMANS += gnutls_pcert_export_x509.3
 APIMANS += gnutls_pcert_import_openpgp.3
 APIMANS += gnutls_pcert_import_openpgp_raw.3
+APIMANS += gnutls_pcert_import_rawpk.3
+APIMANS += gnutls_pcert_import_rawpk_raw.3
 APIMANS += gnutls_pcert_import_x509.3
 APIMANS += gnutls_pcert_import_x509_list.3
 APIMANS += gnutls_pcert_import_x509_raw.3

lib/Makefile.am
@@ -80,7 +80,7 @@ COBJECTS = range.c record.c compress.c debug.c cipher.c gthreads.h handshake-tls
 	system-keys.h urls.c urls.h prf.c auto-verify.c dh-session.c \
 	cert-session.c handshake-checks.c dtls-sw.c dh-primes.c openpgp_compat.c \
 	crypto-selftests.c crypto-selftests-pk.c secrets.c extv.c extv.h \
-	hello_ext_lib.c hello_ext_lib.h ocsp-api.c stek.c
+	hello_ext_lib.c hello_ext_lib.h ocsp-api.c stek.c cert-cred-rawpk.c
 
 if WINDOWS
 COBJECTS += system/keys-win.c
@@ -124,7 +124,7 @@ HFILES = abstract_int.h debug.h cipher.h	\
 	srp.h auth/srp_kx.h auth/srp_passwd.h	\
 	file.h supplemental.h crypto.h random.h system.h\
 	locks.h mbuffers.h ecc.h pin.h fips.h \
-	priority_options.h secrets.h stek.h
+	priority_options.h secrets.h stek.h cert-cred.h
 
 if ENABLE_PKCS11
 HFILES += pkcs11_int.h pkcs11x.h
@@ -216,7 +216,7 @@ libgnutlsxx_la_CPPFLAGS = -I$(top_srcdir)/includes -I$(top_builddir)/includes -I
 
 AM_CXXFLAGS = \
 	-I$(srcdir)/includes	\
-	-I$(builddir)/includes	
+	-I$(builddir)/includes
 
 lib_LTLIBRARIES += libgnutlsxx.la
 

commit 7a4e4dcb1f6a0b75338bfb8964a4b98fe6ee8f55

lib/cert-cred-rawpk.c: Add gnutls_free() and gnutls_pcert_deinit() in the error paths
Add gnutls_free() and gnutls_pcert_deinit() in the error paths to avoid potential memory leak.

Fixes: 565efaeac ("Implemented support for raw public-key functionality (RFC7250).")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

lib/cert-cred-rawpk.c
@@ -117,6 +117,8 @@ int gnutls_certificate_set_rawpk_key_mem(
 	ret = gnutls_pcert_import_rawpk_raw(pcert, spki, format, key_usage, 0);
 	if (ret < 0) {
 		gnutls_privkey_deinit(privkey);
+		gnutls_pcert_deinit(pcert);
+		gnutls_free(pcert);
 
 		return gnutls_assert_val(ret);
 	}
@@ -131,6 +133,8 @@ int gnutls_certificate_set_rawpk_key_mem(
 			if (ret < 0) {
 				gnutls_privkey_deinit(privkey);
 				_gnutls_str_array_clear(&str_names);
+				gnutls_pcert_deinit(pcert);
+				gnutls_free(pcert);
 
 				return gnutls_assert_val(ret);
 			}
@@ -256,6 +260,7 @@ int gnutls_certificate_set_rawpk_key_file(
 		ret = gnutls_pubkey_init(&pubkey);
 		if (ret < 0) {
 			gnutls_privkey_deinit(privkey);
+			gnutls_free(pcert);
 
 			return gnutls_assert_val(ret);
 		}
@@ -264,6 +269,7 @@ int gnutls_certificate_set_rawpk_key_file(
 		if (ret < 0) {
 			gnutls_privkey_deinit(privkey);
 			gnutls_pubkey_deinit(pubkey);
+			gnutls_free(pcert);
 
 			return gnutls_assert_val(ret);
 		}
@@ -272,6 +278,7 @@ int gnutls_certificate_set_rawpk_key_file(
 		if (ret < 0) {
 			gnutls_privkey_deinit(privkey);
 			gnutls_pubkey_deinit(pubkey);
+			gnutls_free(pcert);
 
 			return gnutls_assert_val(ret);
 		}
@@ -285,6 +292,7 @@ int gnutls_certificate_set_rawpk_key_file(
 			rawpkfile, RF_BINARY | RF_SENSITIVE, &key_size);
 		if (rawpubkey.data == NULL) {
 			gnutls_privkey_deinit(privkey);
+			gnutls_free(pcert);
 
 			return gnutls_assert_val(GNUTLS_E_FILE_ERROR);
 		}
@@ -303,6 +311,7 @@ int gnutls_certificate_set_rawpk_key_file(
 
 		if (ret < 0) {
 			gnutls_privkey_deinit(privkey);
+			gnutls_free(pcert);
 
 			return gnutls_assert_val(ret);
 		}
@@ -318,6 +327,7 @@ int gnutls_certificate_set_rawpk_key_file(
 			if (ret < 0) {
 				gnutls_privkey_deinit(privkey);
 				_gnutls_str_array_clear(&str_names);
+				gnutls_free(pcert);
 
 				return gnutls_assert_val(ret);
 			}