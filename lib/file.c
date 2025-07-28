gnutls/gnutls

commit d1428c0f9e87655c5bb8543b443bb54979bf53be

helper.c -> file.c

lib/file.c
@@ -1,0 +1,74 @@
+/*
+ * Copyright (C) 2005-2015 Free Software Foundation, Inc.
+ * Copyright (C) 2015 Nikos Mavrogiannopoulos, Inc.
+ *
+ * Author: Nikos Mavrogiannopoulos
+ *
+ * This file is part of GnuTLS.
+ *
+ * The GnuTLS is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+#include "gnutls_int.h"
+#include <file.h>
+#include <read-file.h>
+
+int _gnutls_file_exists(const char *file)
+{
+	FILE *fd;
+
+	fd = fopen(file, "r");
+	if (fd == NULL)
+		return -1;
+
+	fclose(fd);
+	return 0;
+}
+
+/**
+ * gnutls_load_file:
+ * @filename: the name of the file to load
+ * @data: Where the file will be stored
+ *
+ * This function will load a file into a datum. The data are
+ * zero terminated but the terminating null is not included in length.
+ * The returned data are allocated using gnutls_malloc().
+ *
+ * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, otherwise
+ *   an error code is returned.
+ *
+ * Since 3.1.0
+ **/
+int gnutls_load_file(const char *filename, gnutls_datum_t * data)
+{
+	size_t len;
+
+	data->data = (void *) read_binary_file(filename, &len);
+	if (data->data == NULL)
+		return GNUTLS_E_FILE_ERROR;
+
+	if (malloc != gnutls_malloc) {
+		void *tmp = gnutls_malloc(len);
+
+		memcpy(tmp, data->data, len);
+		free(data->data);
+		data->data = tmp;
+	}
+
+	data->size = len;
+
+	return 0;
+}
+

lib/Makefile.am
@@ -71,7 +71,7 @@ COBJECTS = range.c record.c compress.c debug.c cipher.c			\
 	extensions.c auth.c sslv2_compat.c datum.c session_pack.c mpi.c \
 	pk.c cert.c global.c constate.c anon_cred.c pkix_asn1_tab.c gnutls_asn1_tab.c	\
 	mem.c ui.c vasprintf.c vasprintf.h tls-sig.c ecc.c alert.c privkey_raw.c	\
-	system.c inet_ntop.c str.c state.c x509.c helper.c supplemental.c	\
+	system.c inet_ntop.c str.c state.c x509.c file.c supplemental.c	\
 	random.c crypto-api.c privkey.c pcert.c	pubkey.c locks.c dtls.c 	\
 	system_override.c crypto-backend.c verify-tofu.c pin.c tpm.c fips.c	\
 	safe-memfuncs.c inet_pton.c atfork.c atfork.h \
@@ -107,7 +107,7 @@ HFILES = abstract_int.h debug.h compress.h cipher.h	\
 	session_pack.h str.h str_array.h		\
 	state.h x509.h crypto-backend.h			\
 	srp.h auth/srp_kx.h auth/srp_passwd.h	\
-	helper.h supplemental.h crypto.h random.h system.h\
+	file.h supplemental.h crypto.h random.h system.h\
 	locks.h mbuffers.h ecc.h pin.h fips.h \
 	priority_options.h
 

lib/helper.c
@@ -1,36 +1,0 @@
-/*
- * Copyright (C) 2005-2012 Free Software Foundation, Inc.
- *
- * Author: Nikos Mavrogiannopoulos
- *
- * This file is part of GnuTLS.
- *
- * The GnuTLS is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>
- *
- */
-
-#include "gnutls_int.h"
-#include <helper.h>
-
-int _gnutls_file_exists(const char *file)
-{
-	FILE *fd;
-
-	fd = fopen(file, "r");
-	if (fd == NULL)
-		return -1;
-
-	fclose(fd);
-	return 0;
-}

lib/psk.c
@@ -31,7 +31,7 @@
 
 #include <auth/psk_passwd.h>
 #include <num.h>
-#include <helper.h>
+#include <file.h>
 #include <datum.h>
 #include "debug.h"
 

lib/srp.c
@@ -31,7 +31,7 @@
 #include <auth/srp_passwd.h>
 #include <mpi.h>
 #include <num.h>
-#include <helper.h>
+#include <file.h>
 #include <algorithms.h>
 #include <random.h>
 

lib/ui.c
@@ -34,7 +34,6 @@
 #include <state.h>
 #include <datum.h>
 #include <extras/randomart.h>
-#include <read-file.h>
 #include <algorithms.h>
 
 /**
@@ -729,41 +728,6 @@ gnutls_anon_set_params_function(gnutls_anon_server_credentials_t res,
 	res->params_func = func;
 }
 #endif
-
-/**
- * gnutls_load_file:
- * @filename: the name of the file to load
- * @data: Where the file will be stored
- *
- * This function will load a file into a datum. The data are
- * zero terminated but the terminating null is not included in length.
- * The returned data are allocated using gnutls_malloc().
- *
- * Returns: On success, %GNUTLS_E_SUCCESS (0) is returned, otherwise
- *   an error code is returned.
- *
- * Since 3.1.0
- **/
-int gnutls_load_file(const char *filename, gnutls_datum_t * data)
-{
-	size_t len;
-
-	data->data = (void *) read_binary_file(filename, &len);
-	if (data->data == NULL)
-		return GNUTLS_E_FILE_ERROR;
-
-	if (malloc != gnutls_malloc) {
-		void *tmp = gnutls_malloc(len);
-
-		memcpy(tmp, data->data, len);
-		free(data->data);
-		data->data = tmp;
-	}
-
-	data->size = len;
-
-	return 0;
-}
 
 #ifdef ENABLE_OCSP
 /**

commit d4b550e906337b38e43ed708cf5d12e0dc6a78ac

lib/file.c: Add check for gnutls_malloc()
Add check for the return value of gnutls_malloc() to avoid potential NULL pointer dereference.

Fixes: d1428c0f9 ("helper.c -> file.c")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

lib/file.c
@@ -66,6 +66,12 @@ int gnutls_load_file(const char *filename, gnutls_datum_t *data)
 	if (malloc != gnutls_malloc) {
 		void *tmp = gnutls_malloc(len);
 
+		if (tmp == NULL) {
+			gnutls_free(data->data);
+			data->data = NULL;
+			return GNUTLS_E_MEMORY_ERROR;
+		}
+
 		memcpy(tmp, data->data, len);
 		free(data->data);
 		data->data = tmp;