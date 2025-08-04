gnutls/gnutls

commit be560a8138daced8f37f2b6012e4417311dd4983

Added gnutls_pkcs11_privkey_t and gnutls_privkey_t types. Those are an abstract private key type that can be used to sign/encrypt any private key of pkcs11,x509 or openpgp types. Added support for PKCS11 in gnutls-cli/gnutls-serv.

lib/pkcs11_privkey.c
@@ -1,0 +1,430 @@
+/*
+ * GnuTLS PKCS#11 support
+ * Copyright (C) 2010 Free Software Foundation
+ * 
+ * Author: Nikos Mavrogiannopoulos
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
+ * MA 02111-1307, USA
+*/
+
+#include <gnutls_int.h>
+#include <pakchois/pakchois.h>
+#include <gnutls/pkcs11.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <gnutls_errors.h>
+#include <gnutls_datum.h>
+#include <pkcs11_int.h>
+#include <sign.h>
+
+struct gnutls_pkcs11_privkey_st {
+	pakchois_session_t *pks;
+	ck_object_handle_t privkey;
+	gnutls_pk_algorithm_t pk_algorithm;
+	struct pkcs11_url_info info;
+};
+
+struct privkey_find_data_st {
+	gnutls_pkcs11_privkey_t privkey;
+};
+
+static int find_privkey_url(pakchois_session_t * pks,
+			    struct token_info *info, void *input);
+
+int gnutls_pkcs11_privkey_init(gnutls_pkcs11_privkey_t * key)
+{
+	*key = gnutls_calloc(1, sizeof(struct gnutls_pkcs11_privkey_st));
+	if (*key == NULL) {
+		gnutls_assert();
+		return GNUTLS_E_MEMORY_ERROR;
+	}
+	(*key)->privkey = CK_INVALID_HANDLE;
+	
+	return 0;
+}
+
+void gnutls_pkcs11_privkey_deinit(gnutls_pkcs11_privkey_t key)
+{
+	if (key->pks) {
+fprintf(stderr, "xxx: close session %p at %d\n", key->pks, __LINE__);
+		pakchois_close_session(key->pks);
+        }
+	gnutls_free(key);
+}
+
+int gnutls_pkcs11_privkey_get_pk_algorithm(gnutls_pkcs11_privkey_t key, unsigned int *bits)
+{
+        if (bits)
+          *bits = 0; /* FIXME */
+	return key->pk_algorithm;
+}
+
+int gnutls_pkcs11_privkey_get_info(gnutls_pkcs11_privkey_t pkey,
+				   gnutls_pkcs11_cert_info_t itype,
+				   void *output, size_t * output_size)
+{
+	return pkcs11_get_info(&pkey->info, itype, output, output_size);
+}
+
+#define RETRY_BLOCK_START(key) struct privkey_find_data_st find_data; \
+	int retries = 0; find_data.privkey = key; retry:
+
+
+
+#define RETRY_CHECK(rv, label) { \
+		if (token_func && rv == CKR_SESSION_HANDLE_INVALID) { \
+			ret = token_func(token_data, label, retries++); \
+			if (ret == 0) { \
+				_pkcs11_traverse_tokens(find_privkey_url, &find_data, 1); \
+				goto retry; \
+			} \
+		} \
+	}
+
+/**
+ * gnutls_pkcs11_privkey_sign_data:
+ * @signer: Holds the key
+ * @digest: should be MD5 or SHA1
+ * @flags: should be 0 for now
+ * @data: holds the data to be signed
+ * @signature: will contain the signature allocated with gnutls_malloc()
+ *
+ * This function will sign the given data using a signature algorithm
+ * supported by the private key. Signature algorithms are always used
+ * together with a hash functions.  Different hash functions may be
+ * used for the RSA algorithm, but only SHA-1 for the DSA keys.
+ *
+ * Returns: On success, %GNUTLS_E_SUCCESS is returned, otherwise a
+ *   negative error value.
+ **/
+int
+gnutls_pkcs11_privkey_sign_data(gnutls_pkcs11_privkey_t signer,
+				gnutls_digest_algorithm_t hash,
+				unsigned int flags,
+				const gnutls_datum_t * data,
+				gnutls_datum_t * signature)
+{
+	int ret;
+	gnutls_datum_t digest;
+
+	switch (signer->pk_algorithm) {
+	case GNUTLS_PK_RSA:
+		ret = pk_pkcs1_rsa_hash(hash, data, &digest);
+		if (ret < 0) {
+			gnutls_assert();
+			return ret;
+		}
+		break;
+	case GNUTLS_PK_DSA:
+		ret = pk_dsa_hash(data, &digest);
+		if (ret < 0) {
+			gnutls_assert();
+			return ret;
+		}
+
+		break;
+	default:
+		gnutls_assert();
+		return GNUTLS_E_INTERNAL_ERROR;
+	}
+
+	ret = gnutls_pkcs11_privkey_sign_hash(signer, &digest, signature);
+	_gnutls_free_datum(&digest);
+
+	if (ret < 0) {
+		gnutls_assert();
+		return ret;
+	}
+
+	return 0;
+
+}
+
+
+int gnutls_pkcs11_privkey_sign_hash(gnutls_pkcs11_privkey_t key,
+				    const gnutls_datum_t * hash,
+				    gnutls_datum_t * signature)
+{
+	ck_rv_t rv;
+	int ret;
+	struct ck_mechanism mech;
+	unsigned long siglen;
+
+	RETRY_BLOCK_START(key);
+
+	if (key->privkey == CK_INVALID_HANDLE) {
+		gnutls_assert();
+		return GNUTLS_E_PKCS11_ERROR;
+	}
+
+	mech.mechanism =
+	    key->pk_algorithm == GNUTLS_PK_DSA ? CKM_DSA : CKM_RSA_PKCS;
+	mech.parameter = NULL;
+	mech.parameter_len = 0;
+
+	/* Initialize signing operation; using the private key discovered
+	 * earlier. */
+	rv = pakchois_sign_init(key->pks, &mech, key->privkey);
+	if (rv != CKR_OK) {
+		RETRY_CHECK(rv, key->info.label);
+		gnutls_assert();
+		return GNUTLS_E_PK_SIGN_FAILED;
+	}
+
+	/* Work out how long the signature must be: */
+	rv = pakchois_sign(key->pks, hash->data, hash->size, NULL,
+			   &siglen);
+	if (rv != CKR_OK) {
+		RETRY_CHECK(rv, key->info.label);
+		gnutls_assert();
+		return GNUTLS_E_PK_SIGN_FAILED;
+	}
+
+	signature->data = gnutls_malloc(siglen);
+	signature->size = siglen;
+
+	rv = pakchois_sign(key->pks, hash->data, hash->size,
+			   signature->data, &siglen);
+	if (rv != CKR_OK) {
+		gnutls_free(signature->data);
+		gnutls_assert();
+		return GNUTLS_E_PK_SIGN_FAILED;
+	}
+
+	signature->size = siglen;
+
+	return 0;
+}
+
+static int find_privkey_url(pakchois_session_t * pks,
+			    struct token_info *info, void *input)
+{
+	struct privkey_find_data_st *find_data = input;
+	struct ck_attribute a[4];
+	ck_object_class_t class;
+	ck_rv_t rv;
+	ck_object_handle_t obj;
+	unsigned long count;
+	int found = 0, ret;
+	ck_key_type_t keytype;
+
+	if (info == NULL) {	/* we don't support multiple calls */
+		gnutls_assert();
+		return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	}
+
+	/* do not bother reading the token if basic fields do not match
+	 */
+	if (find_data->privkey->info.manufacturer[0] != 0) {
+		if (strcmp
+		    (find_data->privkey->info.manufacturer,
+		     info->tinfo.manufacturer_id) != 0)
+			return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	}
+
+	if (find_data->privkey->info.token[0] != 0) {
+		if (strcmp
+		    (find_data->privkey->info.token,
+		     info->tinfo.label) != 0)
+			return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	}
+
+	if (find_data->privkey->info.model[0] != 0) {
+		if (strcmp
+		    (find_data->privkey->info.model,
+		     info->tinfo.model) != 0)
+			return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	}
+
+	if (find_data->privkey->info.serial[0] != 0) {
+		if (strcmp
+		    (find_data->privkey->info.serial,
+		     info->tinfo.serial_number) != 0)
+			return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	}
+
+	if (find_data->privkey->info.type[0] != 0) {
+		if (strcmp(find_data->privkey->info.type, "cert") != 0) {
+			gnutls_assert();
+			return GNUTLS_E_UNIMPLEMENTED_FEATURE;
+		}
+	}
+
+	/* search the token for the id */
+        ret = pkcs11_login(pks, info);
+        if (ret < 0) {
+            gnutls_assert();
+            return ret;
+        }
+        
+	/* Find objects with cert class and X.509 cert type. */
+	class = CKO_PRIVATE_KEY;
+
+	a[0].type = CKA_CLASS;
+	a[0].value = &class;
+	a[0].value_len = sizeof class;
+
+	a[1].type = CKA_ID;
+	a[1].value = find_data->privkey->info.certid_raw;
+	a[1].value_len = find_data->privkey->info.certid_raw_size;
+
+
+	rv = pakchois_find_objects_init(pks, a, 2);
+	if (rv != CKR_OK) {
+		gnutls_assert();
+		_gnutls_debug_log("pk11: FindObjectsInit failed.\n");
+		ret = GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+		goto cleanup;
+	}
+
+	while (pakchois_find_objects(pks, &obj, 1, &count) == CKR_OK
+	       && count == 1) {
+
+		a[0].type = CKA_KEY_TYPE;
+		a[0].value = &keytype;
+		a[0].value_len = sizeof keytype;
+
+		if (pakchois_get_attribute_value(pks, obj, a, 1) == CKR_OK) {
+			if (keytype == CKK_RSA)
+				find_data->privkey->pk_algorithm = GNUTLS_PK_RSA;
+			else if (keytype == CKK_DSA)
+				find_data->privkey->pk_algorithm = GNUTLS_PK_DSA;
+			else {
+				gnutls_assert();
+				ret =
+				    GNUTLS_E_UNSUPPORTED_CERTIFICATE_TYPE;
+				goto cleanup;
+			}
+			find_data->privkey->pks = pks;
+			find_data->privkey->privkey = obj;
+			found = 1;
+		} else {
+			_gnutls_debug_log
+			    ("pk11: Skipped cert, missing attrs.\n");
+		}
+	}
+
+	if (found == 0) {
+		gnutls_assert();
+		ret = GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	} else {
+		ret = 0;
+	}
+
+      cleanup:
+	pakchois_find_objects_final(pks);
+
+	return ret;
+}
+
+int gnutls_pkcs11_privkey_import_url(gnutls_pkcs11_privkey_t pkey,
+				     const char *url)
+{
+	int ret;
+	struct privkey_find_data_st find_data;
+
+	/* fill in the find data structure */
+	find_data.privkey = pkey;
+
+	ret = pkcs11_url_to_info(url, &pkey->info);
+	if (ret < 0) {
+		gnutls_assert();
+		return ret;
+	}
+
+	if (pkey->info.id[0] == 0) {
+		gnutls_assert();
+		return GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE;
+	}
+
+	ret = _pkcs11_traverse_tokens(find_privkey_url, &find_data, 1);
+	if (ret < 0) {
+		gnutls_assert();
+		return ret;
+	}
+
+	return 0;
+}
+
+/**
+ * gnutls_pkcs11_privkey_decrypt_data:
+ * @key: Holds the key
+ * @flags: should be 0 for now
+ * @ciphertext: holds the data to be signed
+ * @plaintext: will contain the plaintext, allocated with gnutls_malloc()
+ *
+ * This function will decrypt the given data using the public key algorithm
+ * supported by the private key. 
+ *
+ * Returns: On success, %GNUTLS_E_SUCCESS is returned, otherwise a
+ *   negative error value.
+ **/
+int
+gnutls_pkcs11_privkey_decrypt_data(gnutls_pkcs11_privkey_t key,
+  unsigned int flags, const gnutls_datum_t * ciphertext,
+				gnutls_datum_t * plaintext)
+{
+	ck_rv_t rv;
+	int ret;
+	struct ck_mechanism mech;
+	unsigned long siglen;
+
+	RETRY_BLOCK_START(key);
+
+	if (key->privkey == CK_INVALID_HANDLE) {
+		gnutls_assert();
+		return GNUTLS_E_PKCS11_ERROR;
+	}
+
+	mech.mechanism =
+	    key->pk_algorithm == GNUTLS_PK_DSA ? CKM_DSA : CKM_RSA_PKCS;
+	mech.parameter = NULL;
+	mech.parameter_len = 0;
+
+	/* Initialize signing operation; using the private key discovered
+	 * earlier. */
+	rv = pakchois_decrypt_init(key->pks, &mech, key->privkey);
+	if (rv != CKR_OK) {
+		RETRY_CHECK(rv, key->info.label);
+		gnutls_assert();
+		return GNUTLS_E_PK_DECRYPTION_FAILED;
+	}
+
+	/* Work out how long the plaintext must be: */
+	rv = pakchois_decrypt(key->pks, ciphertext->data, ciphertext->size, NULL,
+			   &siglen);
+	if (rv != CKR_OK) {
+		RETRY_CHECK(rv, key->info.label);
+		gnutls_assert();
+		return GNUTLS_E_PK_DECRYPTION_FAILED;
+	}
+
+	plaintext->data = gnutls_malloc(siglen);
+	plaintext->size = siglen;
+
+	rv = pakchois_decrypt(key->pks, ciphertext->data, ciphertext->size,
+			   plaintext->data, &siglen);
+	if (rv != CKR_OK) {
+		gnutls_free(plaintext->data);
+		gnutls_assert();
+		return GNUTLS_E_PK_DECRYPTION_FAILED;
+	}
+
+	plaintext->size = siglen;
+
+	return 0;
+}

doc/manpages/Makefile.am
@@ -178,6 +178,7 @@ APIMANS += gnutls_priority_deinit.3
 APIMANS += gnutls_priority_set_direct.3
 APIMANS += gnutls_set_default_priority.3
 APIMANS += gnutls_set_default_export_priority.3
+APIMANS += gnutls_privkey_sign_data.3
 APIMANS += gnutls_psk_free_client_credentials.3
 APIMANS += gnutls_psk_allocate_client_credentials.3
 APIMANS += gnutls_psk_set_client_credentials.3
@@ -273,6 +274,8 @@ APIMANS += gnutls_certificate_set_x509_crl_file.3
 APIMANS += gnutls_certificate_set_x509_simple_pkcs12_file.3
 APIMANS += gnutls_certificate_set_x509_simple_pkcs12_mem.3
 APIMANS += gnutls_certificate_free_crls.3
+APIMANS += gnutls_pkcs11_privkey_sign_data.3
+APIMANS += gnutls_pkcs11_privkey_decrypt_data.3
 APIMANS += gnutls_pem_base64_encode.3
 APIMANS += gnutls_pem_base64_encode_alloc.3
 APIMANS += gnutls_pem_base64_decode.3
@@ -422,8 +425,9 @@ APIMANS += gnutls_x509_privkey_export_rsa_raw.3
 APIMANS += gnutls_x509_privkey_export_dsa_raw.3
 APIMANS += gnutls_x509_privkey_generate.3
 APIMANS += gnutls_x509_privkey_get_key_id.3
-APIMANS += gnutls_x509_privkey_sign_data.3
+APIMANS += gnutls_x509_privkey_sign_data2.3
 APIMANS += gnutls_x509_privkey_sign_hash.3
+APIMANS += gnutls_x509_privkey_sign_data.3
 APIMANS += gnutls_x509_privkey_verify_data.3
 APIMANS += gnutls_x509_privkey_fix.3
 APIMANS += gnutls_x509_privkey_export_pkcs8.3
@@ -523,6 +527,7 @@ APIMANS += gnutls_certificate_set_openpgp_keyring_file.3
 APIMANS += gnutls_certificate_set_openpgp_keyring_mem.3
 APIMANS += gnutls_openpgp_set_recv_key_function.3
 APIMANS += gnutls_openpgp_privkey_sign_hash.3
+APIMANS += gnutls_openpgp_privkey_decrypt_data.3
 APIMANS += gnutls_openpgp_crt_print.3
 APIMANS += gnutls_openpgp_crt_init.3
 APIMANS += gnutls_openpgp_crt_deinit.3

lib/Makefile.am
@@ -82,10 +82,10 @@ COBJECTS = gnutls_record.c gnutls_compress.c debug.c gnutls_cipher.c	\
 	auth_dh_common.c gnutls_helper.c gnutls_supplemental.c		\
 	crypto.c random.c pk-libgcrypt.c mpi-libgcrypt.c cryptodev.c	\
 	rnd-libgcrypt.c cipher-libgcrypt.c mac-libgcrypt.c ext_signature.c \
-	crypto-api.c ext_safe_renegotiation.c gnutls_mbuffers.c
+	crypto-api.c ext_safe_renegotiation.c gnutls_mbuffers.c gnutls_privkey.c
 
 if ENABLE_PKCS11
-COBJECTS += pkcs11.c
+COBJECTS += pkcs11.c pkcs11_privkey.c
 endif
 
 if ENABLE_OPRFI

lib/auth_cert.c
@@ -45,21 +45,23 @@
 #include <gnutls_state.h>
 #include <gnutls_pk.h>
 #include <gnutls_x509.h>
+#include <gnutls/privkey.h>
 #include "debug.h"
 
 #ifdef ENABLE_OPENPGP
 # include "openpgp/gnutls_openpgp.h"
 
-static gnutls_privkey *alloc_and_load_pgp_key (const gnutls_openpgp_privkey_t
-					       key);
+static gnutls_privkey_t alloc_and_load_pgp_key (const gnutls_openpgp_privkey_t
+					       key, int deinit);
 static gnutls_cert *alloc_and_load_pgp_certs (gnutls_openpgp_crt_t cert);
 
 #endif
 
 static gnutls_cert *alloc_and_load_x509_certs (gnutls_x509_crt_t * certs,
 					       unsigned);
-static gnutls_privkey *alloc_and_load_x509_key (gnutls_x509_privkey_t key);
+static gnutls_privkey_t alloc_and_load_x509_key (gnutls_x509_privkey_t key, int deinit);
 
+static gnutls_privkey_t alloc_and_load_pkcs11_key ( gnutls_pkcs11_privkey_t key, int deinit);
 
 
 /* Copies data from a internal certificate struct (gnutls_cert) to 
@@ -409,21 +411,47 @@ get_issuers (gnutls_session_t session,
   return 0;
 }
 
+static void st_to_st2(gnutls_retr2_st *st2, gnutls_retr_st* st)
+{
+	st2->cert_type = st->type;
+	if (st->type == GNUTLS_CRT_OPENPGP) {
+		st2->key_type = GNUTLS_PRIVKEY_OPENPGP;
+	} else {
+		st2->key_type = GNUTLS_PRIVKEY_X509;
+	}
+	st2->ncerts = st->ncerts;
+	st2->deinit_all = st->deinit_all;
+	
+	switch(st2->cert_type) {
+		case GNUTLS_CRT_OPENPGP:
+			st2->cert.pgp = st->cert.pgp;
+			st2->key.pgp = st->key.pgp;
+			break;
+		case GNUTLS_CRT_X509:
+			st2->cert.x509 = st->cert.x509;
+			st2->key.x509 = st->key.x509;
+			break;
+		default:
+			return;
+	}
+
+}
+
 /* Calls the client get callback.
  */
 static int
 call_get_cert_callback (gnutls_session_t session,
-			gnutls_datum_t * issuers_dn,
+			const gnutls_datum_t * issuers_dn,
 			int issuers_dn_length,
 			gnutls_pk_algorithm_t * pk_algos, int pk_algos_length)
 {
   unsigned i;
   gnutls_cert *local_certs = NULL;
-  gnutls_privkey *local_key = NULL;
-  gnutls_retr_st st;
+  gnutls_privkey_t local_key = NULL;
   int ret = GNUTLS_E_INTERNAL_ERROR;
   gnutls_certificate_type_t type = gnutls_certificate_type_get (session);
   gnutls_certificate_credentials_t cred;
+  gnutls_retr2_st st2;
 
   cred = (gnutls_certificate_credentials_t)
     _gnutls_get_cred (session->key, GNUTLS_CRD_CERTIFICATE, NULL);
@@ -433,29 +461,42 @@ call_get_cert_callback (gnutls_session_t session,
       return GNUTLS_E_INSUFFICIENT_CREDENTIALS;
     }
 
-  memset (&st, 0, sizeof (st));
+  memset (&st2, 0, sizeof (st2));
 
-  if (session->security_parameters.entity == GNUTLS_SERVER)
-    {
-      if (cred->server_get_cert_callback == NULL)
+  if (cred->get_cert_callback)
 	{
-	  gnutls_assert ();
-	  return GNUTLS_E_INTERNAL_ERROR;
+		ret = cred->get_cert_callback( session, issuers_dn, issuers_dn_length,
+					pk_algos, pk_algos_length, &st2);
+
 	}
-      ret = cred->server_get_cert_callback (session, &st);
-    }
   else
-    {				/* CLIENT */
-      if (cred->client_get_cert_callback == NULL)
-	{
-	  gnutls_assert ();
-	  return GNUTLS_E_INTERNAL_ERROR;
+    { /* compatibility mode */
+	  gnutls_retr_st st;
+	  memset (&st, 0, sizeof (st));
+	  if (session->security_parameters.entity == GNUTLS_SERVER)
+		{
+			if (cred->server_get_cert_callback == NULL)
+				{
+				  gnutls_assert ();
+				  return GNUTLS_E_INTERNAL_ERROR;
+				}
+			ret = cred->server_get_cert_callback (session, &st);
+			if (ret >= 0) st_to_st2(&st2, &st);
+		}
+	  else
+		{				/* CLIENT */
+
+		  if (cred->client_get_cert_callback == NULL)
+			{
+			  gnutls_assert ();
+			  return GNUTLS_E_INTERNAL_ERROR;
+			}
+		  ret =	cred->client_get_cert_callback (session,
+						issuers_dn, issuers_dn_length,
+						pk_algos, pk_algos_length, &st);
+		  if (ret >= 0) st_to_st2(&st2, &st);
+		}
 	}
-      ret =
-	cred->client_get_cert_callback (session,
-					issuers_dn, issuers_dn_length,
-					pk_algos, pk_algos_length, &st);
-    }
 
   if (ret < 0)
     {
@@ -463,86 +504,118 @@ call_get_cert_callback (gnutls_session_t session,
       return GNUTLS_E_INTERNAL_ERROR;
     }
 
-  if (st.ncerts == 0)
+  if (st2.ncerts == 0)
     return 0;			/* no certificate was selected */
 
-  if (type != st.type)
+  if (type != st2.cert_type)
     {
       gnutls_assert ();
       ret = GNUTLS_E_INVALID_REQUEST;
       goto cleanup;
     }
 
+
   if (type == GNUTLS_CRT_X509)
     {
-      local_certs = alloc_and_load_x509_certs (st.cert.x509, st.ncerts);
-      if (local_certs != NULL && st.key.x509 != NULL)
-	{
-	  local_key = alloc_and_load_x509_key (st.key.x509);
-	  if (local_key == NULL)
-	    {
-	      gnutls_assert ();
-	      ret = GNUTLS_E_INTERNAL_ERROR;
-	      goto cleanup;
-	    }
-	}
-
+      local_certs = alloc_and_load_x509_certs (st2.cert.x509, st2.ncerts);
     }
   else
     {				/* PGP */
-      if (st.ncerts > 1)
-	{
-	  gnutls_assert ();
-	  ret = GNUTLS_E_INVALID_REQUEST;
-	  goto cleanup;
-	}
-
+      if (st2.ncerts > 1)
+		{
+		  gnutls_assert ();
+		  ret = GNUTLS_E_INVALID_REQUEST;
+		  goto cleanup;
+		}
 #ifdef ENABLE_OPENPGP
-      {
-	local_certs = alloc_and_load_pgp_certs (st.cert.pgp);
-	if (local_certs != NULL && st.key.pgp != NULL)
 	  {
-	    local_key = alloc_and_load_pgp_key (st.key.pgp);
-	    if (local_key == NULL)
-	      {
-		gnutls_assert ();
-		ret = GNUTLS_E_INTERNAL_ERROR;
-		goto cleanup;
-	      }
-	  }
+		local_certs = alloc_and_load_pgp_certs (st2.cert.pgp);
       }
+#else
+		ret = GNUTLS_E_UNIMPLEMENTED_FEATURE;
+		goto cleanup;
 #endif
     }
 
+  if (local_certs == NULL) {
+	  gnutls_assert();
+	  ret = GNUTLS_E_MEMORY_ERROR;
+	  goto cleanup;
+  }
+
+  switch(st2.key_type) {
+	  case GNUTLS_PRIVKEY_OPENPGP:
+#ifdef ENABLE_OPENPGP
+		if (st2.key.pgp != NULL)
+		  {
+			local_key = alloc_and_load_pgp_key (st2.key.pgp, st2.deinit_all);
+			if (local_key == NULL)
+			  {
+				gnutls_assert ();
+				ret = GNUTLS_E_INTERNAL_ERROR;
+				goto cleanup;
+			  }
+		  }
+		  break;
+#endif
+	  case GNUTLS_PRIVKEY_PKCS11:
+		if (st2.key.pkcs11 != NULL)
+		  {
+			local_key = alloc_and_load_pkcs11_key (st2.key.pkcs11, st2.deinit_all);
+			if (local_key == NULL)
+			  {
+				gnutls_assert ();
+				ret = GNUTLS_E_INTERNAL_ERROR;
+				goto cleanup;
+			  }
+		  }
+		  break;
+	  case GNUTLS_PRIVKEY_X509:
+		  if (st2.key.x509 != NULL)
+			{
+			  local_key = alloc_and_load_x509_key (st2.key.x509, st2.deinit_all);
+			  if (local_key == NULL)
+				{
+				  gnutls_assert ();
+				  ret = GNUTLS_E_INTERNAL_ERROR;
+				  goto cleanup;
+				}
+			}
+			break;
+	}
+
   _gnutls_selected_certs_set (session, local_certs,
-			      (local_certs != NULL) ? st.ncerts : 0,
+			      (local_certs != NULL) ? st2.ncerts : 0,
 			      local_key, 1);
 
   ret = 0;
 
 cleanup:
 
-  if (st.type == GNUTLS_CRT_X509)
+  if (st2.cert_type == GNUTLS_CRT_X509)
     {
-      if (st.deinit_all)
-	{
-	  for (i = 0; i < st.ncerts; i++)
-	    {
-	      gnutls_x509_crt_deinit (st.cert.x509[i]);
-	    }
-	  gnutls_free (st.cert.x509);
-	  gnutls_x509_privkey_deinit (st.key.x509);
-	}
+      if (st2.deinit_all)
+		{
+		  for (i = 0; i < st2.ncerts; i++)
+			{
+			  gnutls_x509_crt_deinit (st2.cert.x509[i]);
+			}
+		}
     }
   else
     {
 #ifdef ENABLE_OPENPGP
-      if (st.deinit_all)
-	{
-	  gnutls_openpgp_crt_deinit (st.cert.pgp);
-	  gnutls_openpgp_privkey_deinit (st.key.pgp);
-	}
+	if (st2.deinit_all)
+		{
+		  gnutls_openpgp_crt_deinit (st2.cert.pgp);
+		}
 #endif
+    }
+    
+  if (ret < 0)
+    {
+      if (local_key != NULL)
+        gnutls_privkey_deinit(local_key);
     }
 
   return ret;
@@ -576,7 +649,7 @@ _select_client_cert (gnutls_session_t session,
       return GNUTLS_E_INSUFFICIENT_CREDENTIALS;
     }
 
-  if (cred->client_get_cert_callback != NULL)
+  if (cred->client_get_cert_callback != NULL || cred->get_cert_callback != NULL)
     {
 
       /* use a callback to get certificate 
@@ -646,7 +719,7 @@ _select_client_cert (gnutls_session_t session,
 	  _gnutls_selected_certs_set (session,
 				      &cred->cert_list[indx][0],
 				      cred->cert_list_length[indx],
-				      &cred->pkey[indx], 0);
+				      cred->pkey[indx], 0);
 	}
       else
 	{
@@ -671,7 +744,7 @@ _gnutls_gen_x509_crt (gnutls_session_t session, opaque ** data)
   int ret, i;
   opaque *pdata;
   gnutls_cert *apr_cert_list;
-  gnutls_privkey *apr_pkey;
+  gnutls_privkey_t apr_pkey;
   int apr_cert_list_length;
 
   /* find the appropriate certificate 
@@ -730,7 +803,7 @@ _gnutls_gen_openpgp_certificate (gnutls_session_t session, opaque ** data)
   int ret;
   opaque *pdata;
   gnutls_cert *apr_cert_list;
-  gnutls_privkey *apr_pkey;
+  gnutls_privkey_t apr_pkey;
   int apr_cert_list_length;
 
   /* find the appropriate certificate */
@@ -804,7 +877,7 @@ _gnutls_gen_openpgp_certificate_fpr (gnutls_session_t session, opaque ** data)
   size_t fpr_size;
   opaque *pdata;
   gnutls_cert *apr_cert_list;
-  gnutls_privkey *apr_pkey;
+  gnutls_privkey_t apr_pkey;
   int apr_cert_list_length;
 
   /* find the appropriate certificate */
@@ -1443,7 +1516,7 @@ _gnutls_gen_cert_client_cert_vrfy (gnutls_session_t session, opaque ** data)
 {
   int ret;
   gnutls_cert *apr_cert_list;
-  gnutls_privkey *apr_pkey;
+  gnutls_privkey_t apr_pkey;
   int apr_cert_list_length, size;
   gnutls_datum_t signature;
   int total_data;
@@ -1685,7 +1758,7 @@ int
 _gnutls_get_selected_cert (gnutls_session_t session,
 			   gnutls_cert ** apr_cert_list,
 			   int *apr_cert_list_length,
-			   gnutls_privkey ** apr_pkey)
+			   gnutls_privkey_t * apr_pkey)
 {
   if (session->security_parameters.entity == GNUTLS_SERVER)
     {
@@ -1764,26 +1837,27 @@ alloc_and_load_x509_certs (gnutls_x509_crt_t * certs, unsigned ncerts)
 /* converts the given x509 key to gnutls_privkey* and allocates
  * space for it.
  */
-static gnutls_privkey *
-alloc_and_load_x509_key (gnutls_x509_privkey_t key)
+static gnutls_privkey_t
+alloc_and_load_x509_key (gnutls_x509_privkey_t key, int deinit)
 {
-  gnutls_privkey *local_key;
+  gnutls_privkey_t local_key;
   int ret = 0;
 
   if (key == NULL)
     return NULL;
 
-  local_key = gnutls_malloc (sizeof (gnutls_privkey));
-  if (local_key == NULL)
+  ret = gnutls_privkey_init(&local_key);
+  if (ret < 0) 
     {
       gnutls_assert ();
       return NULL;
     }
 
-  ret = _gnutls_x509_privkey_to_gkey (local_key, key);
+  ret = gnutls_privkey_import_x509 (local_key, key, deinit?GNUTLS_PRIVKEY_IMPORT_AUTO_RELEASE:0);
   if (ret < 0)
     {
       gnutls_assert ();
+      gnutls_privkey_deinit(local_key);
       return NULL;
     }
 
@@ -1838,32 +1912,63 @@ alloc_and_load_pgp_certs (gnutls_openpgp_crt_t cert)
 /* converts the given raw key to gnutls_privkey* and allocates
  * space for it.
  */
-static gnutls_privkey *
-alloc_and_load_pgp_key (const gnutls_openpgp_privkey_t key)
+static gnutls_privkey_t
+alloc_and_load_pgp_key ( gnutls_openpgp_privkey_t key, int deinit)
 {
-  gnutls_privkey *local_key;
+  gnutls_privkey_t local_key;
   int ret = 0;
 
   if (key == NULL)
     return NULL;
 
-  local_key = gnutls_malloc (sizeof (gnutls_privkey));
-  if (local_key == NULL)
-    {
-      gnutls_assert ();
-      return NULL;
-    }
-
-  ret = _gnutls_openpgp_privkey_to_gkey (local_key, key);
+  ret = gnutls_privkey_init(&local_key);
   if (ret < 0)
     {
       gnutls_assert ();
       return NULL;
     }
 
+  ret = gnutls_privkey_import_openpgp (local_key, key, deinit?GNUTLS_PRIVKEY_IMPORT_AUTO_RELEASE:0);
+  if (ret < 0)
+    {
+      gnutls_assert ();
+      gnutls_privkey_deinit(local_key);
+      return NULL;
+    }
+
   return local_key;
 }
 #endif
+
+/* converts the given raw key to gnutls_privkey* and allocates
+ * space for it.
+ */
+static gnutls_privkey_t
+alloc_and_load_pkcs11_key ( gnutls_pkcs11_privkey_t key, int deinit)
+{
+  gnutls_privkey_t local_key;
+  int ret = 0;
+
+  if (key == NULL)
+    return NULL;
+
+  ret = gnutls_privkey_init(&local_key);
+  if (ret < 0)
+    {
+      gnutls_assert ();
+      return NULL;
+    }
+
+  ret = gnutls_privkey_import_pkcs11 (local_key, key, deinit?GNUTLS_PRIVKEY_IMPORT_AUTO_RELEASE:0);
+  if (ret < 0)
+    {
+      gnutls_assert ();
+      gnutls_privkey_deinit(local_key);
+      return NULL;
+    }
+
+  return local_key;
+}
 
 void
 _gnutls_selected_certs_deinit (gnutls_session_t session)
@@ -1880,12 +1985,7 @@ _gnutls_selected_certs_deinit (gnutls_session_t session)
       session->internals.selected_cert_list = NULL;
       session->internals.selected_cert_list_length = 0;
 
-      _gnutls_gkey_deinit (session->internals.selected_key);
-      if (session->internals.selected_key)
-	{
-	  gnutls_free (session->internals.selected_key);
-	  session->internals.selected_key = NULL;
-	}
+      session->internals.selected_key = NULL;
     }
 
   return;
@@ -1894,7 +1994,7 @@ _gnutls_selected_certs_deinit (gnutls_session_t session)
 void
 _gnutls_selected_certs_set (gnutls_session_t session,
 			    gnutls_cert * certs, int ncerts,
-			    gnutls_privkey * key, int need_free)
+			    gnutls_privkey_t key, int need_free)
 {
   _gnutls_selected_certs_deinit (session);
 
@@ -1979,7 +2079,7 @@ _gnutls_server_select_cert (gnutls_session_t session,
       _gnutls_selected_certs_set (session,
 				  &cred->cert_list[idx][0],
 				  cred->cert_list_length[idx],
-				  &cred->pkey[idx], 0);
+				  cred->pkey[idx], 0);
     }
   else
     /* Certificate does not support REQUESTED_ALGO.  */

commit cd656dbb97bade7d46264b71061268b299f17e6b

lib/pkcs11_privkey.c: Add check for gnutls_malloc
Add check for the return value of gnutls_malloc() to avoid potential NULL pointer dereference.

Fixes: be560a813 ("Added gnutls_pkcs11_privkey_t and gnutls_privkey_t types. Those are an abstract private key type that can be used to sign/encrypt any private key of pkcs11,x509 or openpgp types. Added support for PKCS11 in gnutls-cli/gnutls-serv.")
Signed-off-by: Jiasheng Jiang <jiashengjiangcool@gmail.com>

lib/pkcs11_privkey.c
@@ -401,6 +401,12 @@ retry_login:
 	}
 
 	tmp.data = gnutls_malloc(siglen);
+	if (tmp.data == NULL) {
+		gnutls_assert();
+		ret = GNUTLS_E_MEMORY_ERROR;
+		goto cleanup;
+	}
+
 	tmp.size = siglen;
 
 	rv = pkcs11_sign(sinfo->module, sinfo->pks, hash->data, hash->size,
@@ -757,6 +763,12 @@ retry_login:
 	}
 
 	plaintext->data = gnutls_malloc(siglen);
+	if (plaintext->data == NULL) {
+		gnutls_assert();
+		ret = GNUTLS_E_MEMORY_ERROR;
+		goto cleanup;
+	}
+
 	plaintext->size = siglen;
 
 	rv = pkcs11_decrypt(key->sinfo.module, key->sinfo.pks, ciphertext->data,